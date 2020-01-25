a36ecee3a5197dcf40c482f6ba71b38b08a7d72f
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-01-25 20:27:57 +0100

    moved the path of files

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
+
 function toggle() {
   this.classList.toggle("active")
 }
