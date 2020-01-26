cd8b4e11a5f4dde1d2c046ef0e0f494c5acb2779
camilla <camilla.gausland@gmail.com>
2020-01-26 17:22:16 +0100

    changed the document

--- code/script.js
+++ code/script.js
@@ -1,3 +1,8 @@
+var sectionOne=document.getElementById("section1");
+var sectionTwo=document.getElementById("section2");
+var sectionThree=document.getElementById("section3");
+
+
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
@@ -5,5 +10,10 @@ function toggle() {
   this.classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// Selects an HTML element, and calls a function which 
+//will be executed when the element is clicked.
+document.getElementById("section1").onclick = toggle;
+document.getElementById("section2").onclick = toggle;
+document.getElementById("section3").onclick = toggle;
+
+
