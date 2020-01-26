ecf1bb28783cf9b49d98a28a6d3c4a805057a4f6
Josephine <josephine@Josephines-Air.lan>
2020-01-26 10:37:16 +0100

    added hero-image, form and accordion that works

--- code/script.js
+++ code/script.js
@@ -1,9 +1,26 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+/*document.getElementsByTagName("button").onclick = toggle
+button.onclick = toggleAnswer
 
+function toggleAnswer() {
+  this.classlist.toggle(answer)
+}*/
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+
+
+var question = document.getElementsByClassName("question");
+var i;
+
+for (i = 0; i < question.length; i++) {
+  question[i].addEventListener("click", function () {
+    this.classList.toggle("answer");
+    var answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}
