ea7ed19e10066e6cdb61d8a5a7059573d06292f5
bealun <53275597+bealun@users.noreply.github.com>
2020-01-26 16:47:46 +0100

    updated layout

--- code/script.js
+++ code/script.js
@@ -5,7 +5,19 @@ function toggle() {
   this.classList.toggle("active")
 }
 
+
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-document.getElementById("section2").onclick = toggle
-document.getElementById("section3").onclick = toggle
+// document.getElementById("section1").onclick = toggle
+// document.getElementById("section2").onclick = toggle
+// document.getElementById("section3").onclick = toggle
+
+
+// const question1 = document.getElementById("section1");
+// question1.onclick = function(){
+// question1.classList.toggle('active');
+// }
+
+
+// const question1 =document.getElementById("section1")
+// const question2 =document.getElementById("section2")
+// const question3 =document.getElementById("section3")
