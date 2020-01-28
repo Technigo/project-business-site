fb364200c35b4986c4e90f37c814251a987da242
Anne-Sophie Gendron <email@example.comannesophie.gendron@gmail.com>
2020-01-29 00:26:18 +0100

    accordion added

--- code/script.js
+++ code/script.js
@@ -2,8 +2,13 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 function toggle() {
-  this.classList.toggle("active")
+  this.ClassList.toggle("active")
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("section1").onclick = toggle;
+document.getElementById("section2").onclick = toggle;
+document.getElementById("section3").onclick = toggle;
+document.getElementById("section4").onclick = toggle;
+document.getElementById("section5").onclick = toggle;
+
