ea09a6edd6955b20d67fa4532acc2ab5e73c6eed
Christina Persson <christina@Christinas-MacBook.local>
2020-01-28 18:18:15 +0100

    Transparent layer added to video

--- code/script.js
+++ code/script.js
@@ -1,6 +1,3 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
 const btnOne = document.getElementById("section-one");
 const btnTwo = document.getElementById("section-two");
 btnOne.onclick = toggle;
@@ -9,6 +6,3 @@ btnTwo.onclick = toggle;
 function toggle() {
   this.classList.toggle("open");
 }
-
-// // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section1").onclick = toggle;
