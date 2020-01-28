b85f88521c1f040e2615a50da373f59fc8db8cf1
Kajsa Bootin <kajsa@Kajsas-MacBook-Air.local>
2020-01-28 16:27:22 +0100

    the last one done

--- code/script.js
+++ code/script.js
@@ -13,22 +13,14 @@ const question1 = document.getElementById("section1");
 const question2 = document.getElementById("section2");
 const question3 = document.getElementById("section3");
 
-// question1.onclick = function(){
-//   question1.classList.toggle('active');
-// }
-
-function banana(){
-  question1.classList.toggle('active');
+question1.onclick = function(){
+question1.classList.toggle('active');
 }
 
-function banana2(){
-  question2.classList.toggle('active');
+question2.onclick = function(){
+question2.classList.toggle('active');
 }
 
-function peter (){
-  question3.classList.toggle('active');
+question3.onclick = function(){
+question3.classList.toggle('active');
 }
-
-question1.addEventListener('click',banana);
-question2.addEventListener('click',banana2);
-question3.addEventListener('click',peter);
