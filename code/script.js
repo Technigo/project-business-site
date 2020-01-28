68dbf7e312ca18f61f226c23e5864f391b512064
Ragna Þorbjörg Úlfarsdóttir <Ragna@Ragnas-MBP.lan>
2020-01-28 15:56:34 +0100

    Final styling, media queries and icons that rotate

--- code/script.js
+++ code/script.js
@@ -1,9 +1,18 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
+// function toggle() {
+//   this.classList.toggle("active")
+// }
+
+
 function toggle() {
   this.classList.toggle("active")
 }
+
+// function rotate() {
+//   this..toggle("active")
+// }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
@@ -15,3 +24,12 @@ document.getElementById("section3").onclick = toggle
 document.getElementById("section4").onclick = toggle
 
 
+// $(".rotate").click(function(){
+//   $(this).toggleClass("down"); 
+// });
+
+// const icon = document.getElementById("fa fa-plus")
+
+// icon.onclick = () => {
+//   DeviceRotationRate(45)
+// } 
