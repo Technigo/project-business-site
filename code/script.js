f958734ff266e431abe55f218f615417d82a0c8f
Ida Abrahamsson <IdaAbra@94-255-145-130.cust.bredband2.com>
2020-01-29 17:36:04 +0100

    Finished the HTML and mostly of the CSS. Does not get the JS at all

--- code/script.js
+++ code/script.js
@@ -1,9 +1,17 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
+
+const acc = document.getElementById("section1")
+const answ = document.getElementById("show1")
+
+acc.onclick = () => {
+  acc.classList.toggle('active')
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+
+
+
+
+
+// Selects an HTML element, and call a function which will be executed when the element is clicked.
