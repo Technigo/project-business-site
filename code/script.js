099221d292cb29ba170c1d1efa35c888189331d5
palhamel <pal.hamel@gmail.com>
2020-01-29 14:29:33 +0100

    Add fact in accordion

--- code/script.js
+++ code/script.js
@@ -9,3 +9,4 @@ function toggle() {
 document.getElementById("section1").onclick = toggle;
 document.getElementById("section2").onclick = toggle;
 document.getElementById("section3").onclick = toggle;
+document.getElementById("section4").onclick = toggle;
