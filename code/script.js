cd41c2b05d9ff0a98f1a85b05e33ebd3d7ba16bd
VickBe <32632292+VickBe@users.noreply.github.com>
2020-01-29 17:19:48 +0100

    Added animations to the answer boxes

--- code/script.js
+++ code/script.js
@@ -8,7 +8,7 @@
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 //document.getElementById("section1").onclick = toggle
 
-
+// document.getElementsByTagName('button').onclick = getAnswer
 
 
 document.getElementById('section1').onclick = getAnswer
