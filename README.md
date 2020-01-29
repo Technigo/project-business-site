54fae86ad8879ea80005a47b3bc3b4aba867d336
William Jensen <williamjensen@Williams-MacBook-Air.local>
2020-01-29 14:09:20 +0100

    First draft of website with html, css and javascript

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Business Site
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The assignment was to create a business webpage. The basic requirements were that it should contain a responsive hero image or video as its header, as well as a form and an 'accordion'. 
 
 ## The problem
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
 
+I took my design inspiration for the header from the website 'confetti'. I downloaded a program to resize video files to create three different sized videos which would change depending on the browser size and set them to absolute, positioned within a relative header with hidden overflow. I also made the video's z-index -1 so that other text would show on top of it, and set it to a fixed height with width 100%. This proved the biggest challenge of the project. 
+
+I thought the form was very straight-forward, though no doubt I could have spent more time on it to make it more lively, e.g. by adding animation to the valid/invalid pseudo classes. The accordion took longer to animate. I learnt that 'transition' will not apply to display changes, so I needed an alternative to simply switching between display: none and block. I found max-height: 0 w/ overflow hidden a good alternative and used a keyframes animation to change from that to max-height 108px. I wanted to switch to max-height 100% but this made the animation buggy. I also had to use classList.add and remove instead of toggle, as this caused problems with (I think) multiple different classes being added at once. I would liked to have spent more time understanding this and using more JavaScript. 
+
+
 ## View it live
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+
--- code/script.js
+++ code/script.js
@@ -1,9 +1,75 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
+/*** Accordian functionality ***/
+
+const sectionOne = document.getElementById('section1');
+const sectionTwo = document.getElementById('section2');
+const sectionThree = document.getElementById('section3');
+
+function toggleFirst() {
+  if (this.innerHTML === "➕") {
+    this.innerHTML = "➖";
+    sectionOne.classList.remove('folded');
+    sectionOne.classList.add('active');
+  } else {
+    this.innerHTML = '➕';
+    sectionOne.classList.remove('active');
+    sectionOne.classList.add('folded');
+
+  }
 }
 
+function toggleSecond() {
+  if (this.innerHTML === "➕") {
+    this.innerHTML = "➖";
+    sectionTwo.classList.remove('folded');
+    sectionTwo.classList.add('active');
+  } else {
+    this.innerHTML = '➕';
+    sectionTwo.classList.remove('active');
+    sectionTwo.classList.add('folded');
+
+  }
+}
+
+function toggleThird() {
+  if (this.innerHTML === "➕") {
+    this.innerHTML = "➖";
+    sectionThree.classList.remove('folded');
+    sectionThree.classList.add('active');
+  } else {
+    this.innerHTML = '➕';
+    sectionThree.classList.remove('active');
+    sectionThree.classList.add('folded');
+
+  }
+}
+
+/***  Night Mode Button in footer ***/
+
+function nightTime() {
+  document.getElementById('mainId').classList.toggle('nightMode');
+  document.getElementById('footerId').classList.toggle('nightMode');
+  document.getElementById('night-switch').classList.toggle('footer-button');
+  if (this.innerHTML === "Night Mode") {
+    this.innerHTML = "Day Mode";
+  } else {
+    this.innerHTML = "Night Mode";
+  }
+}
+
+document.getElementById('night-switch').onclick = nightTime
+
+
+
+
+
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("accordion1").onclick = toggleFirst
+document.getElementById("accordion2").onclick = toggleSecond
+document.getElementById("accordion3").onclick = toggleThird
+
+
+
+
+
+
+
