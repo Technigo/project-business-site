674c5f81efd07324a34e7552d16be2afd67e0c2d
Johanna Olsen <johanna.olsen.92@gmail.com>
2020-01-29 15:38:50 +0100

    navbar, video, form, accordian and footer added

--- code/script.js
+++ code/script.js
@@ -1,9 +1,31 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
+
+
+// function toggleFAQ will toggle between class active and without class active
+// active class is styled in CSS (does not exist in HTML)
+// in CSS .active {} will style the question
+// .active + .answer {} display and style the answer 
+
+function toggleFAQ() {
   this.classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+
+// variables
+
+var section1 = document.getElementById("section1")
+
+var section2 = document.getElementById("section2")
+
+var section3 = document.getElementById("section3")
+
+// Selects variables and call for function which will be executed when the element is clicked.
+
+
+section1.onclick = toggleFAQ
+
+section2.onclick = toggleFAQ
+
+section3.onclick = toggleFAQ
