d88c14b1373d25e096eeba548b329604539200c8
Ebba Bergholtz <ebbabergholtz@gmail.com>
2020-01-29 01:48:49 +0100

    Added style and fixed overal issues

--- code/script.js
+++ code/script.js
@@ -1,8 +1,11 @@
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 
-document.getElementById('section').onclick = toggle
-
+document.getElementById('section1').onclick = toggle
+document.getElementById('section2').onclick = toggle
+document.getElementById('section3').onclick = toggle
+document.getElementById('section4').onclick = toggle
+document.getElementById('section5').onclick = toggle
 
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
