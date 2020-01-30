f5df0600802d46e45b06a073a154a40ee49c2245
Louise Eriksson <louise@Louises-MacBook-Air.local>
2020-01-30 10:12:07 +0100

    fixed the javascript, styled the icons, cleaned up the code

--- code/script.js
+++ code/script.js
@@ -1,9 +1,14 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+
+
+var acc = document.getElementsByClassName("accordion-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+  });
+}
