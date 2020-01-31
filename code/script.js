5a7bf0028f91682429ddcc90b29bd8377b5fb7ae
Your Name <you@example.com>
2020-01-31 14:00:03 +0100

    new code

--- code/script.js
+++ code/script.js
@@ -1,9 +1,14 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
+// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+
+document.getElementById("section1").onclick = toggle
+
 function toggle() {
   this.classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("section2").onclick = toggle
+
+document.getElementById("section3").onclick = toggle
