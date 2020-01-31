9a51482f3541462e99882eacdca9c6387ac31ded
aasa72 <asa.andersson10@comhem.se>
2020-01-31 21:16:11 +0100

    added hero, form and accordion

--- code/script.js
+++ code/script.js
@@ -1,9 +1,14 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    accordionItemHeader.classList.toggle("active")
+
+  })
+})
+
+/* I struggled with understanding how we were supposed to do this, so in
+  the end I scrapped the pre-written code and followed a tutorial on youtube
+  instead.*/
