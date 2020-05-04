982df20fd69ce2426692939c654fff4718782aef
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-05-04 15:58:05 +0200

    change

--- code/script.js
+++ code/script.js
@@ -7,20 +7,18 @@
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 
-/*document.getElementById("section1").onclick = toggle*/
-
 const question1 = document.getElementById("section1");
 const question2 = document.getElementById("section2");
 const question3 = document.getElementById("section3");
 
-question1.onclick = function(){
-question1.classList.toggle('active');
-}
+question1.onclick = function () {
+  question1.classList.toggle("active");
+};
 
-question2.onclick = function(){
-question2.classList.toggle('active');
-}
+question2.onclick = function () {
+  question2.classList.toggle("active");
+};
 
-question3.onclick = function(){
-question3.classList.toggle('active');
-}
+question3.onclick = function () {
+  question3.classList.toggle("active");
+};
