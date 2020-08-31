40d2b1b0861c0167177b4a9511d20edeeec8c0a6
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-08-31 14:15:33 +0200

    Add accordion with basic styling

--- code/script.js
+++ code/script.js
@@ -1,9 +1,10 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+// Selects the different elements to be opened with the function
+document.getElementById("button1").onclick = open
+document.getElementById("button2").onclick = open
+document.getElementById("button3").onclick = open
+document.getElementById("button4").onclick = open
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// This function toggles the open class on the button elements
+function open() {
+  this.classList.toggle("open")
+}
