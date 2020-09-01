4b9a9c53b2a0ca657c64596d93ca1879a6960c9c
Moa <63230455+mhammarback@users.noreply.github.com>
2020-09-01 10:19:08 +0200

    javascript file added, working accordion with mobile first design added

--- code/script.js
+++ code/script.js
@@ -5,5 +5,12 @@ function toggle() {
   this.classList.toggle("active")
 }
 
+
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+
+document.getElementById("section2").onclick = toggle
+
+
+
+
