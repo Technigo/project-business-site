22c14274e0a012396bc8708c23bcf06bd2293193
Max Forssén <max.forssen@mentimeter.com>
2020-09-01 16:03:42 +0200

    added styling

--- code/script.js
+++ code/script.js
@@ -10,3 +10,4 @@ document.getElementById("section1").onclick = toggle;
 document.getElementById("section2").onclick = toggle;
 document.getElementById("section3").onclick = toggle;
 document.getElementById("section4").onclick = toggle;
+//[1,2,3,4].forEach(d => document.getElementById(`section${d}`).onclick = toggle)
