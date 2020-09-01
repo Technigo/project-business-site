2ea5c44494ef0e2bafcd99e56e6c58223ce6f2d9
MindstormingAB <s.elander@mindstorming.se>
2020-09-01 23:24:32 +0200

    First draft

--- code/script.js
+++ code/script.js
@@ -1,3 +1,15 @@
+const question1 = document.getElementById("section1")
+const question2 = document.getElementById("section2")
+const question3 = document.getElementById("section3")
+const question4 = document.getElementById("section4")
+const question5 = document.getElementById("section5")
+
+question1.onclick = toggle
+question2.onclick = toggle
+question3.onclick = toggle
+question4.onclick = toggle
+question5.onclick = toggle
+
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
@@ -6,4 +18,4 @@ function toggle() {
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// document.getElementById("section1").onclick = toggle
