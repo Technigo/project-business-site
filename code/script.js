7f8b61a6e486186c0f466b3344e3fbd474a8b4e0
Sara Sundblom <sundblomsara@gmail.com>
2020-09-01 19:16:31 +0200

    made the accordiongit add .

--- code/script.js
+++ code/script.js
@@ -4,3 +4,12 @@ function toggleHamburger() {
 
 document.getElementById("topnavbar").onclick = toggleHamburger
 
+function toggleFaq () {
+  this.classList.toggle("question")
+}
+
+document.getElementById("question1").onclick = toggleFaq
+document.getElementById("question2").onclick = toggleFaq
+document.getElementById("question3").onclick = toggleFaq
+document.getElementById("question4").onclick = toggleFaq
+document.getElementById("question5").onclick = toggleFaq
