6a5199d479f7956be28fa03ee29014f2e0780de4
jonna ru <jonna.ru@icloud.com>
2020-09-01 20:25:05 +0200

    header and hamburger meny lyaout for tablet and mobiel

--- code/script.js
+++ code/script.js
@@ -1,9 +1,16 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("hamburger").onclick = toggle;
+
+function toggle() {
+  this.classList.toggle("open");
+}
+
+// document.getElementById("something").onclick = toggle;
+
+//function toggle() {
+// this.classList.toggle("active");
+//}
