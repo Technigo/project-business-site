9ab1733f2bf6577fe2163855b346d9fa3ce60dca
Sara Sundblom <sundblomsara@gmail.com>
2020-09-02 18:39:13 +0200

    made the navbar transpararentgit add .git add .

--- code/script.js
+++ code/script.js
@@ -1,9 +1,13 @@
 function toggleHamburger() {
-  this.parentNode.classList.toggle("responsive")
+  this.parentNode.classList.toggle("active")
 }
 
 document.getElementById("hamburger").onclick = toggleHamburger
 
+
+
+
+/*
 function toggleFaq () {
   this.classList.toggle("question")
 }
@@ -13,3 +17,35 @@ document.getElementById("question2").onclick = toggleFaq
 document.getElementById("question3").onclick = toggleFaq
 document.getElementById("question4").onclick = toggleFaq
 document.getElementById("question5").onclick = toggleFaq
+*/
+
+const faqList = document.querySelectorAll(".question") 
+
+faqList.forEach(item => {
+  item.onclick = (event) => {
+    faqList.forEach(itemNested => {
+      if (itemNested !== event.target) {
+          itemNested.nextElementSibling.classList.remove("question")
+      } else {
+        itemNested.nextElementSibling.classList.toggle("question")
+      }
+     })
+  }
+})
+
+
+
+console.log(faqList)
+
+/*
+accordionList.forEach(item => {
+    item.onclick = (event) => {
+        accordionList.forEach(itemNested => {
+            if (itemNested !== event.target) {
+                itemNested.nextElementSibling.classList.remove('active');
+            } else {
+                itemNested.nextElementSibling.classList.toggle('active');
+            }
+        })
+    }
+});*/
