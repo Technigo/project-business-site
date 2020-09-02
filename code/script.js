30581b2b34eb6b8d8ec62e0ff6d7efb4dff3b404
Ylva Nilsson <ylva.z.nilsson@teliacompany.com>
2020-09-02 14:15:39 +0200

    fixed SIGNUP form styling and added more question section for FAQ

--- code/script.js
+++ code/script.js
@@ -8,3 +8,5 @@ function toggle() {
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle;
+document.getElementById("section2").onclick = toggle;
+document.getElementById("section3").onclick = toggle;
