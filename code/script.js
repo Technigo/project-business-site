f1e1c8cb8b593f0acbec245f3811bf1ab7d6c976
Gabbi-89 <gabriellabolin@hotmail.com>
2020-09-02 20:28:20 +0200

    Blue level reached. Design might be updated when I continue.

--- code/script.js
+++ code/script.js
@@ -6,22 +6,23 @@ this.classList.toggle("active")
 } 
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle 
-const sectionOne = document.getElementById("section1");
+document.getElementById("question-1").onclick = toggle 
+const questionOne = document.getElementById("question-1");
 
-document.getElementById("section2").onclick = toggle
-const sectionTwo = document.getElementById("section2");
+document.getElementById("question-2").onclick = toggle
+const questionTwo = document.getElementById("question-2");
 
-document.getElementById("section3").onclick = toggle
-const sectionThree = document.getElementById("section3");
+document.getElementById("question-3").onclick = toggle
+const questionThree = document.getElementById("question-3");
 
-document.getElementById("section4").onclick = toggle
-const sectionFour = document.getElementById("section4");
+document.getElementById("question-4").onclick = toggle
+const questionFour = document.getElementById("question-4");
 
-document.getElementById("section5").onclick = toggle
-const sectionFive = document.getElementById("section5");
+document.getElementById("question-5").onclick = toggle
+const questionFive = document.getElementById("question-5");
 
 console.log(this)
+
 
 /*
 document.getElementById("section1").onclick = toggle 
@@ -112,3 +113,27 @@ for (i=0; i < sec.length; i++) {
     }
   });
 } */
+/*
+const questionOne = document.getElementById("question-1");
+const questionTwo = document.getElementById("question-2");
+const questionThree = document.getElementById("question-3");
+const questionFour = document.getElementById("question-4");
+const questionFive = document.getElementById("question-5");
+
+const onQuestionClick = (event) => {
+  event.target.nextElementSibling.classList.toggle("active");
+
+  const questionList = document.querySelectorAll(".question");
+  questionList.forEach(item => {
+    if (item !== event.target) {
+      item.nextElementSibling.classList.remove("active");
+    }
+  });
+}
+
+questionOne.onclick = onQuestionClick;
+questionTwo.onclick = onQuestionClick;
+questionThree.onclick = onQuestionClick;
+questionFour.onclick = onQuestionClick;
+questionFive.onclick = onQuestionClick;
+*/
