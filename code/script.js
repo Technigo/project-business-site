0526bcb9792df19632647ffd8245fcc93ddaa154
MindstormingAB <s.elander@mindstorming.se>
2020-09-03 00:34:20 +0200

    Sticky nav, hero video and FAQ

--- code/script.js
+++ code/script.js
@@ -1,14 +1,55 @@
-const question1 = document.getElementById("section1")
-const question2 = document.getElementById("section2")
-const question3 = document.getElementById("section3")
-const question4 = document.getElementById("section4")
-const question5 = document.getElementById("section5")
+const question1 = document.getElementById("question-1")
+const question2 = document.getElementById("question-2")
+const question3 = document.getElementById("question-3")
+const question4 = document.getElementById("question-4")
+const question5 = document.getElementById("question-5")
+const answer1 = question1.nextElementSibling;
+const answer2 = question2.nextElementSibling;
+const answer3 = question3.nextElementSibling;
+const answer4 = question4.nextElementSibling;
+const answer5 = question5.nextElementSibling;
 
-question1.onclick = toggle
-question2.onclick = toggle
-question3.onclick = toggle
-question4.onclick = toggle
-question5.onclick = toggle
+question1.onclick = () => {
+  answer1.classList.toggle('active');
+  answer2.classList.remove('active');
+  answer3.classList.remove('active');
+  answer4.classList.remove('active');
+  answer5.classList.remove('active');
+}
+
+question2.onclick = () => {
+  answer2.classList.toggle('active');
+  answer1.classList.remove('active');
+  answer3.classList.remove('active');
+  answer4.classList.remove('active');
+  answer5.classList.remove('active');
+}
+
+question3.onclick = () => {
+  answer3.classList.toggle('active');
+  answer2.classList.remove('active');
+  answer1.classList.remove('active');
+  answer4.classList.remove('active');
+  answer5.classList.remove('active');
+}
+
+question4.onclick = () => {
+  answer4.classList.toggle('active');
+  answer2.classList.remove('active');
+  answer3.classList.remove('active');
+  answer1.classList.remove('active');
+  answer5.classList.remove('active');
+}
+
+question5.onclick = () => {
+  answer5.classList.toggle('active');
+  answer2.classList.remove('active');
+  answer3.classList.remove('active');
+  answer4.classList.remove('active');
+  answer1.classList.remove('active');
+}
+
+
 
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
@@ -19,3 +60,16 @@ function toggle() {
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 // document.getElementById("section1").onclick = toggle
+
+//Sticky nav in Javascript
+const nav = document.querySelector('nav');
+const navOffset = nav.offsetTop;
+
+window.onscroll = () => {
+  if (navOffset > window.pageYOffset) {
+    nav.classList.remove('sticky')
+  }
+  else {
+    nav.classList.add('sticky')
+  }
+}
