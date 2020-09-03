24d19a0cb15fddb3c7b26e512366daf1c50cdaf6
Annika Melin <melin.anki>
2020-09-04 01:36:09 +0200

    Added accordion, responsive header and content, text animation

--- code/script.js
+++ code/script.js
@@ -1,9 +1,16 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
+
+const question1 = document.getElementById("question1")
+const question2 = document.getElementById("question2")
+const question3 = document.getElementById("question3")
+
 function toggle() {
   this.classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+question1.onclick = toggle
+question2.onclick = toggle
+question3.onclick = toggle
+
+
+
+
