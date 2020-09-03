1ab69973277b0712a39893db21570816a792825e
Lili <lilihorvath@github.com>
2020-09-03 15:55:55 +0200

    Sceleton without stlying

--- code/script.js
+++ code/script.js
@@ -1,9 +1,40 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
+
+const accordionHeader1 = document.getElementById('accordion-header-1');
+const accordionHeader2 = document.getElementById('accordion-header-2');
+const accordionHeader3 = document.getElementById('accordion-header-3');
+
+const onAccordion1Click = () => {
+  accordionHeader1.nextElementSibling.classList.toggle('active');
+
+  accordionHeader2.nextElementSibling.classList.remove('active');
+  accordionHeader3.nextElementSibling.classList.remove('active');
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+const onAccordion2Click = () => {
+  accordionHeader2.nextElementSibling.classList.toggle('active');
+
+  accordionHeader1.nextElementSibling.classList.remove('active');
+  accordionHeader3.nextElementSibling.classList.remove('active');
+}
+
+const onAccordion3Click = () => {
+  accordionHeader3.nextElementSibling.classList.toggle('active');
+
+  accordionHeader1.nextElementSibling.classList.remove('active');
+  accordionHeader2.nextElementSibling.classList.remove('active');
+}
+
+accordionHeader1.onclick = onAccordion1Click;
+accordionHeader2.onclick = onAccordion2Click;
+accordionHeader3.onclick = onAccordion3Click;
+
+const navbar = document.querySelector('.navbar');
+const navbarOffset = navbar.offsetTop;
+
+window.onscroll = () => {
+     if (navbarOffset >= window.pageYOffset) {
+         navbar.classList.remove('sticky');
+     } else {
+         navbar.classList.add('sticky');
+     }
+ }
