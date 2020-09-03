33cf01aeadcd474c6c685699b26bd15880875a36
Emelie Svensson <emeliesv@live.se>
2020-09-03 22:27:38 +0200

    Styled background and header

--- code/script.js
+++ code/script.js
@@ -5,13 +5,21 @@ const sectionOne = document.getElementById('section1');
 const sectionTwo = document.getElementById('section2');
 const sectionThree = document.getElementById('section3');
 const sectionFour = document.getElementById('section4');
+const questions = document.querySelectorAll('.question');
 
-function toggle() {
-  this.classList.toggle("active")
+function toggle(event) {
+  this.classList.toggle("active");
+  questions.forEach(question => {
+    if (question !== event.target){
+      question.classList.remove('active');
+    } else {
+      question.classList.toggle('active');
+    }
+  })
 }
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+ 
 sectionOne.onclick = toggle
 sectionTwo.onclick = toggle
 sectionThree.onclick = toggle
 sectionFour.onclick = toggle
+
