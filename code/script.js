bd68986accb6973661886250c2d64ceb987cd985
Sandra Shumanteva Kaevska <sandra@shumantevi.mk>
2020-09-03 14:58:26 +0200

    blue&red level

--- code/script.js
+++ code/script.js
@@ -1,9 +1,12 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
+
 function toggle() {
-  this.classList.toggle("active")
+  this.classList.toggle("active");
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("section1").onclick = toggle;
+document.getElementById("section2").onclick = toggle;
+document.getElementById("section3").onclick = toggle;
