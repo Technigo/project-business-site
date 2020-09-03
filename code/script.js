ebed7b3f3291471eaedaec85389f8d5a8796209f
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2020-09-03 23:33:06 +0200

    added video-header/sign up form and accordion

--- code/script.js
+++ code/script.js
@@ -1,9 +1,28 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+const accordionHeader1 = document.getElementById("accordion-header-1");
+const accordionHeader2 = document.getElementById("accordion-header-2");
+const accordionHeader3 = document.getElementById("accordion-header-3");
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+const onAccordion1Click = () => {
+  accordionHeader1.nextElementSibling.classList.toggle("active");
+
+  accordionHeader2.nextElementSibling.classList.remove("active");
+  accordionHeader3.nextElementSibling.classList.remove("active");
+};
+
+const onAccordion2Click = () => {
+  accordionHeader2.nextElementSibling.classList.toggle("active");
+
+  accordionHeader1.nextElementSibling.classList.remove("active");
+  accordionHeader3.nextElementSibling.classList.remove("active");
+};
+
+const onAccordion3Click = () => {
+  accordionHeader3.nextElementSibling.classList.toggle("active");
+
+  accordionHeader1.nextElementSibling.classList.remove("active");
+  accordionHeader2.nextElementSibling.classList.remove("active");
+};
+
+accordionHeader1.onclick = onAccordion1Click;
+accordionHeader2.onclick = onAccordion2Click;
+accordionHeader3.onclick = onAccordion3Click;
