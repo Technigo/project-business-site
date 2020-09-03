0315a9c9f5045e5b8e59f33a2c9da3ec4e7fde5d
Therese <Therese@Thereses-MacBook-Air.local>
2020-09-03 16:41:44 +0200

    first edit HTML, CSS & JS

--- code/script.js
+++ code/script.js
@@ -1,9 +1,21 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
+const FAQHeader1 = document.getElementById('FAQ-Header-1');
+const FAQHeader1 = document.getElementById('FAQ-Header-2');
+const FAQHeader1 = document.getElementById('FAQ-Header-3');
+
+console.log(FAQHeader1, FAQHeader2, FAQHeader3);
+console.dir(FAQHeader1, FAQHeader2, FAQHeader3);
+
+FAQHeader1.onclick = () => {
+  const FAQAnswerContainer1 = FAQAnswerContainer1.nextElementSibling;
+  FAQAnswerContainer1.classList.toggle('active');
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+FAQHeader2.onclick = () => {
+  const FAQAnswerContainer1 = FFAQAnswerContainer2.nextElementSibling;
+  FAQAnswerContainer2.classList.toggle('active');
+}
+
+FAQHeader3.onclick = () => {
+  const FAQAnswerContainer2 = FAQAnswerContainer3.nextElementSibling;
+  FAQAnswerContainer3.classList.toggle('active');
+}
