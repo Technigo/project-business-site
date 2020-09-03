904acfdd7d9c7ab1a4c9414b1213f0bccdbb8240
Jamie Cook <jamie.p.cook@outlook.com>
2020-09-03 16:20:41 +0200

    Video header added, form and accordion created

--- code/script.js
+++ code/script.js
@@ -6,4 +6,7 @@ function toggle() {
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("question1").onclick = toggle
+document.getElementById("question2").onclick = toggle
+document.getElementById("question3").onclick = toggle
+document.getElementById("question4").onclick = toggle
