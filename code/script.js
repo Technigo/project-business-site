3f29d125bb27476b5fac8f84de5f20123b0d503a
Sara Stjernberg <sara_stjernberg@hotmail.com>
2020-09-03 13:59:37 +0200

    added a form

--- code/script.js
+++ code/script.js
@@ -1,9 +1,16 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+// function toggle() {
+//   this.classList.toggle("active")
+// }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// // Selects an HTML element, and calls a function which will be executed when the element is clicked.
+// document.getElementById("section1").onclick = toggle
+
+const accordionHeader1 = document.getElementById("accordion-header-1");
+const accordionHeader2 = document.getElementById("accordion-header-2");
+const accordionHeader3 = document.getElementById("accordion-header-3");
+
+console.log("accordionHeader1", "accordionHeader2", "accordionHeader3");
+alert("hey");
