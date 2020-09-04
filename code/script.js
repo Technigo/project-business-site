dca670af561b3ef663efa66cbf596e9a4dbf79f8
Therese Lindén <theresemlinden@gmail.com>
2020-09-04 21:14:17 +0200

    styled radio and checkboxes, added a favicon, added z-index to my header-bar sticky to make it cover my absolut position on radio buttons

--- code/script.js
+++ code/script.js
@@ -1,41 +1,40 @@
-const accordionHeader1 = document.getElementById('accordion-header-1');
-const accordionHeader2 = document.getElementById('accordion-header-2');
-const accordionHeader3 = document.getElementById('accordion-header-3');
+//const accordionHeader1 = document.getElementById('accordion-header-1');
+//const accordionHeader2 = document.getElementById('accordion-header-2');
+//const accordionHeader3 = document.getElementById('accordion-header-3');
 
-console.log(accordionHeader1,accordionHeader2,accordionHeader3);
-console.dir(accordionHeader1,accordionHeader2,accordionHeader3);
+//console.log(accordionHeader1,accordionHeader2,accordionHeader3);
+//console.dir(accordionHeader1,accordionHeader2,accordionHeader3);
 
-accordionHeader1.onclick = () => {
-  const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-  accordionTextContainer1.classList.toggle('active');
+//accordionHeader1.onclick = () => {
+  //const accordionTextContainer1 = accordionHeader1.nextElementSibling;
+  //accordionTextContainer1.classList.toggle('active');
 
-  const accordionTextContainer2 = accordionHeader2.nextElementSibling;
-  const accordionTextContainer3 = accordionHeader3.nextElementSibling;
-  accordionTextContainer2.classList.remove('active');
-  accordionTextContainer3.classList.remove('active');
-}
+  //const accordionTextContainer2 = accordionHeader2.nextElementSibling;
+  //const accordionTextContainer3 = accordionHeader3.nextElementSibling;
+  //accordionTextContainer2.classList.remove('active');
+  //accordionTextContainer3.classList.remove('active');
+//}
 
-accordionHeader2.onclick = () => {
-  const accordionTextContainer2 = accordionHeader2.nextElementSibling;
-  accordionTextContainer2.classList.toggle('active');
+//accordionHeader2.onclick = () => {
+  //const accordionTextContainer2 = accordionHeader2.nextElementSibling;
+  //accordionTextContainer2.classList.toggle('active');
 
-  const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-  const accordionTextContainer3 = accordionHeader3.nextElementSibling;
-  accordionTextContainer1.classList.remove('active');
-  accordionTextContainer3.classList.remove('active');
-}
+  //const accordionTextContainer1 = accordionHeader1.nextElementSibling;
+  //const accordionTextContainer3 = accordionHeader3.nextElementSibling;
+  //accordionTextContainer1.classList.remove('active');
+  //accordionTextContainer3.classList.remove('active');
+//}
 
 
-accordionHeader3.onclick = () => {
-  const accordionTextContainer3 = accordionHeader3.nextElementSibling;
-  accordionTextContainer3.classList.toggle('active');
+//accordionHeader3.onclick = () => {
+  //const accordionTextContainer3 = accordionHeader3.nextElementSibling;
+  //accordionTextContainer3.classList.toggle('active');
 
-  const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-  const accordionTextContainer2 = accordionHeader2.nextElementSibling;
-  accordionTextContainer1.classList.remove('active');
-  accordionTextContainer2.classList.remove('active');
-}
-
+  //const accordionTextContainer1 = accordionHeader1.nextElementSibling;
+  //const accordionTextContainer2 = accordionHeader2.nextElementSibling;
+  //accordionTextContainer1.classList.remove('active');
+  //accordionTextContainer2.classList.remove('active');
+//}
 
 const header = document.querySelector('.header-bar');
 const headerOffset = header.offsetTop;
@@ -50,6 +49,29 @@ window.onscroll = () => {
         header.classList.add('sticky');
     }
 }
+
+const accordionList = document.querySelectorAll('.accordion-header');
+
+accordionList.forEach(item => {
+  item.onclick = (event) => {
+    accordionList.forEach(itemNested => {
+      if (itemNested !== event.target) {
+        itemNested.nextElementSibling.classList.remove('active');
+        itemNested.classList.remove('active');
+      } else {
+        itemNested.nextElementSibling.classList.toggle('active');
+        itemNested.classList.toggle('active')
+      }
+    })
+  } 
+})
+
+
+
+
+
+
+
 
 //console.log(header);
 //console.dir(header);
