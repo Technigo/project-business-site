c8178323daa9094566b2fac475a490e9d0567315
Katsiez <katarinajovanovic.a@gmail.com>
2020-09-05 00:32:07 +0200

    Blue level commit with a responsive accordion, sign-up form, hero video

--- code/script.js
+++ code/script.js
@@ -7,6 +7,9 @@
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 // document.getElementById("section1").onclick = toggle
+
+
+// Sign in form validation
 const firstName = document.getElementById('firstName')
 const lastName = document.getElementById('lastName')
 const email = document.getElementById('email')
@@ -27,3 +30,68 @@ form.addEventListener('submit', (e) => {
   }
 
 })
+// Accordion section
+
+// 1. Function on each element
+
+const accordionHeader1 = document.getElementById('accordion-header-1');
+const accordionHeader2 = document.getElementById('accordion-header-2');
+const accordionHeader3 = document.getElementById('accordion-header-3');
+
+const onAccordion1Click = () => {
+  accordionHeader1.nextElementSibling.classList.toggle('active');
+
+  accordionHeader2.nextElementSibling.classList.remove('active');
+  accordionHeader3.nextElementSibling.classList.remove('active');
+};
+
+const onAccordion2Click = () => {
+  accordionHeader2.nextElementSibling.classList.toggle('active');
+
+  accordionHeader1.nextElementSibling.classList.remove('active');
+  accordionHeader3.nextElementSibling.classList.remove('active');
+}
+
+const onAccordion3Click = () => {
+  accordionHeader3.nextElementSibling.classList.toggle('active');
+
+  accordionHeader1.nextElementSibling.classList.remove('active');
+  accordionHeader2.nextElementSibling.classList.remove('active');
+}
+
+accordionHeader1.onclick = onAccordion1Click;
+accordionHeader2.onclick = onAccordion2Click;
+accordionHeader3.onclick = onAccordion3Click;
+
+// 2. Universal function
+
+const onAccordionClick = (event) => {
+  event.target.nextElementSibling.classList.toggle('active');
+
+  const accordionList = document.querySelectorAll('.accordion-header');
+  accordionList.forEach(item => {
+    if (item !== event.target) {
+      item.nextElementSibling.classList.remove('active');
+    }
+  });
+}
+
+accordionHeader1.onclick = onAccordionClick;
+accordionHeader2.onclick = onAccordionClick;
+accordionHeader3.onclick = onAccordionClick;
+
+// 3. Universal function with loop
+
+const accordionList = document.querySelectorAll('.accordion-header');
+
+accordionList.forEach(item => {
+  item.onclick = (event) => {
+    accordionList.forEach(itemNested => {
+      if (itemNested !== event.target) {
+        itemNested.nextElementSibling.classList.remove('active');
+      } else {
+        itemNested.nextElementSibling.classList.toggle('active');
+      }
+    })
+  }
+});
