456895d430cfe707d039a69678c702eb8da25824
Sara Stjernberg <sara_stjernberg@hotmail.com>
2020-09-04 19:00:42 +0200

    hero image

--- code/script.js
+++ code/script.js
@@ -12,5 +12,28 @@ const accordionHeader1 = document.getElementById("accordion-header-1");
 const accordionHeader2 = document.getElementById("accordion-header-2");
 const accordionHeader3 = document.getElementById("accordion-header-3");
 
-console.log("accordionHeader1", "accordionHeader2", "accordionHeader3");
-alert("hey");
+accordionHeader1.onclick = () => {
+    const accordionTextContainer1 = accordionHeader1.nextElementSibling;
+    accordionTextContainer1.classList.toggle('active');
+
+    accordionHeader2.nextElementSibling.classList.remove('active');
+    accordionHeader3.nextElementSibling.classList.remove('active');
+
+
+}
+
+accordionHeader2.onclick = () => {
+    const accordionTextContainer2 = accordionHeader2.nextElementSibling;
+    accordionTextContainer2.classList.toggle('active');
+
+    accordionHeader1.nextElementSibling.classList.remove('active');
+    accordionHeader3.nextElementSibling.classList.remove('active');
+
+}
+
+accordionHeader3.onclick = () => {
+    const accordionTextContainer3 = accordionHeader3.nextElementSibling;
+    accordionTextContainer3.classList.toggle('active');
+    accordionHeader1.nextElementSibling.classList.remove('active');
+    accordionHeader2.nextElementSibling.classList.remove('active');
+}
