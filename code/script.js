97df991292168c39937bdbecd88a78c0ea56542e
Therese <Therese@Thereses-MacBook-Air.local>
2020-09-04 09:01:33 +0200

    update with functional accordion

--- code/script.js
+++ code/script.js
@@ -1,21 +1,31 @@
 const FAQHeader1 = document.getElementById('FAQ-Header-1');
-const FAQHeader1 = document.getElementById('FAQ-Header-2');
-const FAQHeader1 = document.getElementById('FAQ-Header-3');
+const FAQHeader2 = document.getElementById('FAQ-Header-2');
+const FAQHeader3 = document.getElementById('FAQ-Header-3');
 
 console.log(FAQHeader1, FAQHeader2, FAQHeader3);
 console.dir(FAQHeader1, FAQHeader2, FAQHeader3);
 
-FAQHeader1.onclick = () => {
-  const FAQAnswerContainer1 = FAQAnswerContainer1.nextElementSibling;
-  FAQAnswerContainer1.classList.toggle('active');
+const onFAQ1Click = () => {
+  FAQHeader1.nextElementSibling.classList.toggle('active');
+
+  FAQHeader2.nextElementSibling.classList.remove('active');
+  FAQHeader3.nextElementSibling.classList.remove('active');
+} 
+
+const onFAQ2Click = () => {
+  FAQHeader2.nextElementSibling.classList.toggle('active');
+
+  FAQHeader1.nextElementSibling.classList.remove('active');
+  FAQHeader3.nextElementSibling.classList.remove('active');
 }
 
-FAQHeader2.onclick = () => {
-  const FAQAnswerContainer1 = FFAQAnswerContainer2.nextElementSibling;
-  FAQAnswerContainer2.classList.toggle('active');
+const onFAQ3Click =() => {
+  FAQHeader3.nextElementSibling.classList.toggle('active');
+
+  FAQHeader1.nextElementSibling.classList.remove('active');
+  FAQHeader2.nextElementSibling.classList.remove('active');
 }
 
-FAQHeader3.onclick = () => {
-  const FAQAnswerContainer2 = FAQAnswerContainer3.nextElementSibling;
-  FAQAnswerContainer3.classList.toggle('active');
-}
+FAQHeader1.onclick = onFAQ1Click;
+FAQHeader2.onclick = onFAQ2Click;
+FAQHeader3.onclick = onFAQ3Click;
