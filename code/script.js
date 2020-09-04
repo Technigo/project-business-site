66b435a5b3cc98fc15fbf8025d4496c7783585e9
emeliesv <emeliesv@live.se>
2020-09-04 11:24:55 +0200

    added required to form

--- code/script.js
+++ code/script.js
@@ -1,7 +1,8 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-const sectionOne = document.getElementById('section1');
+
+/*const sectionOne = document.getElementById('section1');
 const sectionTwo = document.getElementById('section2');
 const sectionThree = document.getElementById('section3');
 const sectionFour = document.getElementById('section4');
@@ -9,17 +10,37 @@ const questions = document.querySelectorAll('.question');
 
 function toggle(event) {
   this.classList.toggle("active");
-  /* questions.forEach(question => {
+  questions.forEach(question => {
     if (question !== event.target){
       question.classList.remove('active');
     } else {
       question.classList.toggle('active');
     }
-  }) */
+  })
 }
  
 sectionOne.onclick = toggle
 sectionTwo.onclick = toggle
 sectionThree.onclick = toggle
-sectionFour.onclick = toggle
+sectionFour.onclick = toggle*/
 
+/*Youtube: 23:23*/
+const accordionQuestions = document.querySelectorAll('.question');
+
+accordionQuestions.forEach(accordionQuestion => {
+  accordionQuestion.addEventListener('click', event => {
+    
+    
+    accordionQuestion.classList.toggle('active');
+    const accordionAnswer = accordionQuestion.nextElementSibling;
+    if(accordionAnswer.classList.contains('active')){
+      accordionAnswer.style.maxHeight = accordionAnswer.scrollHeight + "px";
+    }
+   
+
+    const currentQuestion = document.querySelector('.question.active');
+    if(currentQuestion && currentQuestion!==accordionQuestion) {
+      currentQuestion.classList.toggle('active');
+    }
+  })
+})
