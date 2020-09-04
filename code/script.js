e2363a771a5c8ae2b4e13eecb076b9593d135937
emeliesv <emeliesv@live.se>
2020-09-04 14:19:12 +0200

    Changed the header

--- code/script.js
+++ code/script.js
@@ -1,30 +1,4 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
 
-/*const sectionOne = document.getElementById('section1');
-const sectionTwo = document.getElementById('section2');
-const sectionThree = document.getElementById('section3');
-const sectionFour = document.getElementById('section4');
-const questions = document.querySelectorAll('.question');
-
-function toggle(event) {
-  this.classList.toggle("active");
-  questions.forEach(question => {
-    if (question !== event.target){
-      question.classList.remove('active');
-    } else {
-      question.classList.toggle('active');
-    }
-  })
-}
- 
-sectionOne.onclick = toggle
-sectionTwo.onclick = toggle
-sectionThree.onclick = toggle
-sectionFour.onclick = toggle*/
-
-/*Youtube: 23:23*/
 const accordionQuestions = document.querySelectorAll('.question');
 
 accordionQuestions.forEach(accordionQuestion => {
@@ -44,3 +18,5 @@ accordionQuestions.forEach(accordionQuestion => {
     }
   })
 })
+/*I've got a problem with the if-condition not working as I want it to.
+It works at first and then it stops.*/
