8de8f58b3785abfd22d5915b87b82af047e8c1fb
MindstormingAB <s.elander@mindstorming.se>
2020-09-05 13:50:11 +0200

    Responsiveness and fall back image

--- code/script.js
+++ code/script.js
@@ -49,18 +49,6 @@ question5.onclick = () => {
   answer1.classList.remove('active');
 }
 
-
-
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section1").onclick = toggle
-
 //Sticky nav in Javascript
 const nav = document.querySelector('nav');
 const navOffset = nav.offsetTop;
