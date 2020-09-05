dfda175cdb832e86c9fc017f7457d9a26ea7614c
Hanna Skeppe <hannaskeppe@gmail.com>
2020-09-05 23:15:00 +0200

    removed comments in code and fonts in html

--- code/script.js
+++ code/script.js
@@ -22,38 +22,3 @@ accordionItemHeaders.forEach(accordionItemHeader => {
     }
   });
 });
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-// section1.onclick = toggle
-
-// function toggle() {
-//   this.classList.toggle("active")
-// }
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section1").onclick = toggle
-
-// const accordionHeader1 = document.getElementById('accordion-header-1');
-// const accordionHeader2 = document.getElementById('accordion-header-2');
-// const accordionHeader3 = document.getElementById('accordion-header-3');
-
-// accordionHeader1.onclick = () => {
-
-// const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-// toggle a class .active on click:
-// accordionTextContainer1.classList.toggle('active')
-// 
-// }
-// accordionHeader1.onclick = () => {
-// const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-// toggle a class .active on click:
-// accordionTextContainer2.classList.toggle('active')
-// }
-// accordionHeader1.onclick = () => {
-// const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-// toggle a class .active on click:
-// accordionTextContainer3.classList.toggle('active')
-// }
-// document.querySelectorAll(.answer);
-// document.getElementsByClassName(.answer);
