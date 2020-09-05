03faaf2fcb7b43494c643da3e49c4f632c0bc9af
Rebkova <rebkova@gmail.com>
2020-09-05 09:08:43 +0200

    Added header, logo, styled textarea and button

--- code/script.js
+++ code/script.js
@@ -1,28 +1,9 @@
 // A function that adds and remove the class "active" on the section you click on.
-// const questionVariables = document.querySelectorAll(".question");
-
-// questionVariables.forEach(questionVariable => {
-//   questionVariable.addEventListener("click", event => {
-//     questionVariable.classList.toggle(".active");
-//   });
-// }); 
-
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-
-// function toggle() {
-//   this.classList.toggle("active")
-// }
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section1").onclick = toggle
-
+//ACCORDION: 
 const accordionSection1 = document.getElementById("section1");
 const accordionSection2 = document.getElementById("section2");
 const accordionSection3 = document.getElementById("section3");
 const accordionSection4 = document.getElementById("section4");
-console.log(accordionSection1, accordionSection2);
-console.dir(accordionSection1, accordionSection2);
 
 accordionSection1.onclick = () => {
   const answerContainer1 = accordionSection1.nextElementSibling;
@@ -43,3 +24,38 @@ accordionSection1.onclick = () => {
   const answerContainer4 = accordionSection4.nextElementSibling;
   answerContainer4.classList.toggle("active");
  }
+
+ //STICKY HEADER:
+// const stickyHeader = document.querySelector(".sticky-header");
+// const headerOffset = stickyHeader.offsetTop;
+
+// window.onscroll = () => {
+//   if (headerOffset > window.pageYOffset) {
+//     stickyHeader.classList.remove("sticky");
+//   } else {
+//     stickyHeader.classList.add("sticky");
+//   }
+// }
+
+
+
+
+
+// const questionVariables = document.querySelectorAll(".question");
+
+// questionVariables.forEach(questionVariable => {
+//   questionVariable.addEventListener("click", event => {
+//     questionVariable.classList.toggle(".active");
+//   });
+// }); 
+
+// We haven't really talked about ´this´ yet, but we will... ;)
+// console.log(this) to see how it works
+
+// function toggle() {
+//   this.classList.toggle("active")
+// }
+
+// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+// document.getElementById("section1").onclick = toggle
+
