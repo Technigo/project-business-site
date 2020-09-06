59a8185e4bb34aeb98056bd1c66a5d20d042e214
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2020-09-06 15:47:47 +0200

    Finished product

--- code/script.js
+++ code/script.js
@@ -1,9 +1,4 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-//function toggle() {
-  //this.classList.toggle("active")
-//}
+
 
 
 
@@ -13,36 +8,7 @@ const accordionHeader2 = document.getElementById('accordion-header-2');
 const accordionHeader3 = document.getElementById('accordion-header-3');
 
 
-/* accordionHeader1.onclick = () => {
-    const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-    accordionTextContainer1.classList.toggle('active');
 
-    const accordionTextContainer2 = accordionHeader2.nextElementSibling;
-    const accordionTextContainer3 = accordionHeader3.nextElementSibling;
-    accordionTextContainer2.classList.remove('active');
-    accordionTextContainer3.classList.remove('active');
-}
-
-accordionHeader2.onclick = () => {
-  const accordionTextContainer2 = accordionHeader2.nextElementSibling;
-  accordionTextContainer2.classList.toggle('active');
-
-
-  const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-    const accordionTextContainer3 = accordionHeader3.nextElementSibling;
-    accordionTextContainer1.classList.remove('active');
-    accordionTextContainer3.classList.remove('active');
-}
-
-accordionHeader3.onclick = () => {
-  const accordionTextContainer3 = accordionHeader3.nextElementSibling;
-  accordionTextContainer3.classList.toggle('active');
-
-  const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-    const accordionTextContainer2 = accordionHeader2.nextElementSibling;
-    accordionTextContainer1.classList.remove('active');
-    accordionTextContainer2.classList.remove('active');
-} */
 
 const onAccordion1Click = () => {
   accordionHeader1.nextElementSibling.classList.toggle('active');
