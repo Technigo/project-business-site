edd69808b63d55ca24448730cd9fccda2bb4f7ea
Sara Stjernberg <sara_stjernberg@hotmail.com>
2020-09-06 20:42:38 +0200

    final commit

--- code/script.js
+++ code/script.js
@@ -1,34 +1,30 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-// function toggle() {
-//   this.classList.toggle("active")
-// }
+// Set the date for check in
+const checkIn = document.querySelector('#check-in');
+const date = new Date();
+checkIn.value = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) +
+    '-' + date.getDate().toString().padStart(2, 0);
 
-// // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section1").onclick = toggle
 
+//Accordion. Toggles between the different headers.
 const accordionHeader1 = document.getElementById("accordion-header-1");
 const accordionHeader2 = document.getElementById("accordion-header-2");
 const accordionHeader3 = document.getElementById("accordion-header-3");
+const accordionHeader4 = document.getElementById("accordion-header-4");
 
 accordionHeader1.onclick = () => {
     const accordionTextContainer1 = accordionHeader1.nextElementSibling;
     accordionTextContainer1.classList.toggle('active');
-
     accordionHeader2.nextElementSibling.classList.remove('active');
     accordionHeader3.nextElementSibling.classList.remove('active');
-
-
+    accordionHeader4.nextElementSibling.classList.remove('active');
 }
 
 accordionHeader2.onclick = () => {
     const accordionTextContainer2 = accordionHeader2.nextElementSibling;
     accordionTextContainer2.classList.toggle('active');
-
     accordionHeader1.nextElementSibling.classList.remove('active');
     accordionHeader3.nextElementSibling.classList.remove('active');
-
+    accordionHeader4.nextElementSibling.classList.remove('active');
 }
 
 accordionHeader3.onclick = () => {
@@ -36,4 +32,13 @@ accordionHeader3.onclick = () => {
     accordionTextContainer3.classList.toggle('active');
     accordionHeader1.nextElementSibling.classList.remove('active');
     accordionHeader2.nextElementSibling.classList.remove('active');
+    accordionHeader4.nextElementSibling.classList.remove('active');
+}
+
+accordionHeader4.onclick = () => {
+    const accordionTextContainer4 = accordionHeader4.nextElementSibling;
+    accordionTextContainer4.classList.toggle('active');
+    accordionHeader1.nextElementSibling.classList.remove('active');
+    accordionHeader2.nextElementSibling.classList.remove('active');
+    accordionHeader3.nextElementSibling.classList.remove('active');
 }
