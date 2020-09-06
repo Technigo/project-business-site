2b129344d8e15a5585dade49647332c4f54a2f84
Petra Almgren <petra.eriksson@live.com>
2020-09-06 21:32:45 +0200

    first draft

--- code/script.js
+++ code/script.js
@@ -1,9 +1,50 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+const accordionHeader1 = document.getElementById("accordion-header1");
+const accordionHeader2 = document.getElementById("accordion-header2");
+const accordionHeader3 = document.getElementById("accordion-header3");
+const accordionHeader4 = document.getElementById("accordion-header4");
+const accordionHeader5 = document.getElementById("accordion-header5");
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+accordionHeader1.onclick = () => {
+  accordionHeader1.nextElementSibling.classList.toggle("active");
+
+  accordionHeader2.nextElementSibling.classList.remove("active");
+  accordionHeader3.nextElementSibling.classList.remove("active");
+  accordionHeader4.nextElementSibling.classList.remove("active");
+  accordionHeader5.nextElementSibling.classList.remove("active");
+};
+
+accordionHeader2.onclick = () => {
+  accordionHeader2.nextElementSibling.classList.toggle("active");
+
+  accordionHeader1.nextElementSibling.classList.remove("active");
+  accordionHeader3.nextElementSibling.classList.remove("active");
+  accordionHeader4.nextElementSibling.classList.remove("active");
+  accordionHeader5.nextElementSibling.classList.remove("active");
+};
+
+accordionHeader3.onclick = () => {
+  accordionHeader3.nextElementSibling.classList.toggle("active");
+
+  accordionHeader1.nextElementSibling.classList.remove("active");
+  accordionHeader2.nextElementSibling.classList.remove("active");
+  accordionHeader4.nextElementSibling.classList.remove("active");
+  accordionHeader5.nextElementSibling.classList.remove("active");
+};
+
+accordionHeader4.onclick = () => {
+  accordionHeader4.nextElementSibling.classList.toggle("active");
+
+  accordionHeader1.nextElementSibling.classList.remove("active");
+  accordionHeader2.nextElementSibling.classList.remove("active");
+  accordionHeader3.nextElementSibling.classList.remove("active");
+  accordionHeader5.nextElementSibling.classList.remove("active");
+};
+
+accordionHeader5.onclick = () => {
+  accordionHeader5.nextElementSibling.classList.toggle("active");
+
+  accordionHeader1.nextElementSibling.classList.remove("active");
+  accordionHeader2.nextElementSibling.classList.remove("active");
+  accordionHeader3.nextElementSibling.classList.remove("active");
+  accordionHeader4.nextElementSibling.classList.remove("active");
+};
