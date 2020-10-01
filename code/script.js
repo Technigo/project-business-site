22c8305d3647407641eaaeef65a01a7b3825b9ed
Therese Lindén <theresemlinden@gmail.com>
2020-10-01 15:36:16 +0200

    changed naming of classes, removed linebreaks and comments from js file.

--- code/script.js
+++ code/script.js
@@ -1,15 +1,10 @@
 const header = document.querySelector('.header-bar');
 const headerOffset = header.offsetTop;
 
-console.dir(header);
-console.log(header);
-
 window.onscroll = () => {
     if (headerOffset >= window.pageYOffset) {
         header.classList.remove('sticky');
-    }   else {
-        header.classList.add('sticky');
-    }
+    } else header.classList.add('sticky');
 }
 
 const accordionList = document.querySelectorAll('.accordion-header');
@@ -27,28 +22,3 @@ accordionList.forEach(item => {
     })
   } 
 })
-
-
-
-
-
-
-
-
-//console.log(header);
-//console.dir(header);
-  
-
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-//function toggle() {
-  //this.classList.toggle("active")
-  //console.log(this)
-//}
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-//document.getElementById("accordion-header-1").onclick = toggle
-//document.getElementById("accordion-header-2").onclick = toggle
-//document.getElementById("accordion-header-3").onclick = toggle
-
