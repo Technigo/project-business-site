13e49b316848435f2a73289f1f83f59f53c72e0e
Rasmus Persson <rrasmuspersson@gmail.com>
2021-01-23 10:22:18 +0100

    added media querys for tablet and desktop

--- code/script.js
+++ code/script.js
@@ -10,9 +10,12 @@ document.getElementById("section1").onclick = toggle
 
 
 /* Hamgurger menu start */
+
+/* I select the Navbar and Ham elements using query selector */
 let navbar = document.querySelector(".navbar")
 let ham = document.querySelector(".ham")
 
+/* I added an Event Listener click to toggle the menu */
 ham.addEventListener("click", toggleHamburger)
 
 // toggles hamburger menu in and out when clicking on the hamburger
@@ -21,8 +24,10 @@ function toggleHamburger(){
   ham.classList.toggle("showClose")
 }
 
+/* I select the links in the hamburger meny */
 let menuLinks = document.querySelectorAll(".menuLink")
 
+/* I added an event listener click to check is any of the links are clicked */
 menuLinks.forEach( 
   function(menuLink) { 
     menuLink.addEventListener("click", toggleHamburger) 
