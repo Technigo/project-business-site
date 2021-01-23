86627d55410970561d161630878782427a7bb921
Rasmus Persson <rrasmuspersson@gmail.com>
2021-01-23 11:10:49 +0100

    added structure for a singup form

--- code/script.js
+++ code/script.js
@@ -1,14 +1,3 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-
-
 /* Hamgurger menu start */
 
 /* I select the Navbar and Ham elements using query selector */
