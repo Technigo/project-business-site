a605414e3f872434bef5f7c7ab370089e698ed36
Julia Ros <70485921+julialeonieros@users.noreply.github.com>
2021-01-25 15:52:34 +0100

    Added accordion with three questions

--- code/script.js
+++ code/script.js
@@ -1,16 +1,15 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-// function toggle() {
-//   this.classList.toggle("active")
-// }
+// 
 
-// // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section1").onclick = toggle
+function toggle() {
+  this.classList.toggle("active")
+}
 
-const accordionHeader = document.getElementById('accordion-header');
-console.log(accordionHeader);
+// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+document.getElementById("question-1").onclick = toggle
 
-accordionHeader.addEventListener('click', () => {
-  console.log('clicked"');
-})
+document.getElementById("question-2").onclick = toggle
+
+document.getElementById("question-3").onclick = toggle
