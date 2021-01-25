4c12ef95f4b088a0563ff64dacb55f27e482c2c3
Pauan8 <pauline.j.andersson@vgregion.se>
2021-01-25 21:52:53 +0100

    added js for first accordion

--- code/script.js
+++ code/script.js
@@ -1,3 +1,16 @@
+const accordionDisplay1 = document.getElementById("accordionDisplay1");
+const accordionExpand1 = document.getElementById("accordionExpand1")
+
+accordionDisplay1.addEventListener("click", function toggle() {
+accordionExpand1.classList.toggle("accordion__expand--selected")
+})
+
+console.log(accordionDisplay1);
+
+
+
+
+
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
