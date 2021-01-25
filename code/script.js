1bf5bcb260f80422a891ee6d0acd4d0dc88798b3
Ann-Sofi Jönsson <annsofi.jonsson@hotmail.se>
2021-01-25 19:38:52 +0100

    added link to css & javascript

--- code/script.js
+++ code/script.js
@@ -7,3 +7,5 @@ function toggle() {
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+
+console.log(this);
