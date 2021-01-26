79ba2090accb10148b88adbf890eddcb53537712
[Darcy Winterhill] <[darcy.winterhill@gmail.com]>
2021-01-26 16:02:30 +0100

    commit

--- code/script.js
+++ code/script.js
@@ -10,31 +10,31 @@ function toggle() {
 
 //document.getElementById("signUp").onClick = toggle
 
-const accordionHeader1 = document.getElementById('accordion-header1');
+const accordionQuestion1 = document.getElementById('accordion-question1');
 const accordionContent1 = document.getElementById('accordion-content1');
 
-accordionHeader1.addEventListener('click', () => {
+accordionQuestion1.addEventListener('click', () => {
   accordionContent1.classList.toggle('accordion-selected');
 })
 
-const accordionHeader2 = document.getElementById('accordion-header2');
+const accordionQuestion2 = document.getElementById('accordion-question2');
 const accordionContent2 = document.getElementById('accordion-content2');
 
-accordionHeader2.addEventListener('click', () => {
+accordionQuestion2.addEventListener('click', () => {
   accordionContent2.classList.toggle('accordion-selected');
 })
 
-const accordionHeader3 = document.getElementById('accordion-header3');
+const accordionQuestion3 = document.getElementById('accordion-question3');
 const accordionContent3 = document.getElementById('accordion-content3');
 
-accordionHeader3.addEventListener('click', () => {
+accordionQuestion3.addEventListener('click', () => {
   accordionContent3.classList.toggle('accordion-selected');
 })
 
-const accordionHeader4 = document.getElementById('accordion-header4');
+const accordionQuestion4 = document.getElementById('accordion-question4');
 const accordionContent4 = document.getElementById('accordion-content4');
 
-accordionHeader4.addEventListener('click', () => {
+accordionQuestion4.addEventListener('click', () => {
   accordionContent4.classList.toggle('accordion-selected');
 })
 
