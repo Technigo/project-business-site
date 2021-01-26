12ef6612579761eb9874ca455ef7ee57b9fbd2ad
Pauan8 <pauline.j.andersson@vgregion.se>
2021-01-26 02:27:57 +0100

    fixed a dynamic +/- button for the accordion expand

--- code/script.js
+++ code/script.js
@@ -1,16 +1,17 @@
 let accordionDisplay = document.getElementsByClassName("accordion__display");
 let accordionExpand = document.getElementsByClassName("accordion__expand");
+let accordionButton = document.getElementsByClassName("accordion__button");
 
 for (let i = 0; i < accordionDisplay.length; i++) {
   accordionDisplay[i].addEventListener("click", () => {
     accordionExpand[i].classList.toggle("accordion__expand--selected");
     
-    accordionExpand[i] = this.nextElementSibling;
-    if (accordionExpand[i].style.display === "block") {
-      this.style.display = "none";
-    } else {
-      accordionExpand[i].style.display = "block";
-    }
+    if(accordionExpand[i].classList.contains("accordion__expand--selected")){
+      accordionButton[i].textContent="-";}
+    else {
+        accordionButton[i].textContent="+";
+      }
+    
   });
 }
 
