32b30f1eacc88950987f58aeef0ae22b023db3c6
Pauan8 <pauline.j.andersson@vgregion.se>
2021-01-26 20:41:09 +0100

    added comments and added some content to accordion

--- code/script.js
+++ code/script.js
@@ -1,11 +1,12 @@
-let accordionDisplay = document.getElementsByClassName("accordion__display");
-let accordionExpand = document.getElementsByClassName("accordion__expand");
-let accordionButton = document.getElementsByClassName("accordion__button");
+let accordionDisplay = document.querySelectorAll(".accordion__display");
+let accordionExpand = document.querySelectorAll(".accordion__expand");
+let accordionButton = document.querySelectorAll(".accordion__button");
 
+//loops through the array of multiple objects of the same class and toggles the --selected class on and off on mouseclick //
 for (let i = 0; i < accordionDisplay.length; i++) {
   accordionDisplay[i].addEventListener("click", () => {
     accordionExpand[i].classList.toggle("accordion__expand--selected");
-    
+    //if the selected class is active the expand/collapse button has a minus, otherwise + //
     if(accordionExpand[i].classList.contains("accordion__expand--selected")){
       accordionButton[i].textContent="-";}
     else {
