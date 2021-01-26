c53a4a3ad0975f347d0b3620323784c866fa5420
Your Name <isabellamarkskog@live.se>
2021-01-26 22:58:26 +0100

    Added images and code to the general structure

--- code/script.js
+++ code/script.js
@@ -5,5 +5,6 @@ function toggle() {
   this.classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+// Selects an HTML element, and calls a function which will be executed 
+// when the element is clicked.
 document.getElementById("section1").onclick = toggle
