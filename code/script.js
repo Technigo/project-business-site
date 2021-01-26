e2f6407f2416370f0c4b532d8615a7dead1c28cc
Theresehag <tezzinthecity@gmail.com>
2021-01-27 00:08:34 +0100

    Add accordions with a first styling and functionality

--- code/script.js
+++ code/script.js
@@ -1,9 +1,23 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+const accordionHeaderOne = document.getElementById('accordion-header-1');
+const accordionContentOne = document.getElementById('accordion-content-1');
+console.log(accordionHeaderOne);
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+accordionHeaderOne.addEventListener('click',()=> {
+  accordionContentOne.classList.toggle('accordion-selected');
+})
+
+const accordionHeaderTwo = document.getElementById('accordion-header-2');
+const accordionContentTwo = document.getElementById('accordion-content-2');
+console.log(accordionHeaderTwo);
+
+accordionHeaderTwo.addEventListener('click',()=> {
+  accordionContentTwo.classList.toggle('accordion-selected');
+})
+
+const accordionHeaderThree = document.getElementById('accordion-header-3');
+const accordionContentThree = document.getElementById('accordion-content-3');
+console.log(accordionHeaderThree);
+
+accordionHeaderThree.addEventListener('click',()=> {
+  accordionContentThree.classList.toggle('accordion-selected');
+})
