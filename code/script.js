f607506ee7216de489d1cd6c9e56c450dd9d23e6
Mir Albertioni <fethullah.dincer@gmail.com>
2021-01-26 14:27:30 +0100

    Har lag till plus/minus tecken

--- code/script.js
+++ code/script.js
@@ -19,14 +19,45 @@ accordionHeaderOne.addEventListener('click', () => {
     accordionContentOne.classList.toggle("accordion-selected")
 });
 
+
+/* This is the code for the minus sign after clicking on the Question */
+accordionHeaderOne.addEventListener('click', () => {
+    accordionHeaderOne.classList.toggle("active")
+});
+
+
+
+
+
+
 accordionHeaderTwo.addEventListener('click', () => {
     accordionContentTwo.classList.toggle("accordion-selected")
 });
+
+/* This is the code for the minus sign after clicking on the Question */
+accordionHeaderTwo.addEventListener('click', () => {
+    accordionHeaderTwo.classList.toggle("active")
+});
+
+
+
 
 accordionHeaderThree.addEventListener('click', () => {
     accordionContentThree.classList.toggle("accordion-selected")
 });
 
+/* This is the code for the minus sign after clicking on the Question */
+accordionHeaderThree.addEventListener('click', () => {
+    accordionHeaderThree.classList.toggle("active")
+});
+
+
+
 accordionHeaderFour.addEventListener('click', () => {
     accordionContentFour.classList.toggle("accordion-selected")
+});
+
+/* This is the code for the minus sign after clicking on the Question */
+accordionHeaderFour.addEventListener('click', () => {
+    accordionHeaderFour.classList.toggle("active")
 });
