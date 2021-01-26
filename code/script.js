bb6a5aee66f993767ff182cca76d6d4072aea555
Ati Takrami <ati.takrami@gmail.com>
2021-01-26 15:10:05 +0100

    add header and cover video for desktop and first section

--- code/script.js
+++ code/script.js
@@ -1,9 +1,8 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
+function transformSection() {
+  last_known_scroll_position = window.scrollY;
+  var element = document.getElementById("first-box");
+  element.classList.add("transform-sec");
+  if (last_known_scroll_position == 0) {
+    element.classList.remove("transform-sec");
+  }
 }
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
