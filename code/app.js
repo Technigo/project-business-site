937a4fa1885e3a0d3d008de52fde0236bc523682
Mabel Liaw <Owner@Mabels-MacBook-Pro.local>
2021-01-27 09:48:24 +0100

    added accord in js, style css, and index

---
+++ code/app.js
@@ -0,0 +1,23 @@
+const accordionHeaderOne = document.getElementById('accordion-header-1');
+const accordionContentOne = document.getElementById('accordion-content-1');
+
+console.log(accordionHeaderOne);
+
+accordionHeaderOne.addEventListener('click', () => {
+    accordionContentOne.classList.toggle('accordion-selected');
+});
+
+const accordionHeaderTwo = document.getElementById('accordion-header-2');
+const accordionContentTwo = document.getElementById('accordion-content-2');
+
+accordionHeaderTwo.addEventListener('click', () => {
+    accordionContentTwo.classList.toggle('accordion-selected');
+});
+
+const accordionHeaderThree = document.getElementById('accordion-header-3');
+const accordionContentThree = document.getElementById('accordion-content-3');
+
+
+accordionHeaderThree.addEventListener('click', () => {
+    accordionContentThree.classList.toggle('accordion-selected');
+});
