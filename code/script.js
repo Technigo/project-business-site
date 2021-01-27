cb8d6c9b124d278e46f38e3f7d6b770ce1d9febe
johlofg <jh.lofgren@gmail.com>
2021-01-27 17:01:34 +0100

    Styled my form and accordion with css and also created some flexboxes for placement

--- code/script.js
+++ code/script.js
@@ -2,7 +2,7 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 //function toggle() {
- // this.classList.toggle("active")
+// this.classList.toggle("active")
 //}
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
@@ -14,27 +14,40 @@
 const question1 = document.getElementById('question1');
 const question2 = document.getElementById('question2');
 const question3 = document.getElementById('question3');
-const accSvar = document.getElementById('acc-svar')
-const accSvar1 = document.getElementById('acc-svar1')
-const accSvar2 = document.getElementById('acc-svar2')
-const showMore =document.getElementById('question1')
+const accSvar1 = document.getElementById('acc-svar')
+const accSvar2 = document.getElementById('acc-svar1')
+const accSvar3 = document.getElementById('acc-svar2')
+const showMore1 =document.getElementById('question1')
+const showMore2 =document.getElementById('question2')
+const showMore3 =document.getElementById('question3')
 //Måste får till att den byter tillbaka
-showMore.onclick = () => {
-  question1.innerHTML = "-"
+showMore1.onclick = () => {
+  showMore1.innerHTML = "˄";  
+}
+showMore2.onclick = () => {
+  showMore2.innerHTML = "˄";  
+}
+showMore3.onclick = () => {
+  showMore3.innerHTML = "˄";  
 }
 
 
 question1.addEventListener('click',() => {
-  accSvar.classList.toggle('acc-svar-klick')
-  
+  accSvar1.classList.toggle('acc-svar-klick')
+  accSvar2.classList.remove('acc-svar-klick')
+  accSvar3.classList.remove('acc-svar-klick')
 })
 
 question2.addEventListener('click',() => {
-  accSvar1.classList.toggle('acc-svar-klick')
+  accSvar1.classList.remove('acc-svar-klick')
+  accSvar2.classList.toggle('acc-svar-klick')
+  accSvar3.classList.remove('acc-svar-klick')
 })
 
 question3.addEventListener('click',() => {
-  accSvar2.classList.toggle('acc-svar-klick')
+  accSvar1.classList.remove('acc-svar-klick')
+  accSvar2.classList.remove('acc-svar-klick')
+  accSvar3.classList.toggle('acc-svar-klick')
 })
 
 
