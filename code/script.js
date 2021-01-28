b3d912f989ed0bfac44fa4552e20b64d40081aad
johlofg <jh.lofgren@gmail.com>
2021-01-28 16:09:51 +0100

    Indentation & spacing looks good, now Im gonna change the conventions

--- code/script.js
+++ code/script.js
@@ -1,12 +1,13 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-//function toggle() {
-// this.classList.toggle("active")
-//}
+/* A function that adds and remove the class "active" on the section you click on.
+We haven't really talked about ´this´ yet, but we will... ;)
+console.log(this) to see how it works
+function toggle() {
+this.classList.toggle("active")
+}
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-//document.getElementById("section1").onclick = toggle
+Selects an HTML element, and calls a function which will be executed when the element is clicked.
+document.getElementById("section1").onclick = toggle
+*/
 
 
 //Skapar myHeader elementet från html-filen 
