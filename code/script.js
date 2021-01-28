a01d141484a8fbd1675b05d7ec4b6cc918bdfb70
Ati Takrami <ati.takrami@gmail.com>
2021-01-28 12:29:26 +0100

    add accordion part

--- code/script.js
+++ code/script.js
@@ -6,3 +6,30 @@ function transformSection() {
     element.classList.remove("transform-sec");
   }
 }
+
+const accordionHeader1 = document.getElementById("accordion-header-1");
+const accordionContent1 = document.getElementById("accordion-content-1");
+
+const accordionHeader2 = document.getElementById("accordion-header-2");
+const accordionContent2 = document.getElementById("accordion-content-2");
+
+const accordionHeader3 = document.getElementById("accordion-header-3");
+const accordionContent3 = document.getElementById("accordion-content-3");
+
+accordionHeader1.addEventListener("click", () => {
+  accordionContent1.classList.toggle("accordion-selected");
+  accordionContent2.classList.remove("accordion-selected");
+  accordionContent3.classList.remove("accordion-selected");
+});
+
+accordionHeader2.addEventListener("click", () => {
+  accordionContent1.classList.remove("accordion-selected");
+  accordionContent2.classList.toggle("accordion-selected");
+  accordionContent3.classList.remove("accordion-selected");
+});
+
+accordionHeader3.addEventListener("click", () => {
+  accordionContent1.classList.remove("accordion-selected");
+  accordionContent2.classList.remove("accordion-selected");
+  accordionContent3.classList.toggle("accordion-selected");
+});
