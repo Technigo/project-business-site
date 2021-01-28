a4142f06244ffa36e3f50affbcb592e8f80e7b51
Anna L <annalenksjo@gmail.com>
2021-01-28 14:29:40 +0100

    basic accordion

--- code/script.js
+++ code/script.js
@@ -1,9 +1,30 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+
+let accordionHeader = document.getElementById("accordionHeader");
+let accordionContent = document.getElementById("accordionContent");
+let accordionHeaderTwo = document.getElementById("accordionHeaderTwo");
+let accordionContentTwo = document.getElementById("accordionContentTwo");
+
+
+console.log(accordionHeader);
+console.log(accordionContent);
+console.log(accordionHeaderTwo);
+
+accordionHeader.addEventListener("click", () => {
+ accordionContent.classList.toggle("active"); 
+})
+
+accordionHeaderTwo.addEventListener("click", () => {
+accordionContentTwo.classList.toggle("activeTwo");
+})
+
+
+
+//function toggle() {
+ // this.classList.toggle("active")
+//}
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// document.getElementById("section").onclick = toggle
