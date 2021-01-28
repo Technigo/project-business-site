8d4f08b5a5ceec33805945ce9c44da592ad7ce34
Bellsibub <isabella.billgren@gmail.com>
2021-01-28 16:01:39 +0100

    refined the accordion toggle logic

--- code/script.js
+++ code/script.js
@@ -9,21 +9,30 @@ accordion.addEventListener("click", (event) => {
   if (!question.classList.contains("accordion__question")) {
     return;
   }
-  // if no previous questions are open then just open the target
+  // run the toggle only once if there is no previous question
   if (currentQuestion === undefined) {
-    //set new current question and open the answer
     toggleAccordionItem(question, answer);
     currentQuestion = question;
     return;
   }
+  // Toggle logic for various use cases
   if (currentQuestion === question) {
+    // user clicked the currentQuestion while it was open => close it.
     toggleAccordionItem(currentQuestion, currentQuestion.nextElementSibling);
+    if (!currentQuestion.classList.contains("accordion__question--open")) {
+      // user closed the currentQuestion and we must reset it
+      currentQuestion = undefined;
+      return;
+    }
   } else {
+    //   user clicked on an unopened question => open it & close the previous question
+    toggleAccordionItem(currentQuestion, currentQuestion.nextElementSibling);
     toggleAccordionItem(question, answer);
     currentQuestion = question;
   }
 });
 
+// Function to toggle the accordion open/close
 const toggleAccordionItem = (question, answer) => {
   question.classList.toggle("accordion__question--open");
   answer.classList.toggle("accordion__answer--open");
