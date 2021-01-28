da306546e2b2365b676155b4fe4e26355c2c4c4f
Sara Carlstein <carlstein.sara@gmail.com>
2021-01-28 17:18:05 +0100

    new changes

--- code/script.js
+++ code/script.js
@@ -1,16 +1,56 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
+
+
+//1 get the icon ids' and store as variables
+const minusElement1 = document.querySelector('#minus1')
+const plusElement1 = document.querySelector('#plus1')
+
+const minusElement2 = document.querySelector('#minus2')
+const plusElement2 = document.querySelector('#plus2')
+
+const minusElement3 = document.querySelector('#minus3')
+const plusElement3 = document.querySelector('#plus3')
+
+//function adds and remove active on toogle, same for the plus an minus icons. 
+//where minus icon styled with none as default and plus display block 
+//and adds the classes .show .hide on toggle
+
+function toggleOne() {
   this.classList.toggle("active")
+  minusElement1.classList.toggle('show')
+  plusElement1.classList.toggle('hide')
 }
 
-function remove() {
-  this.classList.remove("active")
-}
+function toggleTwo() {
+ this.classList.toggle("active")
+ minusElement2.classList.toggle('show')
+ plusElement2.classList.toggle('hide')
+ }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-document.getElementById("section2").onclick = toggle
-document.getElementById("section3").onclick = toggle
+ function toggleThree() {
+  this.classList.toggle("active")
+  minusElement3.classList.toggle('show')
+  plusElement3.classList.toggle('hide')
+  }
+ 
+// function toggleBurger(){
+//   this.classList.toggle("open")
+//   console.log("hello")
+
+// }
+
+function clickAlert() {
+  alert("Welcome to Julia's🙋‍♀️. View your inbox for a starting treat👀🍲")
+}
+// Selects an HTML element, and calls a function 
+//which will be executed when the element is clicked.
+document.querySelector("#section1").onclick = toggleOne
+document.querySelector("#section2").onclick = toggleTwo
+document.querySelector("#section3").onclick = toggleThree
+document.querySelector(".button-sign-up").onclick = clickAlert
+// document.querySelector("#burger").onclick = toggleBurger
+
+
 
