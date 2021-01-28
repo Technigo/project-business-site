9ab7f3455af90c8fff95b9b712fadb881bb88298
Mir Albertioni <fethullah.dincer@gmail.com>
2021-01-28 13:02:09 +0100

    adding more faq questions

--- code/script.js
+++ code/script.js
@@ -17,12 +17,18 @@ const accordionContentFour = document.getElementById("accordion-content-4");
 
 accordionHeaderOne.addEventListener('click', () => {
     accordionContentOne.classList.toggle("accordion-selected")
+    accordionContentTwo.classList.remove("accordion-selected")
+    accordionContentThree.classList.remove("accordion-selected")
+    accordionContentFour.classList.remove("accordion-selected")
 });
 
 
 /* This is the code for the minus sign after clicking on the Question */
 accordionHeaderOne.addEventListener('click', () => {
     accordionHeaderOne.classList.toggle("active")
+    accordionHeaderTwo.classList.remove("active")
+    accordionHeaderThree.classList.remove("active")
+    accordionHeaderFour.classList.remove("active")
 });
 
 
@@ -31,33 +37,51 @@ accordionHeaderOne.addEventListener('click', () => {
 
 
 accordionHeaderTwo.addEventListener('click', () => {
+    accordionContentOne.classList.remove("accordion-selected")
     accordionContentTwo.classList.toggle("accordion-selected")
+    accordionContentThree.classList.remove("accordion-selected")
+    accordionContentFour.classList.remove("accordion-selected")
 });
 
 /* This is the code for the minus sign after clicking on the Question */
 accordionHeaderTwo.addEventListener('click', () => {
+    accordionHeaderOne.classList.remove("active")
     accordionHeaderTwo.classList.toggle("active")
+    accordionHeaderThree.classList.remove("active")
+    accordionHeaderFour.classList.remove("active")
 });
 
 
 
 
 accordionHeaderThree.addEventListener('click', () => {
+    accordionContentOne.classList.remove("accordion-selected")
+    accordionContentTwo.classList.remove("accordion-selected")
     accordionContentThree.classList.toggle("accordion-selected")
+    accordionContentFour.classList.remove("accordion-selected")
 });
 
 /* This is the code for the minus sign after clicking on the Question */
 accordionHeaderThree.addEventListener('click', () => {
+    accordionHeaderOne.classList.remove("active")
+    accordionHeaderTwo.classList.remove("active")
     accordionHeaderThree.classList.toggle("active")
+    accordionHeaderFour.classList.remove("active")
 });
 
 
 
 accordionHeaderFour.addEventListener('click', () => {
+    accordionContentOne.classList.remove("accordion-selected")
+    accordionContentTwo.classList.remove("accordion-selected")
+    accordionContentThree.classList.remove("accordion-selected")
     accordionContentFour.classList.toggle("accordion-selected")
 });
 
 /* This is the code for the minus sign after clicking on the Question */
 accordionHeaderFour.addEventListener('click', () => {
+    accordionHeaderOne.classList.remove("active")
+    accordionHeaderTwo.classList.remove("active")
+    accordionHeaderThree.classList.remove("active")
     accordionHeaderFour.classList.toggle("active")
 });
