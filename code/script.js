6e982c7856ba545814d4a1b1e30894684c2b4f85
Caroline Byström <frivridnadagar@gmail.com>
2021-01-28 15:54:11 +0100

    fix accordion, test form and add simple footer

--- code/script.js
+++ code/script.js
@@ -1,14 +1,3 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-/*function toggle() {
-  this.classList.toggle("active")
-  console.log(this)
-}
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle*/
-
 const accordionHeaderOne = document.getElementById('accordion-header-1');
 const accordionContentOne = document.getElementById('accordion-content-1');
 
@@ -36,4 +25,10 @@ accordionHeaderThree.addEventListener('click', () => {
   accordionContentThree.classList.toggle('accordion-selected');
 });
 
+function toggleActive() {
+  this.classList.add('active')
+} 
 
+accordionHeaderOne.addEventListener('click', toggleActive)
+accordionHeaderTwo.addEventListener('click', toggleActive)
+accordionHeaderThree.addEventListener('click', toggleActive)
