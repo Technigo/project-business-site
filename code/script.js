679845d920a2cda1606f4cd67e3fc4682fd667b0
Your Name <isabellamarkskog@live.se>
2021-01-29 17:13:02 +0100

    Added code to FAQ-section

--- code/script.js
+++ code/script.js
@@ -1,39 +1,28 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
+// ACCORDION & "+" AND "-" ICONS //
 
-// function toggle() {
-//   this.classList.toggle("active")
-// }
-
-// Selects an HTML element, and calls a function which will be executed 
-// when the element is clicked.
-//document.getElementById("section1").onclick = toggle
-
-
-//selects all .question
-
-
-// ACCORDION //
-
-const faqQuestions = document.querySelectorAll(".accordion .question"); 
+const faqQuestions = document.querySelectorAll('.accordion .question'); 
 faqQuestions.forEach(addClickEvent);
 
 function addClickEvent(question) {
   question.addEventListener('click', () => {
     question.classList.toggle('active');
+    const plus = question.querySelector('.question-plus');
+    plus.classList.toggle('hide');
+    const minus = question.querySelector('.question-minus');
+    minus.classList.toggle('hide');
   });
 }
 
 
+
 // HAMBURGER //
 
-const navigation = document.querySelector(".navigation")
-const hamburger = document.querySelector(".hamburger")
+const navigation = document.querySelector('.navigation');
+const ham = document.querySelector('.ham');
 
-hamburger.addEventListener("click", toggleHamburger)
+ham.addEventListener('click', toggleHamburger);
 
-function toggleHamburger(){
-  navigation.classList.toggle("open-menu")
-  hamburger.classList.toggle("close-menu")
+function toggleHamburger() {
+  navigation.classList.toggle('open-menu');
+  ham.classList.toggle('close-menu');
 }
