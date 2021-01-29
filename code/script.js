911ee582243f8390a87678a927486277d30ef4b4
Aepera <astridepersson@gmail.com>
2021-01-29 14:00:09 +0100

    Hero image and accordion added. Form styled

--- code/script.js
+++ code/script.js
@@ -3,7 +3,10 @@
 // console.log(this) to see how it works
 function toggle() {
   this.classList.toggle("active")
+  console.log(this)
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+document.getElementById("section2").onclick = toggle
+document.getElementById("section3").onclick = toggle
