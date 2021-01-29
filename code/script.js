eee7363ef4f183a6606541f2c3749672712b019a
Andrea Larsson <andrea.e.larsson@hotmail.se>
2021-01-29 08:15:20 +0100

    update: styled my submit button

--- code/script.js
+++ code/script.js
@@ -1,12 +1,4 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-//function toggle() {
- // this.classList.toggle("active")
-//}
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-//document.getElementById("section1").onclick = toggle
+//JavaScript for the Accordion
 
 const accordionHeader1 = document.getElementById("accordion-header-1");
 const accordionBody1 = document.getElementById("accordion-body-1");
