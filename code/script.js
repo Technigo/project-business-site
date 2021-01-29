6ebe2b0c11086903a469c00aa79c18d0dead0e9a
Åsa Hildebrand <71731072+AsaHildebrand@users.noreply.github.com>
2021-01-29 09:58:49 +0100

    changes

--- code/script.js
+++ code/script.js
@@ -6,4 +6,33 @@ function toggle() {
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("accordion-header-1").onclick = toggle
+
+const accordionHeaderOne = document.getElementById("accordion-header-1");
+const accordionContentOne = document.getElementById("accordion-content-1");
+
+const accordionHeaderTwo = document.getElementById("accordion-header-2");
+const accordionContentTwo = document.getElementById("accordion-content-2");
+
+const accordionHeaderThree = document.getElementById("accordion-header-3");
+const accordionContentThree = document.getElementById("accordion-content-3");
+
+console.log(accordionHeaderOne);
+
+accordionHeaderOne.addEventListener("click", () => {
+  accordionContentOne.classList.add("accordion-selected");
+  accordionContentTwo.classList.remove("accordion-selected");
+  accordionContentThree.classList.remove("accordion-selected");
+});
+
+accordionHeaderTwo.addEventListener("click", () => {
+  accordionContentOne.classList.remove("accordion-selected");
+  accordionContentTwo.classList.add("accordion-selected");
+  accordionContentThree.classList.remove("accordion-selected");
+});
+
+accordionHeaderThree.addEventListener("click", () => {
+  accordionContentOne.classList.remove("accordion-selected");
+  accordionContentTwo.classList.remove("accordion-selected");
+  accordionContentThree.classList.add("accordion-selected");
+});
