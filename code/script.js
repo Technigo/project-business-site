56cced5b10366dedbabaf43b686c92bd864c44e0
Anna L <annalenksjo@gmail.com>
2021-01-29 12:27:42 +0100

    push before changing to hero picture

--- code/script.js
+++ code/script.js
@@ -1,7 +1,3 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-
 let accordionHeader = document.getElementById("accordionHeader");
 let accordionContent = document.getElementById("accordionContent");
 let accordionHeaderTwo = document.getElementById("accordionHeaderTwo");
@@ -19,12 +15,3 @@ accordionHeader.addEventListener("click", () => {
 accordionHeaderTwo.addEventListener("click", () => {
 accordionContentTwo.classList.toggle("activeTwo");
 })
-
-
-
-//function toggle() {
- // this.classList.toggle("active")
-//}
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section").onclick = toggle
