09dbf6d8cbc2c3d87e99370cf5cd698cb5611be9
Camilla Sandström <camillasandstrom@MacBook-Air-som-tillhor-Camilla.local>
2021-01-30 14:14:07 +0100

    added som pics, and treid to fix js

--- code/script.js
+++ code/script.js
@@ -9,25 +9,32 @@ function toggle() {
 document.getElementById('accordion-section-1').onclick = toggle
 
 
-const accordionHeader1 = document.getElementById('accordion-header-1');
-const accordionContent1 = document.getElementById('accordion-content-1');
+const accordionHeader1 = document.getElementById('accordion-section1');
+const accordionContent1 = document.getElementById('accordion-content1');
 
-const accordionHeader2 = document.getElementById('accordion-header-2');
-const accordionContent2 = document.getElementById('accordion-content-2');
+const accordionHeader2 = document.getElementById('accordion-section2');
+const accordionContent2 = document.getElementById('accordion-content2');
 
-const accordionHeader3 = document.getElementById('accordion-header-3');
-const accordionContent3 = document.getElementById('accordion-content-3');
+const accordionHeader3 = document.getElementById('accordion-section3');
+const accordionContent3 = document.getElementById('accordion-content3');
+
+const accordionHeader4 = document.getElementById('accordion-section4');
+const accordionContent4 = document.getElementById('accordion-content4');
 
 console.log(accordionHeaderOne);
 
-accordionHeade1.addEventListener('click', () => {
-    accordionContent1.classList.toggle('accordion-selected');
+accordionHeader1.addEventListener('click', () => {
+    accordionContent.classList.toggle('accordion-selected1');
 });
 
 accordionHeader2.addEventListener('click', () => {
-    accordionContent2.classList.toggle('accordion-selected');
+    accordionContent.classList.toggle('accordion-selected2');
 });
 
 accordionHeader3.addEventListener('click', () => {
-    accordionContent3.classList.toggle('accordion-selected');
+    accordionContent.classList.toggle('accordion-selected3');
+});
+
+accordionHeader4.addEventListener('click', () => {
+    accordionContent.classList.toggle('accordion-selected4');
 });
