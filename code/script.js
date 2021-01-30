836b16db6e9de6cf21d27b2a63352c589bd77ec2
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-01-30 19:39:49 +0100

    Basic structure and content without styling

--- code/script.js
+++ code/script.js
@@ -1,9 +1,35 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+
+// function toggle() {
+//   this.classList.toggle("active")
+// }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// document.getElementById("accordion1").onclick = toggle
+// document.getElementById("accordion2").onclick = toggle
+// document.getElementById("accordion3").onclick = toggle
+
+const accordionQuestion1 = document.getElementById("question1");
+const accordionQuestion2 = document.getElementById("question2");
+const accordionQuestion3 = document.getElementById("question3");
+
+const accordionAnswer1 = document.getElementById("answer1");
+const accordionAnswer2 = document.getElementById("answer2");
+const accordionAnswer3 = document.getElementById("answer3");
+
+accordionQuestion1.addEventListener("click", () => {
+  accordionQuestion1.classList.toggle("active");
+  accordionQuestion2.classList.remove("active");
+  accordionQuestion3.classList.remove("active");
+});
+
+accordionQuestion2.addEventListener("click", () => {
+  accordionQuestion2.classList.toggle("active");
+  accordionQuestion1.classList.remove("active");
+  accordionQuestion3.classList.remove("active");
+});
+
+accordionQuestion3.addEventListener("click", () => {
+  accordionQuestion3.classList.toggle("active");
+  accordionQuestion2.classList.remove("active");
+  accordionQuestion1.classList.remove("active");
+});
