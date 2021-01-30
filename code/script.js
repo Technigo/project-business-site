8c9b044fb14ecafb1a3d0134694245c074858d04
Anna Honkonen <ak.honkonen@gmail.com>
2021-01-30 15:02:26 +0200

    change javascript and styling

--- code/script.js
+++ code/script.js
@@ -7,16 +7,36 @@ const accordionAnswerSecond = document.getElementById('answer2');
 const accordionQuestionThird = document.getElementById('question3');
 const accordionAnswerThird = document.getElementById('answer3');
 
+const accordionQuestionFourth = document.getElementById('question4');
+const accordionAnswerFourth = document.getElementById('answer4');
+
 accordionQuestionFirst.addEventListener('click', () => {
   accordionAnswerFirst.classList.toggle('answer-clicked');
+  accordionAnswerSecond.classList.remove('answer-clicked');
+  accordionAnswerThird.classList.remove('answer-clicked');
+  accordionAnswerFourth.classList.remove('answer-clicked');
 });
 
 accordionQuestionSecond.addEventListener('click', () => {
+  accordionAnswerFirst.classList.remove('answer-clicked');
   accordionAnswerSecond.classList.toggle('answer-clicked');
+  accordionAnswerThird.classList.remove('answer-clicked');
+  accordionAnswerFourth.classList.remove('answer-clicked');
 });
 
 accordionQuestionThird.addEventListener('click', () => {
+  accordionAnswerFirst.classList.remove('answer-clicked');
+  accordionAnswerSecond.classList.remove('answer-clicked');
   accordionAnswerThird.classList.toggle('answer-clicked');
+  accordionAnswerFourth.classList.remove('answer-clicked');
 });
+
+accordionQuestionFourth.addEventListener('click', () => {
+  accordionAnswerFirst.classList.remove('answer-clicked');
+  accordionAnswerSecond.classList.remove('answer-clicked');
+  accordionAnswerThird.classList.remove('answer-clicked');
+  accordionAnswerFourth.classList.toggle('answer-clicked');
+});
+
 
 
