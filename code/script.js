ad9fc711fe23ab90af8de894d610ca2433f1ee3e
unknown <lindgren.anna.m@gmail.com>
2021-01-30 17:45:43 +0100

    CSS on the form, added more FAQ

--- code/script.js
+++ code/script.js
@@ -9,6 +9,7 @@ function toggle() {
 document.getElementById('section1').onclick = toggle;
 document.getElementById('section2').onclick = toggle;
 document.getElementById('section3').onclick = toggle;
+document.getElementById('section4').onclick = toggle;
 
 
 
