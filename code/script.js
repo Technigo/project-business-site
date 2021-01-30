9458e4201f63a4b75a849f78f10e774a48818941
Irina <jekabsone.irina@gmail.com>
2021-01-30 20:00:03 +0100

    Changes regarding accordion to make the code cleaner

--- code/script.js
+++ code/script.js
@@ -16,48 +16,7 @@ const answer4=document.getElementById("answer-4");
 const section5=document.getElementById("section5");
 const answer5=document.getElementById("answer-5");
 
-// A function that adds and remove the class "answer-selected" on the section clicked on.
-section1.addEventListener('click', () => {
-  answer1.classList.toggle("answer-selected");
-  answer2.classList.remove("answer-selected");
-  answer3.classList.remove("answer-selected");
-  answer4.classList.remove("answer-selected");
-  answer4.classList.remove("answer-selected");
-});
-
-section2.addEventListener('click', () => {
-  answer1.classList.remove("answer-selected");
-  answer2.classList.toggle("answer-selected");
-  answer3.classList.remove("answer-selected");
-  answer4.classList.remove("answer-selected");
-  answer5.classList.remove("answer-selected");
-});
-
-section3.addEventListener('click', () => {
-  answer1.classList.remove("answer-selected");
-  answer2.classList.remove("answer-selected");
-  answer3.classList.toggle("answer-selected");
-  answer4.classList.remove("answer-selected");
-  answer5.classList.remove("answer-selected");
-});
-
-section4.addEventListener('click', () => {
-  answer1.classList.remove("answer-selected");
-  answer2.classList.remove("answer-selected");
-  answer3.classList.remove("answer-selected");
-  answer4.classList.toggle("answer-selected");
-  answer5.classList.remove("answer-selected");
-});
-
-section5.addEventListener('click', () => {
-  answer1.classList.remove("answer-selected");
-  answer2.classList.remove("answer-selected");
-  answer3.classList.remove("answer-selected");
-  answer4.classList.remove("answer-selected");
-  answer5.classList.toggle("answer-selected");
-});
-
-// A function that adds "+" and "-" icon on the section clicked on.
+// A function that adds and remove the class "active" on the section clicked on.
 section1.addEventListener('click', () => {
   section1.classList.toggle('active');
 });
