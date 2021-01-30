4a129369af82973857059ee65a16b126469e8076
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-01-30 18:30:18 +0100

    Cleaned up commented styling and Javascript, added comments

--- code/script.js
+++ code/script.js
@@ -1,22 +1,6 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-// function toggle() {
-//   this.classList.toggle("question-clicked")
-// }
 
-// // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section1").onclick = toggle
 
-// const accordionQuestion = document.getElementById('section1')
-// const accordionAnswer = document.getElementById('answer1')
-
-// accordionQuestion.addEventListener('click', () => {
-//   // accordionQuestion.classList.toggle ('question-clicked');
-//   accordionQuestion.classList.toggle ('question-clicked');
-//   accordionAnswer.classList.toggle ('answer-open');
-// })
-
+// I am not 100% sure whether my indentation in Javascript is correct. Feels like something is off
 const accordionQuestion = document.querySelectorAll('.accordion-question');
 
 accordionQuestion.forEach(accordionHeader =>  {
