944802505801aad395f033a643c0c22e719ac80d
sirihemsley <siri.hemsley@gmail.com>
2021-01-30 14:51:17 +0100

    added script and css to rotate icon in accordion

--- code/script.js
+++ code/script.js
@@ -1,23 +1,3 @@
-
-const questionOne = document.getElementById('question-1');
-const answerOne = document.getElementById('answer-1');
-
-const questionTwo = document.getElementById('question-2');
-const answerTwo = document.getElementById('answer-2');
-
-const questionThree = document.getElementById('question-3');
-const answerThree = document.getElementById('answer-3');
-
-const questionFour = document.getElementById('question-4');
-const answerFour = document.getElementById('answer-4');
-
-const questionFive = document.getElementById('question-5');
-const answerFive = document.getElementById('answer-5');
-
-const questionSix = document.getElementById('question-6');
-const answerSix = document.getElementById('answer-6');
-
-
 const sliderOne = document.getElementById('myInvoices');
 const outputOne = document.getElementById('numberOfInvoices');
 outputOne.innerHTML = sliderOne.value;
@@ -32,11 +12,39 @@ outputTwo.innerHTML = sliderTwo.value;
 
 sliderTwo.oninput = function() {
   outputTwo.innerHTML = this.value;
-}
+} 
 
-console.log(answerOne);
+const questionOne = document.getElementById('question-1');
+const answerOne = document.getElementById('answer-1');
+const arrowOne = document.getElementById('arrow1')
+
+const questionTwo = document.getElementById('question-2');
+const answerTwo = document.getElementById('answer-2');
+const arrowTwo = document.getElementById('arrow2')
+
+const questionThree = document.getElementById('question-3');
+const answerThree = document.getElementById('answer-3');
+const arrowThree = document.getElementById('arrow3')
+
+const questionFour = document.getElementById('question-4');
+const answerFour = document.getElementById('answer-4');
+const arrowFour = document.getElementById('arrow4')
+
+const questionFive = document.getElementById('question-5');
+const answerFive = document.getElementById('answer-5');
+const arrowFive = document.getElementById('arrow5')
+
+const questionSix = document.getElementById('question-6');
+const answerSix = document.getElementById('answer-6');
+const arrowSix = document.getElementById('arrow6')
 
 questionOne.addEventListener('click', () => {
+  arrowOne.classList.toggle('rotate');
+  arrowTwo.classList.remove('rotate');
+  arrowThree.classList.remove('rotate');
+  arrowFour.classList.remove('rotate');
+  arrowFive.classList.remove('rotate');
+  arrowSix.classList.remove('rotate');
   answerOne.classList.toggle('active');
   answerTwo.classList.remove('active');
   answerThree.classList.remove('active');
@@ -46,6 +54,12 @@ questionOne.addEventListener('click', () => {
 })
 
 questionTwo.addEventListener('click', () => {
+  arrowOne.classList.remove('rotate');
+  arrowTwo.classList.toggle('rotate');
+  arrowThree.classList.remove('rotate');
+  arrowFour.classList.remove('rotate');
+  arrowFive.classList.remove('rotate');
+  arrowSix.classList.remove('rotate');
   answerOne.classList.remove('active');
   answerTwo.classList.toggle('active');
   answerThree.classList.remove('active');
@@ -55,6 +69,12 @@ questionTwo.addEventListener('click', () => {
 })
 
 questionThree.addEventListener('click', () => {
+  arrowOne.classList.remove('rotate');
+  arrowTwo.classList.remove('rotate');
+  arrowThree.classList.toggle('rotate');
+  arrowFour.classList.remove('rotate');
+  arrowFive.classList.remove('rotate');
+  arrowSix.classList.remove('rotate');
   answerOne.classList.remove('active');
   answerTwo.classList.remove('active');
   answerThree.classList.toggle('active');
@@ -64,6 +84,12 @@ questionThree.addEventListener('click', () => {
 })
 
 questionFour.addEventListener('click', () => {
+  arrowOne.classList.remove('rotate');
+  arrowTwo.classList.remove('rotate');
+  arrowThree.classList.remove('rotate');
+  arrowFour.classList.toggle('rotate');
+  arrowFive.classList.remove('rotate');
+  arrowSix.classList.remove('rotate');
   answerOne.classList.remove('active');
   answerTwo.classList.remove('active');
   answerThree.classList.remove('active');
@@ -73,6 +99,12 @@ questionFour.addEventListener('click', () => {
 })
 
 questionFive.addEventListener('click', () => {
+  arrowOne.classList.remove('rotate');
+  arrowTwo.classList.remove('rotate');
+  arrowThree.classList.remove('rotate');
+  arrowFour.classList.remove('rotate');
+  arrowFive.classList.toggle('rotate');
+  arrowSix.classList.remove('rotate');
   answerOne.classList.remove('active');
   answerTwo.classList.remove('active');
   answerThree.classList.remove('active');
@@ -82,6 +114,12 @@ questionFive.addEventListener('click', () => {
 })
 
 questionSix.addEventListener('click', () => {
+  arrowOne.classList.remove('rotate');
+  arrowTwo.classList.remove('rotate');
+  arrowThree.classList.remove('rotate');
+  arrowFour.classList.remove('rotate');
+  arrowFive.classList.remove('rotate');
+  arrowSix.classList.toggle('rotate');
   answerOne.classList.remove('active');
   answerTwo.classList.remove('active');
   answerThree.classList.remove('active');
