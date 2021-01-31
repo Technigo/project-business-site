35fb1a816494660eb4946b96879cdca02a2c42c6
Sandra Wallén <sandrawallen_9@hotmail.com>
2021-01-31 21:31:51 +0100

    Added contact section and comments

--- code/script.js
+++ code/script.js
@@ -79,10 +79,3 @@ faqHeaderSix.addEventListener('click', () => {
 });
 
 
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-
