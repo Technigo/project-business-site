25e58ef2226320bdb18e99711fd5e41cf1c5f258
Anna L <annalenksjo@gmail.com>
2021-01-31 19:46:59 +0100

    last changes

--- code/script.js
+++ code/script.js
@@ -1,8 +1,11 @@
 let accordionHeader = document.getElementById("accordionHeader");
 let accordionContent = document.getElementById("accordionContent");
+
 let accordionHeaderTwo = document.getElementById("accordionHeaderTwo");
 let accordionContentTwo = document.getElementById("accordionContentTwo");
 
+let accordionHeaderThree = document.getElementById("accordionHeaderThree")
+let accordionContentThree = document.getElementById("accordionContentThree")
 
 console.log(accordionHeader);
 console.log(accordionContent);
@@ -15,3 +18,7 @@ accordionHeader.addEventListener("click", () => {
 accordionHeaderTwo.addEventListener("click", () => {
 accordionContentTwo.classList.toggle("activeTwo");
 })
+
+accordionHeaderThree.addEventListener("click", () => {
+    accordionContentThree.classList.toggle("activeThree");
+    })
