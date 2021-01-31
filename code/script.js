72f7e8f91e1a7a4192e52bf92ae1b8359749c5ad
Martin <martno15@gmail.com>
2021-01-31 23:28:13 +0100

    changed everything

--- code/script.js
+++ code/script.js
@@ -1,9 +1,31 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+//Variable declaration
+const accordionHeaderOne = document.getElementById("accordion-header-1");
+const accordionContentOne = document.getElementById("accordion-content-1");
+
+const accordionHeaderTwo = document.getElementById("accordion-header-2");
+const accordionContentTwo = document.getElementById("accordion-content-2");
+
+const accordionHeaderThree = document.getElementById("accordion-header-3");
+const accordionContentThree = document.getElementById("accordion-content-3");
+
+//When clicking the first accordion header
+accordionHeaderOne.addEventListener("click",() => {
+    accordionContentOne.classList.toggle("accordion-selected");   //toggles the first accordion header
+    accordionContentTwo.classList.remove("accordion-selected");   //closes the second accordion header
+    accordionContentThree.classList.remove("accordion-selected"); //closes the third accordion header
+})
+
+//When clicking the second accordion header
+accordionHeaderTwo.addEventListener("click",() => {
+    accordionContentOne.classList.remove("accordion-selected")
+    accordionContentTwo.classList.toggle("accordion-selected");
+    accordionContentThree.classList.remove("accordion-selected");
+})
+
+//When clicking the third accordion header
+accordionHeaderThree.addEventListener("click",() => {
+    accordionContentOne.classList.remove("accordion-selected")
+    accordionContentTwo.classList.remove("accordion-selected");
+    accordionContentThree.classList.toggle("accordion-selected");
+})
