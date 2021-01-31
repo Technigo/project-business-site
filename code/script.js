8b92850fcadb15ac8985e7c80e35bcf42ff16e93
Theresehag <tezzinthecity@gmail.com>
2021-01-31 11:18:48 +0100

    improve intendentions

--- code/script.js
+++ code/script.js
@@ -1,36 +1,27 @@
-const accordionHeaderOne = document.getElementById('accordion-header-1');
-const accordionContentOne = document.getElementById('accordion-content-1');
-const accordionHeaderTwo = document.getElementById('accordion-header-2');
-const accordionContentTwo = document.getElementById('accordion-content-2');
-const accordionHeaderThree = document.getElementById('accordion-header-3');
-const accordionContentThree = document.getElementById('accordion-content-3');
+const accordionHeaderOne = document.getElementById("accordion-header-1");
+const accordionContentOne = document.getElementById("accordion-content-1");
+const accordionHeaderTwo = document.getElementById("accordion-header-2");
+const accordionContentTwo = document.getElementById("accordion-content-2");
+const accordionHeaderThree = document.getElementById("accordion-header-3");
+const accordionContentThree = document.getElementById("accordion-content-3");
 console.log(accordionHeaderOne);
 console.log(accordionHeaderTwo);
 console.log(accordionHeaderThree);
 
-
-
-accordionHeaderOne.addEventListener('click',()=> {
-  accordionContentOne.classList.toggle('accordion-selected');
-  accordionContentTwo.classList.remove('accordion-selected');
-  accordionContentThree.classList.remove('accordion-selected');
+accordionHeaderOne.addEventListener("click", () => {
+  accordionContentOne.classList.toggle("accordion-selected");
+  accordionContentTwo.classList.remove("accordion-selected");
+  accordionContentThree.classList.remove("accordion-selected");
 });
 
-accordionHeaderTwo.addEventListener('click', () => {
-  accordionContentOne.classList.remove('accordion-selected');
-  accordionContentTwo.classList.toggle('accordion-selected');
-  accordionContentThree.classList.remove('accordion-selected');
+accordionHeaderTwo.addEventListener("click", () => {
+  accordionContentOne.classList.remove("accordion-selected");
+  accordionContentTwo.classList.toggle("accordion-selected");
+  accordionContentThree.classList.remove("accordion-selected");
 });
 
-accordionHeaderThree.addEventListener('click', () => {
-  accordionContentOne.classList.remove('accordion-selected');
-  accordionContentTwo.classList.remove('accordion-selected');
-  accordionContentThree.classList.toggle('accordion-selected');
+accordionHeaderThree.addEventListener("click", () => {
+  accordionContentOne.classList.remove("accordion-selected");
+  accordionContentTwo.classList.remove("accordion-selected");
+  accordionContentThree.classList.toggle("accordion-selected");
 });
-
-
-
-
-
-
-
