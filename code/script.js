f7780a8d56cb726fe9b8e8393efb6cdc378c8652
Elaine Bergstrom <elainebergstrom26@gmail.com>
2021-01-31 22:53:27 +0100

    working on accordian FAQ and then adjusting it with Form

--- code/script.js
+++ code/script.js
@@ -1,9 +1,21 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+const question1 = document.getElementById('question-1');
+const answer1 = document.getElementById('answer-1');
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+const question2 = document.getElementById('question-2');
+const answer2 = document.getElementById('answer-2');
+
+const question3 = document.getElementById('question-3');
+const answer3 = document.getElementById('answer-3');
+
+
+question1.addEventListener('click', () => {
+    answer1.classList.toggle('answer-selected');
+});
+
+question2.addEventListener('click', () => {
+    answer2.classList.toggle('answer-selected');
+});
+
+question3.addEventListener('click', () => {
+    answer3.classList.toggle('answer-selected');
+});
