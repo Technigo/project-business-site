c30a448dec7c4ff3e5f213c2e03b1e3c1c7030c1
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-01-30 20:12:47 -0600

    added missing script

--- code/script.js
+++ code/script.js
@@ -1,57 +1,19 @@
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
-const accordionHeader1 = document.getElementById('accordion-header-1');
-const accordionHeader2 = document.getElementById('accordion-header-2');
-const accordionHeader3 = document.getElementById('accordion-header-3');
-const accordionHeader4 = document.getElementById('accordion-header-4');
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    
+   
 
-console.log (accordionHeader1, accordionHeader2, accordionHeader3, accordionHeader4);
-console.dir (accordionHeader1, accordionHeader2, accordionHeader3, accordionHeader4);
-
-accordionHeader1.onclick = () => {
-  const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-  accordionTextContainer1.classList.toggle ('active');
-
-  const accordionTextContainer2 = accordionHeader2.nextElementSibling;
-  const accordionTextContainer3 = accordionHeader3.nextElementSibling;
-  const accordionTextContainer4 = accordionHeader4.nextElementSibling;
-  accordionTextContainer2.classList.remove('active');
-  accordionTextContainer3.classList.remove('active');
-  accordionTextContainer4.classList.remove('active');
-}
-
-
-accordionHeader2.onclick = () => {
-  const accordionTextContainer2 = accordionHeader2.nextElementSibling;
-  accordionTextContainer2.classList.toggle ('active');
-
-  const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-  const accordionTextContainer3 = accordionHeader3.nextElementSibling;
-  const accordionTextContainer4 = accordionHeader4.nextElementSibling;
-  accordionTextContainer1.classList.remove('active');
-  accordionTextContainer3.classList.remove('active');
-  accordionTextContainer4.classList.remove('active'); 
-}
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+    
+  });
+});
  
-   accordionHeader3.onclick = () => {
-  const accordionTextContainer3 = accordionHeader3.nextElementSibling;
-  accordionTextContainer3.classList.toggle ('active');
-
-  const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-  const accordionTextContainer2 = accordionHeader2.nextElementSibling;
-  const accordionTextContainer4 = accordionHeader4.nextElementSibling;
-  accordionTextContainer1.classList.remove('active');
-  accordionTextContainer2.classList.remove('active');
-  accordionTextContainer4.classList.remove('active');
-}
-
-accordionHeader4.onclick = () => {
-  const accordionTextContainer4 = accordionHeader4.nextElementSibling;
-  accordionTextContainer4.classList.toggle ('active');
-
-  const accordionTextContainer1 = accordionHeader1.nextElementSibling;
-  const accordionTextContainer2 = accordionHeader2.nextElementSibling;
-  const accordionTextContainer3 = accordionHeader3.nextElementSibling;
-  accordionTextContainer1.classList.remove('active');
-  accordionTextContainer2.classList.remove('active');
-  accordionTextContainer3.classList.remove('active');
-} 
