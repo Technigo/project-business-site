68e4a1b043dc531c84e44a46baf007ecab0cbf9e
Lars Söderman <lars.soderman@gmail.com>
2021-01-31 23:30:08 +0100

    final tweaks, got the accordion to work..

--- README.md
+++ README.md
@@ -1,30 +1,16 @@
 # Business Site
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This is an excercise in forms, hero-images/video and JavaScript accordion. I chose to make a kind of a knockoff of wetransfer (volley.wetransfer.com) - a file transfer service. I liked the idea of a simple site with the "hero" image filling the entire background. 
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I started with throwing together a Figma (https://www.figma.com/file/w6zXREqVRS47es8lFaQhoM/Business-site?node-id=0%3A1) - i then spent a lot of time getting the background image to work as intended, later on i changed to a background video instead - that had me spending many hours trying to get the video to fill the entire window, until i realized the video i was using har "letterbox" black areas in the top and botttom, so it turned out it was in the source material and not in my code :/ 
+I also spent many hours trying to figure out why some accordion elements were not clickable, until i realized it was the hidden accordion content that was in the way even though invisible. So i managed to solve that with z-index. 
+I really had hoped to be able to do a lot better with this weeks project, but i guess i didnt plan my time well enough for that. Some things i would have liked to have done are:
+- Get the background video to place itself centered horizontally. There is also a bug when you chrome inspect the video element goes out to the sides.
+- I did this project not at all mobile first, so that is something i should have planned better from the start. It turned out to be really tricky dowsizing to mobile from this awkward full screen debacle
+- I would have liked to do the form much nicer, with animations and effects, i also wanted to do a custom popup info thing and validation feedback as on wetransfer on active inputs. I would have liked to try fieldset and legend on the form. And custom checkbox.
+
 
 ## View it live
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
-
-
-## To do
-<!-- - Background image working
-- top menu and bottom logo
-- Form - text, textfield, radio button, file upload, submit  -->
-<!-- - Accordion, w changing icon -->
-<!-- - Accordion animation -->
-<!-- - video background - change video -->
-- responsive below 600
-- make nice animations in form ( and try fieldset?)
-- style checkbox
-- background image better breakpoints
-- focus effect on inputs
-- every other accordion row different color
-- cursor change (red level)
-- form validation (and feedback)
-- more input types?
+https://adoring-hugle-335540.netlify.app/
--- code/script.js
+++ code/script.js
@@ -9,76 +9,53 @@
 // document.getElementById("section1").onclick = toggle
 
 
-const accordionOne = document.getElementById('accordion-header-1')
-const accordionTwo = document.getElementById('accordion-header-2')
-const accordionThree = document.getElementById('accordion-header-3')
-const accordionFour = document.getElementById('accordion-header-4')
-const accordionFive = document.getElementById('accordion-header-5')
+const accordionOne = document.getElementById('accordion-1')
+const accordionTwo = document.getElementById('accordion-2')
+const accordionThree = document.getElementById('accordion-3')
+const accordionFour = document.getElementById('accordion-4')
+const accordionFive = document.getElementById('accordion-5')
 
-console.log(accordionOne, accordionTwo, accordionThree, accordionFour, accordionFive)
+const accordionHeaderOne = document.getElementById('accordion-header-1')
+const accordionHeaderTwo = document.getElementById('accordion-header-2')
+const accordionHeaderThree = document.getElementById('accordion-header-3')
+const accordionHeaderFour = document.getElementById('accordion-header-4')
+const accordionHeaderFive = document.getElementById('accordion-header-5')
 
 
-
-accordionOne.addEventListener('click', () => {
+accordionHeaderOne.addEventListener('click', () => {
   accordionOne.classList.toggle('active');
   accordionTwo.classList.remove('active');
   accordionThree.classList.remove('active');
   accordionFour.classList.remove('active');
   accordionFive.classList.remove('active');
-  console.log('1');
 });
-accordionTwo.addEventListener('click', () => {
+accordionHeaderTwo.addEventListener('click', () => {
   accordionOne.classList.remove('active');
   accordionTwo.classList.toggle('active');
   accordionThree.classList.remove('active');
   accordionFour.classList.remove('active');
   accordionFive.classList.remove('active');
-  console.log('2');
 });
-
-// accordionThree.addEventListener('click', () => {
-//   console.log('3');
-// });
-
-// accordionFour.addEventListener('click', () => {
-//   console.log('4');
-// });
-
-// accordionFive.addEventListener('click', () => {
-//   console.log('5');
-// });
-
-accordionThree.addEventListener('click', () => {
+accordionHeaderThree.addEventListener('click', () => {
   accordionOne.classList.remove('active');
   accordionTwo.classList.remove('active');
   accordionThree.classList.toggle('active');
   accordionFour.classList.remove('active');
   accordionFive.classList.remove('active');
-  console.log('3');
 });
-accordionThree.addEventListener('click', () => {
-  accordionOne.classList.remove('active');
-  accordionTwo.classList.remove('active');
-  accordionThree.classList.toggle('active');
-  accordionFour.classList.remove('active');
-  accordionFive.classList.remove('active');
-  console.log(accordionThree);
-});
-accordionFour.addEventListener('click', () => {
+accordionHeaderFour.addEventListener('click', () => {
   accordionOne.classList.remove('active');
   accordionTwo.classList.remove('active');
   accordionThree.classList.remove('active');
   accordionFour.classList.toggle('active');
   accordionFive.classList.remove('active');
-  console.log('4');
 });
-accordionFive.addEventListener('click', () => {
+accordionHeaderFive.addEventListener('click', () => {
   accordionOne.classList.remove('active');
   accordionTwo.classList.remove('active');
   accordionThree.classList.remove('active');
   accordionFour.classList.remove('active');
   accordionFive.classList.toggle('active');
-  console.log('5');
   });
   
 const video = document.getElementById('bg-video');
