4d2d8519c963ff1330f585045871aedac8259b96
petercreations <hello@petercreations.com>
2021-01-31 08:39:06 +0100

    First changes to all source files

---
+++ code/app.js
@@ -0,0 +1,19 @@
+
+
+// Faq Accordion // 
+
+const acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
