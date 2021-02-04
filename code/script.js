6c3d08e91e863ae7969cf7c51d1b1242b9ded94f
Rasmus Persson <rrasmuspersson@gmail.com>
2021-02-04 11:31:56 +0100

    Reviewed the PR and made the changes accordingly.

--- code/script.js
+++ code/script.js
@@ -1,22 +1,22 @@
 /* Hamgurger menu start */
 
 /* I select the Navbar and Ham elements using query selector */
-let navbar = document.querySelector(".navbar")
-let ham = document.querySelector(".ham")
+const navbar = document.querySelector(".navbar")
+const hamburger = document.querySelector(".ham")
 
 /* I added an Event Listener click to toggle the menu */
-ham.addEventListener("click", toggleHamburger)
+hamburger.addEventListener("click", toggleHamburger)
 
 // toggles hamburger menu in and out when clicking on the hamburger
 function toggleHamburger(){
   navbar.classList.toggle("showNav")
-  ham.classList.toggle("showClose")
+  hamburger.classList.toggle("showCloseSign")
 }
 
 /* I select the links in the hamburger meny */
-let menuLinks = document.querySelectorAll(".menuLink")
+const menuLinks = document.querySelectorAll(".menuLink")
 
-/* I added an event listener click to check is any of the links are clicked */
+/* I added an event listener click to check if any of the links are clicked */
 menuLinks.forEach( 
   function(menuLink) { 
     menuLink.addEventListener("click", toggleHamburger) 
@@ -28,8 +28,8 @@ menuLinks.forEach(
 /* Accordion start */
 
   /* Question 1 start */
-let accordionQuestionOne = document.getElementById("accordionQuestionOne");
-let accordionQuestionOneParagraph = document.getElementById("accordionQuestionOneParagraph");
+const accordionQuestionOne = document.getElementById("accordionQuestionOne");
+const accordionQuestionOneParagraph = document.getElementById("accordionQuestionOneParagraph");
 
 accordionQuestionOne.addEventListener("click", () => {
   accordionQuestionOneParagraph.classList.toggle("accordionParagraphContentShown");
@@ -38,8 +38,8 @@ accordionQuestionOne.addEventListener("click", () => {
 
 
   /* Question 2 start */
-let accordionQuestionTwo = document.getElementById("accordionQuestionTwo");
-let accordionQuestionTwoParagraph = document.getElementById("accordionQuestionTwoParagraph");
+const accordionQuestionTwo = document.getElementById("accordionQuestionTwo");
+const accordionQuestionTwoParagraph = document.getElementById("accordionQuestionTwoParagraph");
 
 accordionQuestionTwo.addEventListener("click", () => {
   accordionQuestionTwoParagraph.classList.toggle("accordionParagraphContentShown");
@@ -47,8 +47,8 @@ accordionQuestionTwo.addEventListener("click", () => {
 /* Question 2 end */
 
   /* Question 3 start */
-  let accordionQuestionThree = document.getElementById("accordionQuestionThree");
-  let accordionQuestionThreeParagraph = document.getElementById("accordionQuestionThreeParagraph");
+  const accordionQuestionThree = document.getElementById("accordionQuestionThree");
+  const accordionQuestionThreeParagraph = document.getElementById("accordionQuestionThreeParagraph");
   
   accordionQuestionThree.addEventListener("click", () => {
     accordionQuestionThreeParagraph.classList.toggle("accordionParagraphContentShown");
