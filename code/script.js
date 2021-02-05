2e5c17063830e56fdb4a23549fd5738512113719
Maria Bergman <maria.bergman@elektroskandia.se>
2021-02-05 18:49:25 +0100

    Changes according to code review

--- code/script.js
+++ code/script.js
@@ -1,14 +1,13 @@
-var acc = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = `${panel.scrollHeight}px`;
     } 
   });
 }
