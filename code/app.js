1a684f30f3d74af02f5d0979c14d8c832dda377b
petercreations <hello@petercreations.com>
2021-02-06 17:29:44 +0100

    code review changes

--- code/app.js
+++ code/app.js
@@ -2,13 +2,13 @@
 
 // Faq Accordion // 
 
-const acc = document.getElementsByClassName("accordion");
+const accordion = document.getElementsByClassName("accordion");
 let i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    const panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
