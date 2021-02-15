c425b1df5dfcbfd70005a89fd323f74085e2076d
Andrea Larsson <andrea.e.larsson@hotmail.se>
2021-02-15 21:39:40 +0100

    update

--- code/script.js
+++ code/script.js
@@ -1,18 +1,12 @@
 //JavaScript for the Accordion
-
 const accordionHeader1 = document.getElementById("accordion-header-1");
 const accordionBody1 = document.getElementById("accordion-body-1");
-
-
 
 const accordionHeader2 = document.getElementById("accordion-header-2");
 const accordionBody2 = document.getElementById("accordion-body-2");
 
-
 const accordionHeader3 = document.getElementById("accordion-header-3");
 const accordionBody3 = document.getElementById("accordion-body-3");
-
-
 
 accordionHeader1.addEventListener("click", () => {
   accordionBody1.classList.toggle("accordion-selected");
