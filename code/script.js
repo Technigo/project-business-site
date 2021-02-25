5e2ab4fa2c42bc855c0b9475cb0cc57ad8fac4fd
“[ida <[ida.t.sylven@gmail.com]>
2021-02-25 12:22:23 +0100

    Changed HTML elements for better semantics. Removed unused code and fixed weird indentations

--- code/script.js
+++ code/script.js
@@ -1,36 +1,34 @@
-const accordionQuestionOne = document.getElementById('accordion-question-1');
+const accordionQuestionOne = document.getElementById('accordion-question-1')
 const accordionAnswerOne = document.getElementById('accordion-answer-1')
 
-const accordionQuestionTwo = document.getElementById('accordion-question-2');
+const accordionQuestionTwo = document.getElementById('accordion-question-2')
 const accordionAnswerTwo = document.getElementById('accordion-answer-2')
 
-const accordionQuestionThree = document.getElementById('accordion-question-3');
+const accordionQuestionThree = document.getElementById('accordion-question-3')
 const accordionAnswerThree = document.getElementById('accordion-answer-3')
 
-const accordionQuestionFour = document.getElementById('accordion-question-4');
+const accordionQuestionFour = document.getElementById('accordion-question-4')
 const accordionAnswerFour = document.getElementById('accordion-answer-4')
 
-const accordionQuestionFive = document.getElementById('accordion-question-5');
+const accordionQuestionFive = document.getElementById('accordion-question-5')
 const accordionAnswerFive = document.getElementById('accordion-answer-5')
 
-console.log(accordionQuestionOne);
-
 accordionQuestionOne.addEventListener('click', () => {
-    accordionAnswerOne.classList.toggle('accordion-selected');
+    accordionAnswerOne.classList.toggle('accordion-selected')
 });
 
 accordionQuestionTwo.addEventListener('click', () => {
-    accordionAnswerTwo.classList.toggle('accordion-selected');
+    accordionAnswerTwo.classList.toggle('accordion-selected')
 });
 
 accordionQuestionThree.addEventListener('click', () => {
-    accordionAnswerThree.classList.toggle('accordion-selected');
+    accordionAnswerThree.classList.toggle('accordion-selected')
 });
 
 accordionQuestionFour.addEventListener('click', () => {
-    accordionAnswerFour.classList.toggle('accordion-selected');
+    accordionAnswerFour.classList.toggle('accordion-selected')
 });
 
 accordionQuestionFive.addEventListener('click', () => {
-    accordionAnswerFive.classList.toggle('accordion-selected');
+    accordionAnswerFive.classList.toggle('accordion-selected')
 });
