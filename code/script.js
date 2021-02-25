98398e8685f88034e93d663ee3b13e47d22c0ca4
Anna L <annalenksjo@gmail.com>
2021-02-25 09:20:11 +0100

    Corrections after review

--- code/script.js
+++ code/script.js
@@ -1,24 +1,20 @@
-let accordionHeader = document.getElementById("accordionHeader");
-let accordionContent = document.getElementById("accordionContent");
+const accordionHeader = document.getElementById("accordionHeader")
+const accordionContent = document.getElementById("accordionContent")
 
-let accordionHeaderTwo = document.getElementById("accordionHeaderTwo");
-let accordionContentTwo = document.getElementById("accordionContentTwo");
+const accordionHeaderTwo = document.getElementById("accordionHeaderTwo")
+const accordionContentTwo = document.getElementById("accordionContentTwo")
 
-let accordionHeaderThree = document.getElementById("accordionHeaderThree")
-let accordionContentThree = document.getElementById("accordionContentThree")
-
-console.log(accordionHeader);
-console.log(accordionContent);
-console.log(accordionHeaderTwo);
+const accordionHeaderThree = document.getElementById("accordionHeaderThree")
+const accordionContentThree = document.getElementById("accordionContentThree")
 
 accordionHeader.addEventListener("click", () => {
- accordionContent.classList.toggle("active"); 
+ accordionContent.classList.toggle("active")
 })
 
 accordionHeaderTwo.addEventListener("click", () => {
-accordionContentTwo.classList.toggle("activeTwo");
+accordionContentTwo.classList.toggle("activeTwo")
 })
 
 accordionHeaderThree.addEventListener("click", () => {
-    accordionContentThree.classList.toggle("activeThree");
-    })
+    accordionContentThree.classList.toggle("activeThree")
+})
