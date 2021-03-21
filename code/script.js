8e5d583d77b3274a5ce5612bbc35e0e418f53d2f
Nazanin Adampira <Nazi.pira88@gmail.com>
2021-03-21 21:36:59 +0100

    fix layout, join button

--- code/script.js
+++ code/script.js
@@ -2,19 +2,17 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 const accordionHeaderOne = document.getElementById('accordion-header-1');
-const accordionContentOne = document.getElementById('accordion-content-1')
+const accordionContentOne = document.getElementById('accordion-content-1');
 
 const accordionHeaderTwo = document.getElementById('accordion-header-2');
-const accordionContentTwo = document.getElementById('accordion-content-2')
+const accordionContentTwo = document.getElementById('accordion-content-2');
 
 const accordionHeaderThree = document.getElementById('accordion-header-3');
-const accordionContentThree = document.getElementById('accordion-content-3')
+const accordionContentThree = document.getElementById('accordion-content-3');
 
 const accordionHeaderFour = document.getElementById('accordion-header-4');
-const accordionContentFour = document.getElementById('accordion-content-4')
+const accordionContentFour = document.getElementById('accordion-content-4');
 
-
-console.log(accordionHeaderOne);
 
 accordionHeaderOne.addEventListener('click', () => { 
   accordionContentOne.classList.toggle('accordion-selected');
@@ -31,9 +29,3 @@ accordionHeaderThree.addEventListener('click', () => {
 accordionHeaderFour.addEventListener('click', () => { 
   accordionContentFour.classList.toggle('accordion-selected');
 });
-function toggle() {
-  this.classList.toggle("active")
-}
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
