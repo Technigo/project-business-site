f205bfd162448e344f1bea4f25980c7657488ace
Irina <jekabsone.irina@gmail.com>
2021-03-21 23:56:42 +0100

    classList.remove method added t othe accordion, project title, spacing.

--- code/script.js
+++ code/script.js
@@ -1,39 +1,48 @@
 // Defining variables
-const accordion=document.getElementById("accordion");
+const section1 = document.getElementById("section1");
+const section2 = document.getElementById("section2");
+const section3 = document.getElementById("section3");
+const section4 = document.getElementById("section4");
+const section5 = document.getElementById("section5");
 
-const section1=document.getElementById("section1");
-const answer1=document.getElementById("answer-1");
-
-const section2=document.getElementById("section2");
-const answer2=document.getElementById("answer-2");
-
-const section3=document.getElementById("section3");
-const answer3=document.getElementById("answer-3");
-
-const section4=document.getElementById("section4");
-const answer4=document.getElementById("answer-4");
-
-const section5=document.getElementById("section5");
-const answer5=document.getElementById("answer-5");
-
-// A function that adds and remove the class "active" on the section clicked on.
+// A function that adds and removes the class "active" on the section clicked.
 section1.addEventListener('click', () => {
   section1.classList.toggle('active');
+  section2.classList.remove('active');
+  section3.classList.remove('active');
+  section4.classList.remove('active');
+  section5.classList.remove('active');
 });
 
 section2.addEventListener('click', () => {
+  section1.classList.remove('active');
   section2.classList.toggle('active');
+  section3.classList.remove('active');
+  section4.classList.remove('active');
+  section5.classList.remove('active');
 });
 
 section3.addEventListener('click', () => {
+  section1.classList.remove('active');
+  section2.classList.remove('active');
   section3.classList.toggle('active');
+  section4.classList.remove('active');
+  section5.classList.remove('active');
 });
 
 section4.addEventListener('click', () => {
+  section1.classList.remove('active');
+  section2.classList.remove('active');
+  section3.classList.remove('active');
   section4.classList.toggle('active');
+  section5.classList.remove('active');
 });
 
 section5.addEventListener('click', () => {
+  section1.classList.remove('active');
+  section2.classList.remove('active');
+  section3.classList.remove('active');
+  section4.classList.remove('active');
   section5.classList.toggle('active');
 });
 
