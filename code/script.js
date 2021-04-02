c0aca563d643a4f717b0bca56e95706c149cca23
Julia Ros <70485921+julialeonieros@users.noreply.github.com>
2021-04-02 22:00:12 +0200

    Changed the styling in the form and media queries. Cleaned up the code

--- code/script.js
+++ code/script.js
@@ -1,13 +1,7 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-// 
-
 function toggle() {
   this.classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("question-1").onclick = toggle
 
 document.getElementById("question-2").onclick = toggle
