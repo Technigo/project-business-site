4c2c04bfcb7d40e6af87adc06460cfb7a7dffabb
M4L5 <malin.gu2017@gmail.com>
2021-04-13 23:07:51 +0200

    added logic to js that was missing

--- code/script.js
+++ code/script.js
@@ -1,6 +1,28 @@
-function toggleHamburger(){
-  this.classList.toggle("active")
-}
+
+const accordionHeaderOne = document.getElementById('accordion-header-1');
+const accordionAnswerOne = document.getElementById('accordion-answer-1');
+
+
+const accordionHeaderTwo = document.getElementById('accordion-header-2');
+const accordionAnswerTwo = document.getElementById('accordion-answer-2');
+
+
+const accordionHeaderThree = document.getElementById('accordion-header-3');
+const accordionAnswerThree= document.getElementById('accordion-answer-3');
+
+
+accordionHeaderOne.addEventListener('click', () => {
+  console.log('clicked')
+  accordionAnswerOne.classList.toggle('accordion-selected');
+});
+
+accordionHeaderTwo.addEventListener('click', () => {
+  accordionAnswerTwo.classList.toggle('accordion-selected');
+});
+
+accordionHeaderThree.addEventListener('click', () => {
+  accordionAnswerThree.classList.toggle('accordion-selected');
+});
 
 
 
@@ -11,5 +33,4 @@ function toggle() {
   this.classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+
