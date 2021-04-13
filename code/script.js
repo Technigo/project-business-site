93d3a2a78a9c00c9cbded5579b9e0addf972b2ba
M4L5 <malin.gu2017@gmail.com>
2021-04-13 21:30:48 +0200

    added some styling from old folder, but it's off so need to put some effort in styling

--- code/script.js
+++ code/script.js
@@ -1,3 +1,9 @@
+function toggleHamburger(){
+  this.classList.toggle("active")
+}
+
+
+
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
