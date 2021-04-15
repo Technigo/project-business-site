121d250fd32e6419d1cdadd5cf6a22cd539ee195
“yennie <yennie.leek@hotmail.se>
2021-04-15 12:31:45 +0200

    Requested changes made

--- code/script.js
+++ code/script.js
@@ -5,18 +5,15 @@ const infoContentOne = document.getElementById('info-content-1');
 const buttonTwo = document.getElementById('button2');
 const infoContentTwo = document.getElementById('info-content-2');
 
-console.log(button1);
-console.log(button2);
-
 buttonOne.addEventListener('click', () => {
   infoContentOne.classList.toggle('info-selected');
   infoContentTwo.classList.remove('info-selected');
-})
+});
 
 buttonTwo.addEventListener('click', () => {
   infoContentTwo.classList.toggle('info-selected');
   infoContentOne.classList.remove('info-selected');
-})
+});
 
 
 
