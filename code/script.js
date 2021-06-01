dfb30bcfbf7d4d1a2de38257907d986be7f08bf0
matildasvensson85 <40208267+matildasvensson85@users.noreply.github.com>
2021-06-01 18:06:08 +0200

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,15 +1,8 @@
-// A function that adds and remove the class "open" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
 
 function toggle() {
   this.classList.toggle("open")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-
 document.getElementById("question-1").onclick = toggle
 document.getElementById("question-2").onclick = toggle
 
-const answer1 = document.getElementById('answer-1');
-const answer2 = document.getElementById('answer-2');
