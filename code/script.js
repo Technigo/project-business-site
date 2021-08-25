867891d2d80806f78c19c657318cdbad779bf8af
Sabrina Zancotti <zancotti@gmail.com>
2021-08-25 22:31:37 +0200

    Built a navbar for the mobile device

--- code/script.js
+++ code/script.js
@@ -2,8 +2,19 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 function toggle() {
-  this.classList.toggle("active")
+  this.classList.toggle("active");
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("section1").onclick = toggle;
+
+$(window).scroll(function (e) {
+  var $el = $(".fixedElement");
+  var isPositionFixed = $el.css("position") == "fixed";
+  if ($(this).scrollTop() > 200 && !isPositionFixed) {
+    $el.css({ position: "fixed", top: "0px" });
+  }
+  if ($(this).scrollTop() < 200 && isPositionFixed) {
+    $el.css({ position: "static", top: "0px" });
+  }
+});
