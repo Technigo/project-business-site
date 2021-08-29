d1d914ad0a44d7ae45a63d37bc2acf45f86d7207
Priscila Alfaro <priscila24n@hotmail.com>
2021-08-29 18:38:55 +0200

    main structure

--- code/script.js
+++ code/script.js
@@ -2,8 +2,9 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 function toggle() {
-  this.classList.toggle("active")
+  console.log("this", this);
+  this.classList.toggle("active");
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("question1").onclick = toggle;
