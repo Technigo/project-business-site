22be196197162147b3724a789248741acc3b0bd5
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2021-08-31 13:22:36 +0200

    added accordion

--- code/script.js
+++ code/script.js
@@ -5,5 +5,27 @@ function toggle() {
   this.classList.toggle("active")
 }
 
+// Navbar sticky
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+document.getElementById("section2").onclick = toggle
+document.getElementById("section3").onclick = toggle
+document.getElementById("section4").onclick = toggle
