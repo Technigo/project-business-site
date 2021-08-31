f861e4255313a78610c1c3e2c7025a21a5ca9848
Jsfrulle <Jessicamhermansson@gmail.com>
2021-08-31 19:21:41 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let accordions = document.querySelectorAll("div.accordion");
+
+for( let i = 0; i < accordions.length; i++){
+  accordions[i].onclick = function(){
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("show");
+  }
+}
