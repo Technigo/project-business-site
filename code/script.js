f6e2ff8755b306100bf365be1c85fc14c5a827a9
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-09-01 00:15:47 +0200

    added more styles and finished the form and started with faq-accordion. Played it around with js

--- code/script.js
+++ code/script.js
@@ -6,17 +6,22 @@ function toggle() {
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-
-
+// document.getElementById("section1").onclick = toggle
+document.getElementById("question1").onclick = toggle
+document.getElementById("question2").onclick = toggle
+document.getElementById("question3").onclick = toggle
+document.getElementById("question4").onclick = toggle
 
 const signupBtn = document.getElementById("signupBtn")
 const closeBtn = document.getElementById("closeBtn")
 const modalForm = document.getElementById("modalForm")
 
-
 const selectCountry = document.getElementById("selectCountry")
 const tel = document.getElementById("tel")
+
+const faqBtn = document.getElementById("faqBtn")
+const closeBtn2 = document.getElementById("closeBtn2")
+const modalAccordion = document.getElementById("modalAccordion")
 
 
 signupBtn.onclick = () => {                 //pop up the form when clicking on Sign Up-btn
@@ -30,3 +35,12 @@ closeBtn.onclick = () => {                  //close the form
 selectCountry.onchange = () => {            //select country and autofill the country code"
   tel.value = selectCountry.value
 }
+
+faqBtn.onclick = () => {                    //pop up the faq accordion when clicking on FAQ-btn
+  modalAccordion.style.display="block"
+}
+
+closeBtn2.onclick = () => {                 //close the accordion
+  modalAccordion.style.display="none"
+}
+
