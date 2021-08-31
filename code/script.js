d23ce4d832254d3bccd8c9b2f19619d75617883b
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2021-08-31 15:00:57 +0200

    test

--- code/script.js
+++ code/script.js
@@ -1,9 +1,20 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
+
+//When toggle is activated remove "active" from all buttons
 function toggle() {
-  this.classList.toggle("active")
+    document.querySelectorAll(".question").forEach((btn, i) => {
+       //= tilldela variabel == är värdet likadant === är typen och värdet likadant
+        if (btn === this) 
+            return;
+        btn.classList.remove("active");
+    });
+    this.classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// This code selects all elements of class "question"
+document.querySelectorAll(".question").forEach((btn, i) => {
+    btn.onclick = toggle;
+});
+
