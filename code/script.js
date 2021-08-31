9156d07dfc3f26e565cbb71ce9c8b29b0f556397
Lovisa Carling <lovisa.carling@gmail.com>
2021-08-31 23:24:29 +0200

    Added video as a header, navbar, form, images and accordion. Some basic styling

--- code/script.js
+++ code/script.js
@@ -1,8 +1,25 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
+//Makes hamburger navbar open and close. When active a new navbar icon is shown. See CCS row 64
+const menuToggle = document.querySelector('.toggle')
+      const showcase = document.querySelector('.showcase')
+
+      menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active')
+        showcase.classList.toggle('active')
+      })
+//Accordion opens when clicking on a question
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
