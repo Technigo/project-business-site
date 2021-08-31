1135a0d3a3b684679a8c042fe2abb588d00f549b
Jsfrulle <Jessicamhermansson@gmail.com>
2021-08-31 15:12:37 +0200

    allbasic

--- code/script.js
+++ code/script.js
@@ -6,4 +6,4 @@ function toggle() {
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementByclass("question").onclick = toggle
