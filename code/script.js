126334dcb4a7845021e9fc89747169d69b91d2f1
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-08-31 15:35:04 +0200

    added a navbar and started with the sign up form

--- code/script.js
+++ code/script.js
@@ -7,3 +7,26 @@ function toggle() {
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+
+
+
+const signupBtn = document.getElementById("signupBtn")
+const closeBtn = document.getElementById("closeBtn")
+const modalForm = document.getElementById("modalForm")
+
+
+const selectCountry = document.getElementById("selectCountry")
+const tel = document.getElementById("tel")
+
+
+signupBtn.onclick = () => {                 //pop up the form when clicking on Sign Up-btn
+  modalForm.style.display="block"
+}
+
+closeBtn.onclick = () => {                  //close the form
+  modalForm.style.display="none"
+}
+
+selectCountry.onchange = () => {            //select country and autofill the country code"
+  tel.value = selectCountry.value
+}
