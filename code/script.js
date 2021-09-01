7ebded657888e435f8eee50167d0a33ebe3d530e
ruruahn <81482699+ruruahn@users.noreply.github.com>
2021-09-01 23:01:37 +0200

    display hero image, add sign-up form and accordion

--- code/script.js
+++ code/script.js
@@ -1,9 +1,22 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
+
+// 자바스트립트야~! html document에 가서 Element (<...>)들 중에 아이디가 "answer1"인 애한테 "active"라는 스타일 클래스(classList)를 줬다가 뺐었다가(toggle) 해!
+function banana() {
+  document.getElementById("answer1").classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("question1").onclick = banana
+
+function apple() {
+  document.getElementById("answer2").classList.toggle("active")
+}
+
+document.getElementById("question2").onclick = apple
+
+function kiwi() {
+  document.getElementById("answer3").classList.toggle("active")
+}
+
+document.getElementById("question3").onclick = kiwi
