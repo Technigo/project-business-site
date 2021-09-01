d14e2825f31d626dab148a04ec92c0301a77cf92
Harry Bäcklin <83465217+Groovedharry@users.noreply.github.com>
2021-09-01 19:17:06 +0200

    added FAQ, Radio buttons

--- code/script.js
+++ code/script.js
@@ -1,9 +1,35 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-    this.classList.toggle("active")
-}
+// function toggle() {
+//     this.classList.toggle("active")
+// }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// document.getElementById("section1").onclick = toggle
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    var acc = document.getElementsByClassName("accordion-button");
+var i;
+
+console.log('DOM full loaded and parsed');
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+});
+console.log('DUM fully loaded and parsed');
+}
+});
+
+
