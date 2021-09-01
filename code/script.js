6df5e4e9687104772d4e12fc301a8e685702b05b
Your name here <your_email@example.com>
2021-09-01 18:27:45 +0200

    started over with new code

--- code/script.js
+++ code/script.js
@@ -1,39 +1,22 @@
-
-
-
-// // A function that adds and remove the class "active" on the section you click on.
-// // We haven't really talked about ´this´ yet, but we will... ;)
-// // console.log(this) to see how it works
-// function toggle() {
-//   this.classList.toggle("active")
-// }
-
-// console.log(toggle())
-
-// // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section1").onclick = toggle
-
-
 const section1 = document.getElementById("section1").onclick = toggle 
 const section2 = document.getElementById("section2").onclick = toggle 
 const section3 = document.getElementById("section3").onclick = toggle 
-const section4 = document.getElementById("section4").onclick = toggle 
+const section4 = document.getElementById("section4").onclick = toggle
+
+
+document.getElementById('form').addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const inputValue = document.getElementById('fname').value
+    const greeting = document.getElementById('greeting')
+    console.log(inputValue)
+    greeting.innerHTML = `<h5>Welcome ${inputValue}! We've got you covered ☔</h5>`
+
+}) 
 
 
 
 function toggle() {
   this.classList.toggle("active");
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
