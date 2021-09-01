3fd56585c269fdd9c569420fa5ecd614e71abe8d
HedvigM <hedvig@mejstedt.se>
2021-09-01 20:09:54 +0200

    created the form and some css/js work

--- code/script.js
+++ code/script.js
@@ -2,8 +2,8 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 function toggle() {
-  this.classList.toggle("active")
-}
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+    this.classList.toggle("active")
+  }
+  
+  // Selects an HTML element, and calls a function which will be executed when the element is clicked.
+  document.getElementById("section1").onclick = toggle
