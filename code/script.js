b322a949be06dc056162721a7dc2dd4152b46995
Johanna Mannestål <johannamannestal@Johannas-MacBook-Air.local>
2021-09-01 18:23:22 +0200

    FAQ section started and basic javascript added

--- code/script.js
+++ code/script.js
@@ -1,9 +1,41 @@
+
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+// function toggle() {
+//   this.classList.toggle("active")
+// }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// document.getElementById("section1").onclick = toggle
+
+// toggles faq 1
+const faq1 = document.getElementById('section1')
+faq1.onclick = toggleFaqAnswer1
+
+function toggleFaqAnswer1() {
+  console.log('Hello!')
+  this.classList.toggle('active')
+}
+
+// toggles faq 2
+const faq2 = document.getElementById('section2')
+faq2.onclick = toggleFaqAnswer2
+
+function toggleFaqAnswer2() {
+  this.classList.toggle('active')
+}
+
+// toggles faq 3 (shorter version og writing it)
+document.getElementById('section3').onclick = toggleFaqAnswer3
+
+function toggleFaqAnswer3() {
+  this.classList.toggle('active')
+}
+
+// toggles faq 4 (shorter version og writing it)
+document.getElementById('section4').onclick = toggleFaqAnswer4
+
+function toggleFaqAnswer4() {
+  this.classList.toggle('active')
+}
