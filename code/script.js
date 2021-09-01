370524e991b5948623c2ca6e5f79fe76b3bfeec3
Gustav Frid <gustav.frid@gmail.com>
2021-09-01 12:34:16 +0200

    added content + more sections to faq

--- code/script.js
+++ code/script.js
@@ -17,7 +17,11 @@ function toggle() {
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById('section1').onclick = toggle;
 document.getElementById('section2').onclick = toggle;
+document.getElementById('section3').onclick = toggle;
+document.getElementById('section4').onclick = toggle;
+document.getElementById('section5').onclick = toggle;
 
+// code for alert on submit
 document.getElementById('form').addEventListener('submit', (event) => {
 	event.preventDefault();
 
