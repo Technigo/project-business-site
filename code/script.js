fef48aa12e798269c1db8b3ffb3c3e372fc6a709
Bruna Santos Araujo <brunasantosaraujo@Brunas-Air.lan>
2021-09-01 23:37:36 +0200

    accordion +js

--- code/script.js
+++ code/script.js
@@ -7,3 +7,25 @@ function toggle() {
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+
+
+// Starts here
+
+var acc = document.getElementsByClassName("question");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
