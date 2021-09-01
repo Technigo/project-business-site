00be1639e1dd72d498349a479ceea84a8195ffcb
Никитина Юля <nik.iuliiasw@gmail.com>
2021-09-01 22:48:24 +0200

    first draft with skelet

--- code/script.js
+++ code/script.js
@@ -1,9 +1,18 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+// function toggle() {
+//   this.classList.toggle("active")
+// }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// document.getElementById("section1").onclick = toggle
+
+const accordionItemHeaders = document.querySelectorAll('.accordion-item-header');
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener('click', event => {
+    accordionItemHeader.classList.toggle('active');
+  })
+
+})
