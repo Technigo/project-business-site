5177046af7e2da28fd784e8eb19d082c446720c5
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2021-09-01 15:37:34 +0200

    added checkbox, rewrote some javascript

--- code/script.js
+++ code/script.js
@@ -4,15 +4,16 @@
 function toggle() {
   this.classList.toggle("active")
 }
-
+// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+document.getElementById("section1").onclick = toggle
+document.getElementById("section2").onclick = toggle
+document.getElementById("section3").onclick = toggle
+document.getElementById("section4").onclick = toggle
 
 // Navbar sticky
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
 var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
@@ -25,8 +26,4 @@ function myFunction() {
   }
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-document.getElementById("section2").onclick = toggle
-document.getElementById("section3").onclick = toggle
-document.getElementById("section4").onclick = toggle
+
