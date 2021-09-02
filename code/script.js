b83c8693c7466a31036b1d7b30e200d5e185d880
Ida Aspen <ida.aspen@outlook.com>
2021-09-02 10:52:31 +0200

    HTML structure done

--- code/script.js
+++ code/script.js
@@ -7,3 +7,19 @@ function toggle() {
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+
+
+// Anteckningar från codepen
+// document.getElementById('button').onclick = toggleDrinkMenu
+
+// //or document.uerySelector('#button').onclick = toggleDrinkMenu
+// // When the button is clicked, the toggleDrinkMenu function is called.
+
+// // This is the function that will add or remove the class open to the button.
+// // In the CSS we should to do styling to show or hide the ul 
+// // (important! not the div with the class button that is selected, but it's sibling)
+// function toggleDrinkMenu() {
+//   this.classList.toggle('open')
+//   // the word "this" refers to the element that is the selector.
+// }
+
