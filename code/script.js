0f72108f7be23a85e5ad5728fb63e0632229bff8
Elsa-Johanna <elsa.bjelkedal@gmail.com>
2021-09-02 12:28:23 +0200

    added a faq accordion with styleing. although i dont quite grasp everything right now, in the future i will add comments mostly for myself explainging what everything does

--- code/script.js
+++ code/script.js
@@ -1,9 +1,10 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
+const items = document.querySelectorAll(".accordion a");
+ 
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
 }
+ 
+items.forEach(item => item.addEventListener('click', toggleAccordion));
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// https://webdevtrick.com/html-css-faq-design/ //
