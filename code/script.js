d5eeaed024d380f9e8fad409b738d42d4cdb700a
Darya <darya.lapata@gmail.com>
2021-09-02 15:21:17 +0200

    big commit with changes

--- code/script.js
+++ code/script.js
@@ -1,9 +1,36 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// const button = document.getElementById("section1");
+// button.onclick = toggle;
+
+// console.log(toggle())
+
+// function toggle() {
+
+//   this.classList.toggle("question")
+// }
+
+// const btn = document.getElementById('section1');
+// btn.addEventListener('click', () => {
+//   btn.classList.toogle("answer");
+// })
+
+
+let acc = document.getElementsByClassName("accordion");
+
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
