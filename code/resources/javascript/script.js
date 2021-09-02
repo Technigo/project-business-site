f1863379169169ff4a00d648a770ceffcfc9c1c5
isomoth <ic.gonzalez35@hotmail.com>
2021-09-02 23:28:14 +0200

    added media and favicons

---
+++ code/resources/javascript/script.js
@@ -0,0 +1,9 @@
+// A function that adds and remove the class "active" on the section you click on.
+// We haven't really talked about ´this´ yet, but we will... ;)
+// console.log(this) to see how it works
+function toggle() {
+  this.classList.toggle("active")
+}
+
+// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+document.getElementById("section1").onclick = toggle
