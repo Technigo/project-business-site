a1c48aa41be37257779d74b61059e79888b1877c
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2021-09-02 10:41:48 +0200

    Update in FAQ and Signup form

--- code/script.js
+++ code/script.js
@@ -1,9 +1,16 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
+// Previous code was document.getElementbyId(‘section1’).onclick = toggle; (and next for section2 etc.)
+// My code picks out every element with the name class in the DOM called "question" and add on click function.
+// Which means that every time I add another question in the html - JS will automatically add it. Less repetition of code - which is nice if you want to add more FAQ questions for example.
 
 
-//When toggle is activated remove "active" from all buttons
+document.querySelectorAll(".question").forEach((btn, i) => {
+    btn.onclick = toggle;
+});
+
+
+//This code make a FAQ question that is toggled - close itself when another question is opened.
+// For each .question the JS will loop (===) the code and when it sees the code "btn active" it will close the others (remove active).
+
 function toggle() {
     document.querySelectorAll(".question").forEach((btn, i) => {
      
@@ -14,9 +21,3 @@ function toggle() {
     });
     this.classList.toggle("active")
 }
-
-// This code selects all elements of class "question"
-document.querySelectorAll(".question").forEach((btn, i) => {
-    btn.onclick = toggle;
-});
-
