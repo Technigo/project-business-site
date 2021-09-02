44e800c879027d69f93530a55a328f808bbb7867
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2021-09-02 16:33:49 +0200

    responsiveness accordion sign up form styling

--- code/script.js
+++ code/script.js
@@ -1,9 +1,27 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
+function toggleAnswer1() {
+  this.classList.toggle("active")
+}
+
+function toggleAnswer2() {
+  this.classList.toggle("active")
+}
+
+function toggleAnswer3() {
+  this.classList.toggle("active")
+}
+
+function toggleAnswer4() {
   this.classList.toggle("active")
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("section1").onclick = toggleAnswer1
+
+document.getElementById("section2").onclick = toggleAnswer2
+
+document.getElementById("section3").onclick = toggleAnswer3
+
+document.getElementById("section4").onclick = toggleAnswer4
