213940be94b3b8596e575339742a0fd755bde603
camekman <ekman.camilla@gmail.com>
2021-09-02 10:35:16 +0200

    completed FAQ set up

--- code/script.js
+++ code/script.js
@@ -12,12 +12,29 @@ document.getElementById("section1").onclick = toggle
 
 
 
-// FAQ calls function + when clicked 
+// FAQ calls function + when clicked. 
+//Row 20-23 sets the rule that only one FAQ item can be opened at once. 
+// My guide was this video: https://www.youtube.com/watch?v=dr8Emho-kYo&ab_channel=CodingJourney 
+
 const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
 accordionItemHeaders.forEach(accordionItemHeader => {
- accordionItemHeader.addEventListener("click", event => {
-  accordionItemHeader.classList.toggle("active");
+  accordionItemHeader.addEventListener("click", event => {
+    const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+      currentlyActiveAccordionItemHeader.classList.toggle("active");
+      currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    }
+   
+
+   accordionItemHeader.classList.toggle("active");
+   const accordionItemBody = accordionItemHeader.nextElementSibling;
+   if(accordionItemHeader.classList.contains("active")) {
+     accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+   }
+   else {
+      accordionItemBody.style.maxHeight = 0;
+  }
  });
 });
 
