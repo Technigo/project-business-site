43d3ce7ad7abb5995aaa41d6d5e3884ed4aac9f4
Your name here <your_email@example.com>
2021-09-02 12:00:21 +0200

    cleaned up the CSS

--- code/script.js
+++ code/script.js
@@ -3,20 +3,21 @@ const section2 = document.getElementById("section2").onclick = toggle
 const section3 = document.getElementById("section3").onclick = toggle 
 const section4 = document.getElementById("section4").onclick = toggle
 
+function toggle() {
+  this.classList.toggle("active");
+}
 
-document.getElementById('form').addEventListener('submit', (event) => {
+
+document.getElementById('signup').addEventListener('submit', (event) => {
     event.preventDefault()
 
     const inputValue = document.getElementById('fname').value
     const greeting = document.getElementById('greeting')
-    console.log(inputValue)
     greeting.innerHTML = `<h5>Welcome ${inputValue}! We've got you covered ☔</h5>`
 
 }) 
 
 
 
-function toggle() {
-  this.classList.toggle("active");
-}
+
 
