f6a70fd25de3f5f22ca23d801579264b4e9f054e
Priscila Alfaro <priscila24n@hotmail.com>
2021-09-02 20:59:26 +0200

    add css styles and improve the html structure

--- code/script.js
+++ code/script.js
@@ -1,10 +1,14 @@
 // A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
 function toggle() {
-  console.log("this", this);
   this.classList.toggle("active");
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("question1").onclick = toggle;
+//the function iterate over the nodeList of question class to change the classname
+document.querySelectorAll(".question").forEach(element => element.onclick = toggle);
+
+
+
+//getElementByClassName returns an HTMLCollection which do not have iterate methods
+//querySelectorAll return a nodeList that is possible to iterate with forEach
+//see: https://developer.mozilla.org/es/docs/Web/API/NodeList
