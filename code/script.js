e99c66c9512e0d6c042756bc964a0b4ff37e3c41
Linnea Isebrink <alisebrink@Mac-mini-som-tillhor-Linnea.local>
2021-09-02 18:47:54 +0200

    Changing colors again, mainly footer

--- code/script.js
+++ code/script.js
@@ -6,9 +6,11 @@ function toggle() {
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle;
-document.getElementById("section2").onclick = toggle;
-document.getElementById("section3").onclick = toggle;
-document.getElementById("section4").onclick = toggle;
+// document.getElementById("section1").onclick = toggle;
+// document.getElementById("section2").onclick = toggle;
+// document.getElementById("section3").onclick = toggle;
+// document.getElementById("section4").onclick = toggle;
+
+document.getElementsByClassName("question").onclick = toggle;
 
 console.log(this);
