fc15b1fa72af0c6fe00f665a6d2f15d505c2efd3
e-delsol <80672920+e-delsol@users.noreply.github.com>
2021-09-03 19:34:38 +0200

    Added flex to the form, some more structure and started making the accordion.

--- code/script.js
+++ code/script.js
@@ -7,3 +7,62 @@ function toggle() {
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+document.getElementById("section2").onclick = toggle
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// My code:
+
+
+// const answer1 = document.getElementById("answer1")
+// const section1 = document.getElementById("section1")
+
+
+
+// section1.onclick = () => {
+//   alert("Hello you")
+// }
+
+
+// answer1.textContent = "Hey"
+
+// answer1.innerText = "Hey"
+
+
+
+
+
+
+
+
+// HTML:
+
+{/* <section class="accordion">
+<h1>FAQ</h1>
+
+<div class="question" id="section1">Question 1</div>
+<div class="answer" id="answer1">
+  <p>Answer 1...</p>
+</div>
+
+<div class="question" id="section2">Question 2</div>
+<div class="answer" id="answer2">
+  <p>Answer 2...</p>
+</div>
+</section>*/}
