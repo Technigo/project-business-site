46078bef2501634bb257a3631161d6bcde87c279
Reinis <reinis@Reiniss-MacBook-Pro.local>
2021-09-03 09:09:25 +0300

    Updated JS file

--- code/script.js
+++ code/script.js
@@ -8,10 +8,11 @@ function toggle() {
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 // document.getElementById("section1").onclick = toggle;
 
-// I wrote a looping function that gets runned as soon as page is loaded and adds listeners to all the FAQ sessions
-function loopThrough(){
-  for(let i=1; i<7; i++){
+// I wrote a looping function that gets runned as soon as page is loaded and adds listeners to all the FAQ sessions.
+function loopThroughQuestions(){
+  let numberOfAnswers= document.querySelectorAll('#faq-container .answer').length;
+  for(let i=1; i<=numberOfAnswers; i++){
     document.getElementById(`section${i}`).addEventListener("click", toggle);
   }
 }
-window.onload = loopThrough;
+window.onload = loopThroughQuestions;
