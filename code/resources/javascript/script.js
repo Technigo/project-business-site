048c7681c7264f16d2f66db07e041e8d45d00fbf
isomoth <ic.gonzalez35@hotmail.com>
2021-09-04 00:54:15 +0200

    faq accordion and sign-up form

--- code/resources/javascript/script.js
+++ code/resources/javascript/script.js
@@ -6,4 +6,6 @@ function toggle() {
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("accordion__section1").onclick = toggle
+document.getElementById("accordion__section2").onclick = toggle
+document.getElementById("accordion__section3").onclick = toggle
