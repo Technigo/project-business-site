f5d08c9930c5081e8eb0d44eff103ac9f3240e98
Maria Petersson <mariapetersson@Marias-MacBook.lan>
2021-09-03 16:48:29 +0200

    working accordian

--- code/script.js
+++ code/script.js
@@ -3,7 +3,13 @@
 // console.log(this) to see how it works
 function toggle() {
   this.classList.toggle("active")
+ 
+  
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("question1").onclick = toggle
+document.getElementById("question2").onclick = toggle
+document.getElementById("question3").onclick = toggle
+document.getElementById("question4").onclick = toggle
+document.getElementById("question5").onclick = toggle
