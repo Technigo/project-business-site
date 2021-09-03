ba40766ccded201a923a9740ea707dd225ac4298
Никитина Юля <nik.iuliiasw@gmail.com>
2021-09-03 12:03:49 +0200

    done with layout and responsiveness

--- code/script.js
+++ code/script.js
@@ -1,18 +1,21 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-// function toggle() {
-//   this.classList.toggle("active")
-// }
+function toggleFunction() {
+  this.classList.toggle('active')
+}
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section1").onclick = toggle
+document.getElementById('question1').onclick = toggleFunction
+document.getElementById('question2').onclick = toggleFunction
+document.getElementById('question3').onclick = toggleFunction
+document.getElementById('question4').onclick = toggleFunction
+document.getElementById('question5').onclick = toggleFunction
 
-const accordionItemHeaders = document.querySelectorAll('.accordion-item-header');
+// Another way of doing accordion that I found just to remember myself  //
+// const accordionItemHeaders = document.querySelectorAll('.accordion-item-header');
 
-accordionItemHeaders.forEach(accordionItemHeader => {
-  accordionItemHeader.addEventListener('click', event => {
-    accordionItemHeader.classList.toggle('active');
-  })
-
-})
+// accordionItemHeaders.forEach(accordionItemHeader => {
+//   accordionItemHeader.addEventListener('click', event => {
+//     accordionItemHeader.classList.toggle('active');
+//   })
