7ea97deb975da8dbd7ad478466e08dd54df2b1a4
Johanna Mannestål <johannamannestal@Johannas-MacBook-Air.local>
2021-09-03 19:05:04 +0200

    added transition and text, ready to deploy

--- code/script.js
+++ code/script.js
@@ -12,33 +12,15 @@ document.getElementById("section2").onclick = toggle
 document.getElementById("section3").onclick = toggle
 document.getElementById("section4").onclick = toggle
 
+
+
+
+// My first attempt how to try it:
 // toggles faq 1
 // const faq1 = document.getElementById('section1')
 // faq1.onclick = toggleFaqAnswer1
 
 // function toggleFaqAnswer1() {
 //   console.log('Hello!') //This will show in the console log, just testing
-//   this.classList.toggle('active')
-// }
-
-// // toggles faq 2
-// const faq2 = document.getElementById('section2')
-// faq2.onclick = toggleFaqAnswer2
-
-// function toggleFaqAnswer2() {
-//   this.classList.toggle('active')
-// }
-
-// // toggles faq 3 (shorter version og writing it)
-// document.getElementById('section3').onclick = toggleFaqAnswer3
-
-// function toggleFaqAnswer3() {
-//   this.classList.toggle('active')
-// }
-
-// // toggles faq 4 (shorter version og writing it)
-// document.getElementById('section4').onclick = toggleFaqAnswer4
-
-// function toggleFaqAnswer4() {
 //   this.classList.toggle('active')
 // }
