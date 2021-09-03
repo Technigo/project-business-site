8d9c8b09cef0f3c527b74c0ec05f5266f6fa7f3c
Kaliine <80845418+Kaliine@users.noreply.github.com>
2021-09-03 22:03:10 +0200

    Added an accordion and styled it

--- code/script.js
+++ code/script.js
@@ -2,8 +2,31 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 function toggle() {
-  this.classList.toggle("active")
+  this.classList.toggle('active')
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+/*document.getElementById("section1").onclick = toggle */
+
+
+document.getElementById('accordionbtn1').onclick = toggle
+function toggle() {
+  this.classList.toggle('open')
+}
+
+document.getElementById('accordionbtn2').onclick = toggle
+function toggle() {
+  this.classList.toggle('open')
+}
+
+document.getElementById('accordionbtn3').onclick = toggle
+function toggle() {
+  this.classList.toggle('open')
+}
+
+document.getElementById('accordionbtn4').onclick = toggle
+function toggle() {
+  this.classList.toggle('open')
+}
+
+
