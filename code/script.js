da274d5b2dbc7dfbf4494f868bb026052914ea4a
Linnea Isebrink <alisebrink@Mac-mini-som-tillhor-Linnea.local>
2021-09-03 13:05:54 +0200

    Trying to add jquery functionality

--- code/script.js
+++ code/script.js
@@ -3,13 +3,35 @@
 // This is a function that toggles the class "active"
 function toggle() { 
   // this. is a keyword which lets me add a class to the element I click on
-  this.classList.toggle("active"); 
+  this.classList.toggle("active");
+  this.classList.toggle("fa-minus");
 }
 
-// I create a variable and name it elements, i get my information from the class names and put it as an array in this variable
+let icon = document.getElementsByClassName("fa-plus");
+// I create a variable and name it elements, i get my information from the class names in my html and put it as an array in this variable
 let elements = document.getElementsByClassName("question"); 
 // this for loop lets me loop through the elements on my page and put them into an array
 for (let i = 0; i < elements.length; ++i) { 
   // this lets me get the right element from the right position in my array (i.e the element I click on)
-  elements[i].onclick = toggle; 
+  elements[i].onclick = toggle;
 }
+
+console.log(icon);
+console.log(elements);
+
+// $(".question").click(function(){
+//   $(".answer").slideDown(".answer");
+//   $(".question i").addClass("fa fa-minus");
+// });
+
+$(this).click(function(){
+  if(!$("div.question").hasClass("active")) {
+    $(".answer").slideDown();
+    $(".question i").removeClass("fa fa-plus");
+    $(".question i").addClass("fa fa-minus");
+  } else {
+    $(".answer").slideUp();
+    $(".question i").removeClass("fa fa-minus");
+    $(".question i").addClass("fa fa-plus");
+  }
+});
