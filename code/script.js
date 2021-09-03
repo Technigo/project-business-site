0ed0d5b37c4b89c719821b7669f9fc179db7acf4
molbimien <43146046+molbimien@users.noreply.github.com>
2021-09-03 12:43:06 +0200

    first commit - added basic layout and functionality according to the project minimum requirements

--- code/script.js
+++ code/script.js
@@ -1,12 +1,18 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
+function toggleAccordion() {
   this.classList.toggle("active")
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-document.getElementById("section2").onclick = toggle
-document.getElementById("section3").onclick = toggle
-document.getElementById("section4").onclick = toggle
+document.getElementById("accordion-item1").onclick = toggleAccordion
+document.getElementById("accordion-item2").onclick = toggleAccordion
+document.getElementById("accordion-item3").onclick = toggleAccordion
+document.getElementById("accordion-item4").onclick = toggleAccordion
+
+// segment = ['segment1', 'segment2', ...]
+// segment.forEach(segment => {
+//   const fieldEl = document.getElementById(segment);
+//   .onclick = toggle
+// });
