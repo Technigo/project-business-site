292c182ad2532cb2849bf57554ab55a20cbe439b
Linnea Isebrink <alisebrink@Mac-mini-som-tillhor-Linnea.local>
2021-09-03 16:00:49 +0200

    I've added some final comments to my code

--- code/script.js
+++ code/script.js
@@ -1,14 +1,18 @@
 "use strict";
 
-// let icon = document.getElementsByClassName("question").lastChild;
+// I create a variable called elements
+// I get all elements with the class name of "question"
+// The content in this variable is created as an array
 let elements = document.getElementsByClassName("question");
-let menu = document.getElementById("mobile");
 
+// This is a function that is activated with a click of the element with class name "question"
 function toggle() {
+  // The "this" keyword means that I select the element I click on
   this.classList.toggle("active");
 }
 
+// on page-load this loop runs and gets all the element in the variable "elements"
+// It then sets an onclick event on all elements within the array
 for (let i = 0; i < elements.length; ++i) {
   elements[i].onclick = toggle;
-  menu[i].onclick = toggle;
 }
