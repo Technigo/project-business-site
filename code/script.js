8ee193374c2c4a8ef28005b029735742788165dd
Birgit Nehrwein <nehrwein@posteo.de>
2021-09-03 09:15:15 +0200

    worked on the faq-questions and centered the plus-sign

--- code/script.js
+++ code/script.js
@@ -7,57 +7,57 @@ function toggle() {
 
 var w = document.documentElement.clientWidth || window.innerWidth;
 if (w > 1024) {
-  document.getElementById('intro-anchor').addEventListener('click', (event) => {
-    event.preventDefault()  
-                                                  /*  verhindert, dass der Link abspringt */
-  const openHeadline = document.getElementById('headline')
-  openHeadline.style.display = 'none'
+      document.getElementById('intro-anchor').addEventListener('click', (event) => {
+        event.preventDefault()  
+                                                      /*  verhindert, dass der Link abspringt */
+          const openHeadline = document.getElementById('headline')
+          openHeadline.style.display = 'none'
 
-  const formSection = document.getElementById('form-section-id')
-  formSection.style.display = 'contents'            /*  	Makes the container disappear, making the child elements children of the element the next level up in the DOM */
-  
-  const introForm =document.getElementById('intro-form')
-  introForm.style.position = 'absolute'
-  introForm.style.top = '50px' 
-  
-  const accordionSection = document.getElementById('accordion-id')
-  accordionSection.style.display = 'none' 
-
-  })
-
-  document.getElementById('faq-anchor').addEventListener('click', (event) => {
-    event.preventDefault()  
-                                                  /*  verhindert, dass der Link abspringt */
-  const openHeadline = document.getElementById('headline')
-  openHeadline.style.display = 'none'
-
-  const accordionSection = document.getElementById('accordion-id')
-  accordionSection.style.display = 'contents'            /*  	Makes the container disappear, making the child elements children of the element the next level up in the DOM */
-  
-  const faqAccordion =document.getElementById('faq-accordion')
-  faqAccordion.style.position = 'absolute'
-  faqAccordion.style.top = '50px'  
-
-  const formSection = document.getElementById('form-section-id')
-  formSection.style.display = 'none'  
+          const formSection = document.getElementById('form-section-id')
+          formSection.style.display = 'contents'            /*  	Makes the container disappear, making the child elements children of the element the next level up in the DOM */
+          
+          const introForm =document.getElementById('intro-form')
+          introForm.style.position = 'absolute'
+          introForm.style.top = '50px' 
+          
+          const accordionSection = document.getElementById('accordion-id')
+          accordionSection.style.display = 'none' 
 
   })
 
+      document.getElementById('faq-anchor').addEventListener('click', (event) => {
+        event.preventDefault()  
+                                                      /*  verhindert, dass der Link abspringt */
+          const openHeadline = document.getElementById('headline')
+          openHeadline.style.display = 'none'
+
+          const accordionSection = document.getElementById('accordion-id')
+          accordionSection.style.display = 'contents'            /*  	Makes the container disappear, making the child elements children of the element the next level up in the DOM */
+          
+          const faqAccordion =document.getElementById('faq-accordion')
+          faqAccordion.style.position = 'absolute'
+          faqAccordion.style.top = '50px'  
+
+          const formSection = document.getElementById('form-section-id')
+          formSection.style.display = 'none'  
+
+  })
 
 
 
 
-  document.getElementById('home-anchor').addEventListener('click', (event) => {
-    event.preventDefault()  
-                                                  /*  verhindert, dass der Link abspringt */
-  const openHeadline = document.getElementById('headline')
-  openHeadline.style.display = 'block'
 
-  const accordionSection = document.getElementById('accordion-id')
-  accordionSection.style.display = 'none'      
+      document.getElementById('home-anchor').addEventListener('click', (event) => {
+        event.preventDefault()  
+                                                      /*  verhindert, dass der Link abspringt */
+          const openHeadline = document.getElementById('headline')
+          openHeadline.style.display = 'block'
 
-  const formSection = document.getElementById('form-section-id')
-  formSection.style.display = 'none'  
+          const accordionSection = document.getElementById('accordion-id')
+          accordionSection.style.display = 'none'      
+
+          const formSection = document.getElementById('form-section-id')
+          formSection.style.display = 'none'  
 
   })
 } 
