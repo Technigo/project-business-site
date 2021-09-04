b3e057d97075847c4949373306d90f0f38e53d84
ruruahn <81482699+ruruahn@users.noreply.github.com>
2021-09-04 10:43:02 +0200

    display hero image, submit form and accordion in mobile, ipad and desktop screen

--- code/script.js
+++ code/script.js
@@ -1,9 +1,3 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-
-// 자바스트립트야~! html document에 가서 Element (<...>)들 중에 아이디가 "answer1"인 애한테 "active"라는 스타일 클래스(classList)를 줬다가 뺐었다가(toggle) 해!
-
 function banana() {
   document.getElementById("answer1").classList.toggle("active")
 }
