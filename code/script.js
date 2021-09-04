eb901b8d8430f33e245ba769853ed18feefc357b
molbimien <43146046+molbimien@users.noreply.github.com>
2021-09-04 20:49:24 +0200

    added with to block-section for larger viewports

--- code/script.js
+++ code/script.js
@@ -1,3 +1,8 @@
+// some instrukctions here
+
+// const items = document.querySelectorAll(".accordion a");
+
+
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
