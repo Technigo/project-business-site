f8850247a65f04834c5f210a842ab6c1c206531e
intehon <emelie.soderhielm@gmail.com>
2021-09-04 22:16:04 +0200

    add accordion

--- code/script.js
+++ code/script.js
@@ -1,9 +1,20 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+
+// function toggleAnswer() {
+//   this.classList.toggle('active')
+// }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+
+// document.getElementById('q1').onclick = toggleAnswer
+
+document.getElementById('button1').onclick = toggleAnswer
+document.getElementById('button2').onclick = toggleAnswer
+document.getElementById('button3').onclick = toggleAnswer
+document.getElementById('button4').onclick = toggleAnswer
+
+function toggleAnswer() {
+  this.classList.toggle('open')
+}
