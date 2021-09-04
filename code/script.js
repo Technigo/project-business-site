265b7a5f987055ca5479c18b8b71569eaf259e49
intehon <emelie.soderhielm@gmail.com>
2021-09-05 00:07:17 +0200

    footer fixed

--- code/script.js
+++ code/script.js
@@ -1,14 +1,5 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
 
-// function toggleAnswer() {
-//   this.classList.toggle('active')
-// }
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-
-// document.getElementById('q1').onclick = toggleAnswer
+// Calls for accordion to open
 
 document.getElementById('button1').onclick = toggleAnswer
 document.getElementById('button2').onclick = toggleAnswer
