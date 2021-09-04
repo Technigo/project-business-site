17f82edf4ec31876ede76843933ba23e2f94d8fb
Eddie Lundgren <Eddie.lundgren@hotmail.com>
2021-09-04 23:24:56 +0200

    added a somewhat working accordion

--- code/script.js
+++ code/script.js
@@ -1,9 +1,16 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.querySelectorAll('.accordion__button').forEach(button => {
+  button.addEventListener('click', () => {
+    const accordionContent = button.nextElementSibling;
+
+    button.classList.toggle('accordion__button--active');
+
+    if (button.classList.contains('accordion__button--active')) {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+
+    } else {
+      accordionContent.style.maxHeight = 0;
+    }
+
+  });
+});
