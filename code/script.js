54b94d988093faf280cf540a30174fd6f53df36a
MT-dotse <madelene.trang333@gmail.com>
2021-09-04 22:55:29 +0300

    added a hero header, form, accordion and a footer

--- code/script.js
+++ code/script.js
@@ -1,9 +1,25 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+
+
+
+
+
+document.getElementById('question1').onclick = toggleMagic
+
+document.getElementById('question2').onclick = toggleMagic
+
+document.getElementById('question3').onclick = toggleMagic
+
+document.getElementById('question4').onclick = toggleMagic
+
+document.getElementById('question5').onclick = toggleMagic
+
+function toggleMagic(){
+  this.classList.toggle('open')
+}
+
+
