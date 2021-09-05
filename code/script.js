7a07919446f467baa2d337b407089ea30e73a2a7
molbimien <43146046+molbimien@users.noreply.github.com>
2021-09-05 02:44:40 +0200

    major commit - added the red level requirements for which I rewrote much of the styling for different viewports

--- code/script.js
+++ code/script.js
@@ -1,20 +1,30 @@
-// some instrukctions here
+// some instrukctions here from the js adapted from webdevtrick
 
-// const items = document.querySelectorAll(".accordion a");
+// Code By Webdevtrick ( https://webdevtrick.com )
+const items = document.querySelectorAll(".accordion a");
 
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+// what I had before -->
 
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggleAccordion() {
-  this.classList.toggle("active")
-}
+// function toggleAccordion() {
+//   this.classList.toggle("active")
+// }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("accordion-item1").onclick = toggleAccordion
-document.getElementById("accordion-item2").onclick = toggleAccordion
-document.getElementById("accordion-item3").onclick = toggleAccordion
-document.getElementById("accordion-item4").onclick = toggleAccordion
+// document.getElementById("accordion-item1").onclick = toggleAccordion
+// document.getElementById("accordion-item2").onclick = toggleAccordion
+// document.getElementById("accordion-item3").onclick = toggleAccordion
+// document.getElementById("accordion-item4").onclick = toggleAccordion
 
 // segment = ['segment1', 'segment2', ...]
 // segment.forEach(segment => {
