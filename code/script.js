0c4765b4ab1b024562a30c5901192abddc3c18b9
molbimien <43146046+molbimien@users.noreply.github.com>
2021-09-05 20:43:31 +0200

    cleaned up the code, added HTML and CSS comments and updated Readme

--- code/script.js
+++ code/script.js
@@ -1,6 +1,8 @@
-// some instrukctions here from the js adapted from webdevtrick
 
-// Code By Webdevtrick ( https://webdevtrick.com )
+// This javascript adds and remove the class "active" on the section you click on. Used for the accordion on the site.
+
+// Code adapted from Webdevtrick ( https://webdevtrick.com )
+
 const items = document.querySelectorAll(".accordion a");
 
 function toggleAccordion(){
@@ -10,24 +12,3 @@ function toggleAccordion(){
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
 
-
-// what I had before -->
-
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-// function toggleAccordion() {
-//   this.classList.toggle("active")
-// }
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("accordion-item1").onclick = toggleAccordion
-// document.getElementById("accordion-item2").onclick = toggleAccordion
-// document.getElementById("accordion-item3").onclick = toggleAccordion
-// document.getElementById("accordion-item4").onclick = toggleAccordion
-
-// segment = ['segment1', 'segment2', ...]
-// segment.forEach(segment => {
-//   const fieldEl = document.getElementById(segment);
-//   .onclick = toggle
-// });
