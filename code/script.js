0a77f2139fe749af8dd108c17ada35cfeffc096e
Amanda Tilly <amandatilly@Amandas-2.lan>
2021-09-05 15:46:14 +0200

    header font, colors, accordion text and placeholders

--- code/script.js
+++ code/script.js
@@ -13,3 +13,5 @@ document.getElementById("section2").onclick = toggle
 document.getElementById("section3").onclick = toggle
 
 document.getElementById("section4").onclick = toggle
+
+document.getElementById("section5").onclick = toggle
