f70ff01c98f761b228760ae06c88bcf92f591d4f
Emelie <pcruzemelie@gmail.com>
2021-09-06 01:16:54 +0200

    added texture and dark filter to hero + centered hero caption

--- code/script.js
+++ code/script.js
@@ -7,3 +7,19 @@ function toggle() {
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+
+
+// This javascript adds and remove the class "active" on the section you click on. Used for the accordion on the site.
+
+// Code adapted from Webdevtrick ( https://webdevtrick.com )
+
+
+
+let accordions = document.querySelectorAll("div.accordion");
+
+for(let i = 0; i < accordions.length; i++){
+  accordions[i].onclick = function(){
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("show");
+  }
+}
