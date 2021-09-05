0c2e8ffc9929241f8bf9bb06d834355bc429c599
NinaAlejandra <Niinaa___@hotmail.com>
2021-09-05 20:52:13 +0200

    small changes

--- code/script.js
+++ code/script.js
@@ -1,15 +1,15 @@
 // Hopefully I understood what this does. Still unsure. 
-function toggle(accordion) {
-  this.classList.toggle("active")
+function toggle() {
+  this.classList.toggle('active')
 }
 
 // Still kind of confused about how this works
-document.getElementById("section1").onclick = toggle
-document.getElementById("section2").onclick = toggle
-document.getElementById("section3").onclick = toggle
-document.getElementById("section4").onclick = toggle
-document.getElementById("section5").onclick = toggle
-document.getElementById("order").onclick = toggle
+document.getElementById('section1').onclick = toggle
+document.getElementById('section2').onclick = toggle
+document.getElementById('section3').onclick = toggle
+document.getElementById('section4').onclick = toggle
+document.getElementById('section5').onclick = toggle
+document.getElementById('order').onclick = toggle
 
 
 
