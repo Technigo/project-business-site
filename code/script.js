d89e0f1747ae3fe05602673d323bdf7702f7b875
Maja Larsson <majaja@live.se>
2021-09-06 04:00:18 +0200

    project3

--- code/script.js
+++ code/script.js
@@ -29,7 +29,7 @@ for (i = 0; i < acc.length; i++) {
 
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
