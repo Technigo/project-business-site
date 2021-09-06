7130e99bca6248b5b2e8efbdd693a35d1f635860
vrill <torninger@gmail.com>
2021-09-06 22:21:00 +0200

    FFinally done. v1.0.

--- code/script.js
+++ code/script.js
@@ -1,9 +1,26 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
+
+    /* the below i don't fully understand yet, to be honest
+    but i really wanted the effect! :) 
+    https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion_animate */
+
+    /* define a variable that looks for the questions class */
+var faq = document.getElementsByClassName("questions");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+  faq[i].addEventListener("click", function() {
+    /* looks in the questions class and 
+    adds and removes (toggles) the active class when clicked */
+    this.classList.toggle("active");
+    /* the part that hides or shows the class */
+    var answers = this.nextElementSibling;
+    if (answers.style.maxHeight) {
+      answers.style.maxHeight = null;
+    } else {
+      answers.style.maxHeight = answers.scrollHeight + "px";
+    }
+  });
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+    /* i get the feeling this is overcomplicating things for the effect i'm after,
+    but as i dive deeper into JS maybe it will start to make sense */
