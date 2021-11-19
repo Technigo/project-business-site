7d9f623f3f5f09655fb38428a9ffe8e9a2de5aaa
Haru Ahn <81482699+ruruahn@users.noreply.github.com>
2021-11-19 16:03:41 +0100

    css update

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Business Site
-This business site shows basic features of any online website mainpage. It contains a hero image which is very trendy as a header these days. Also there is a sign up form for users, a frequently asked questions in an accordion form.
 
 ## The problem
 
-Touching java script for the first time was quite challenging for me as I have never experienced any type of interactive code program before. Understanding the basic notion of eventListener and how to trigger it takes a lot of time, but I managed to look through all the essential and optional reading material and it helped me a lot.
-
 ## View it live
-https://relaxed-leavitt-42b273.netlify.app
--- code/script.js
+++ code/script.js
@@ -1,19 +1,35 @@
-function banana() {
-  document.getElementById("answer1").classList.toggle("active")
-}
-document.getElementById("question1").onclick = banana
 
-function apple() {
-  document.getElementById("answer2").classList.toggle("active")
+// A function that adds and remove the class "active" on the section you click on.
+function toggle() {
+  this.classList.toggle("active")
 }
-document.getElementById("question2").onclick = apple
 
-function kiwi() {
-  document.getElementById("answer3").classList.toggle("active")
-}
-document.getElementById("question3").onclick = kiwi
+// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+document.getElementById("FAQ1").onclick = toggle
+document.getElementById("FAQ2").onclick = toggle
+document.getElementById("FAQ3").onclick = toggle
+document.getElementById("FAQ4").onclick = toggle
+document.getElementById("FAQ5").onclick = toggle
 
-function plum() {
-  document.getElementById("answer4").classList.toggle("active")
+
+// A function that adds and remove the class "minus" on the section you click on.
+
+document.getElementById("question1").onclick = () => {
+  question1.classList.toggle("minus")
 }
-document.getElementById("question4").onclick = plum
+
+document.getElementById("question2").onclick = () => {
+  question2.classList.toggle("minus")
+}
+
+document.getElementById("question3").onclick = () => {
+  question3.classList.toggle("minus")
+}
+
+document.getElementById("question4").onclick = () => {
+  question4.classList.toggle("minus")
+}
+
+document.getElementById("question5").onclick = () => {
+  question5.classList.toggle("minus")
+}
