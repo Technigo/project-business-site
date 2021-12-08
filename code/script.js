819fcdb37ae3d5607e879a69a6c0dfd27faa7620
louise-becker <louise.becker@gmail.com>
2021-12-08 16:39:19 +0100

    bg image, form and accordeon functionality added. no content or styling yet

--- code/script.js
+++ code/script.js
@@ -2,8 +2,12 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 function toggle() {
-  this.classList.toggle("active")
+  this.classList.toggle('active');
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById('faq-1').onclick = toggle;
+document.getElementById('faq-2').onclick = toggle;
+document.getElementById('faq-3').onclick = toggle;
+document.getElementById('faq-4').onclick = toggle;
+document.getElementById('faq-5').onclick = toggle;
