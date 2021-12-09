6b8d33e1ba3433c93ffe0a8f5ab1d724db42423f
JessicaNordahl <jessica.nordahl@hotmail.com>
2021-12-09 15:37:55 +0100

    changed the project and started out a new approach on it's apperance

--- code/script.js
+++ code/script.js
@@ -3,7 +3,9 @@
 // console.log(this) to see how it works
 function toggle() {
   this.classList.toggle("active")
+ 
 }
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
-document.getElementById("section2").onclick = toggle
+document.getElementById("section2").onclick = toggle 
+console.log(toggle)
