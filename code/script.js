39940fc4ae696e2d03bc087492996ad415dd0a0e
jiiin <jiiin.cho.dev@gmail.com>
2022-01-25 21:03:01 +0100

    first mockup completed

--- code/script.js
+++ code/script.js
@@ -1,9 +1,37 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
+//form validation
+function invalid(e) {
+  const element = e.target.parentElement;
+  element.classList.add("invalid");
+}
+function valid(e) {
+  const element = e.target.parentElement;
+  const value = e.target.value;
+  !value && element.classList.remove("invalid");
+}
+const validationElements = ["#uname", "#email", "#pass", "#agreed"];
+validationElements.map((elementId) => {
+  const element = document.querySelector(elementId);
+  element.addEventListener("invalid", invalid);
+  element.addEventListener("keypress", valid);
+});
+const form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
+  // e.preventDefault();
+  form.reset();
+});
+
 function toggle() {
-  this.classList.toggle("active")
+  this.classList.toggle("active");
+  const icon = this.children[0].children[0];
+
+  if (this.classList.contains("active")) {
+    // section is expanded
+    icon.src = "./assets/icons/minus-solid.svg";
+  } else {
+    icon.src = "./assets/icons/plus-solid.svg";
+  }
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("section1").onclick = toggle;
+document.getElementById("section2").onclick = toggle;
