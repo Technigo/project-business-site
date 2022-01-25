f8f715caf970d5441b4e52c24c7a2b7ab676d81a
eyahya-khan <eyahyakhan@gmail.com>
2022-01-25 10:41:57 +0100

    complete signup form for mobile version

---
+++ js/accordion.js
@@ -0,0 +1,30 @@
+// A function that adds and remove the class "active" on the section you click on.
+// We haven't really talked about ´this´ yet, but we will... ;)
+// console.log(this) to see how it works
+/*
+function toggle() {
+  this.classList.toggle("active")
+}
+*/
+// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+//document.getElementById("section1").onclick = toggle
+
+
+
+/*dont forget to link the javascript file here! */
+  
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+      });
+    }
+   
