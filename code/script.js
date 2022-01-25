c18f94e5413a3411b69db214c108b32113c15d68
Emma Högberg <emmahogberg88@gmail.com>
2022-01-25 07:56:07 +0100

    change styling on radiobuttons to change direction

--- code/script.js
+++ code/script.js
@@ -7,7 +7,6 @@ const question = document.getElementsByClassName("question")
 for (let i = 0; i < question.length; i++) {
   question[i].addEventListener("click", function() {
     this.classList.toggle("active")
-    
   })
 }
 
