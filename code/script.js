9c769faa0af0220ba699412258740f49422b821c
anki palviainen <anki.palviainen@gmail.com>
2022-01-26 16:25:37 +0200

    add java correct

--- code/script.js
+++ code/script.js
@@ -1,18 +1,23 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+const sectionOne = document.getElementById("section1")
+sectionOne.onclick = () => {
+  sectionOne.classList.toggle("active")
+  } 
 
-document.getElementById("section2").onclick = toggle
+  const sectionTwo = document.getElementById("section2")
+  sectionTwo.onclick = () => {
+    sectionTwo.classList.toggle("active")
+    } 
 
-document.getElementById("section3").onclick = toggle
+    const sectionThree = document.getElementById("section3")
+    sectionThree.onclick = () => {
+      sectionThree.classList.toggle("active")
+      } 
 
-
-
-
-document.getElementById("dumbbell").onclick = toggle
+      const dumbbell = document.getElementById("dumbbell")
+      dumbbell.onclick = () => {
+        dumbbell.classList.toggle("press")
+        } 
