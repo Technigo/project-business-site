191c0bb458258ef16e97afccd435ddd097a40ded
anki palviainen <anki.palviainen@gmail.com>
2022-01-26 10:33:34 +0200

    things

--- code/script.js
+++ code/script.js
@@ -11,3 +11,8 @@ document.getElementById("section1").onclick = toggle
 document.getElementById("section2").onclick = toggle
 
 document.getElementById("section3").onclick = toggle
+
+
+
+
+document.getElementById("dumbbell").onclick = toggle
