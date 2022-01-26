94d17e243ac56dbf7a7b62e18305d250a28cbb1c
mathildakarlsson <86979661+mathildakarlsson@users.noreply.github.com>
2022-01-26 23:07:48 +0100

    added sign up form and FAQ and coded first javascript

--- code/script.js
+++ code/script.js
@@ -1,9 +1,27 @@
+
 // A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
+
 function toggle() {
   this.classList.toggle("active")
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+
+document.getElementById("Question1").onclick = toggle
+
+document.getElementById("Question2").onclick = toggle
+
+
+
+
+
+
+// sign up form javascript below
+
+document.getElementById('form').onsubmit = (event) => {
+  event.preventDefault()
+
+  const inputValue = document.getElementById('nameInput').value
+
+ document.getElementById('thankYou').innerText = `Thank you ${inputValue}` 
+}
