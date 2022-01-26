e6213434da5a9e62cabff7a6bf66858045392c36
Emma Högberg <emmahogberg88@gmail.com>
2022-01-26 16:13:48 +0100

    add minor changes

--- code/script.js
+++ code/script.js
@@ -3,6 +3,8 @@
 // console.log(this) to see how it works
 
 const question = document.getElementsByClassName("question") 
+//const form = document.getElementById("form")
+//const inputName = document.getElementsById("firstname").value
 
 for (let i = 0; i < question.length; i++) {
   question[i].addEventListener("click", function() {
@@ -11,9 +13,19 @@ for (let i = 0; i < question.length; i++) {
 }
 
 
+//form.onsubmit = e => {
+//  e.preventDefault()
+//}
+//document.getElementsById("greeting") = inputName
+
 /*function toggle() {
   this.classList.toggle("active")
 }*/
 
+//(‘Hello $ {inputValue}!’) 
+
+
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 /* document.getElementById("section1").onclick = toggle */
+
+/*insead of "click* use "onsubmit"*/
