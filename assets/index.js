9dd79bb31878633a07ecbac8710b8c487cd3c31d
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-01-26 20:52:29 +0100

    Add javascript to accordion

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,10 @@
+const inputLabels = document.querySelectorAll(".input__header");
+
+inputLabels.forEach(label => {
+  label.addEventListener("click", () => {
+    label.classList.toggle("active");
+  })
+})
 
 
 
