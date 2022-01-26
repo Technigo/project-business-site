80b3f2329d0d195a0a8d98964e381ec3ad7db8d0
Åsa Sieurin <asasieurin@Asas-Air.lan>
2022-01-26 14:02:15 +0100

    first commit

--- code/script.js
+++ code/script.js
@@ -1,9 +1,15 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
+
+console.log('HELLO')
+
 function toggle() {
   this.classList.toggle("active")
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+
+//document.getElementById('form').onsubmit = event => {
+  event.preventDefault()
