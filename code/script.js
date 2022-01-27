025a521669e3ee27a46317ecf805d9b995cbb2ea
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-01-27 16:13:25 +0100

    added the footer and started the accordion

--- code/script.js
+++ code/script.js
@@ -1,14 +1,33 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
+
+// const accordion = document.getElementsByClassName('accordion-question');
+// let i;
+
+// for (i = 0; i < accordion.length; i++) {
+//   accordion[i].addEventListener('click', function() {
+//     this.classList.toggle('active');
+//     const answer = this.nextElementSibling;
+//     if (answer.style.maxHeight) {
+//       answer.style.maxHeight = null;
+//     } else {
+//       answer.style.maxHeight = answer.scrollHeight + 'px';
+//     } 
+//   });
+// }
+const accordion = document.getElementsByClassName("accordion-question");
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    const answer = this.nextElementSibling;
+    if (answer.style.maxHeight) { // means that if `answer` has a height (is open), put it at `null` (close it)
+      answer.style.maxHeight = null;
+    } else { // otherwise (if `answer` has no height), put the other answers' height at `null` (close them) if there are any
+      const active = document.querySelectorAll(".accordion-question.active");
+      for (let j = 0; j < active.length; j++) {
+        active[j].classList.remove("active");
+        active[j].nextElementSibling.style.maxHeight = null;
+      }
+      this.classList.toggle("active"); // ... and open the answer for the question clicked
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
 }
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-
-
-
-
-
