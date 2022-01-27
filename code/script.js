eec5803cfde84216534e614e189f9bfa28fda0b3
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-01-27 22:23:01 +0100

    More styling added to the accordion

--- code/script.js
+++ code/script.js
@@ -1,17 +1,29 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+//function toggle() {
+// this.classList.toggle("active")
+//}
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+//document.getElementById("section1").onclick = toggle
 
+const accordionBtns = document.querySelectorAll(".accordion");
 
-var burgerMenu = document.getElementById('burger-menu');
-var overlay = document.getElementById('menu');
-burgerMenu.addEventListener('click',function(){
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
 });
