f996aa96e73731d34432fe534c2c742e3ea5e707
eyahya-khan <eyahyakhan@gmail.com>
2022-01-27 21:58:13 +0100

    All requirement complete

--- js/accordion.js
+++ js/accordion.js
@@ -1,21 +1,4 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
 
-/*
-function accordion() {
-  this.classList.toggle("active");
-}*/
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-/*
-var acc = document.getElementById("section1");
-acc.onclick = accordion
-*/
-
-
-/*dont forget to link the javascript file here! */
- 
     let acc = document.getElementsByClassName("accordionSection__button");
     let i;
     
