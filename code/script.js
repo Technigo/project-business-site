aefe4fa6e64e1265cc7246eb029b7ac99ed766e8
SofiePellegrini <sofie.pellegrini@outlook.com>
2022-01-27 22:54:39 +0100

    worked more on form and accordion

--- code/script.js
+++ code/script.js
@@ -1,13 +1,10 @@
-// A function that adds and remove the class "active" on the section you click on.
+var acc = document.getElementsByClassName("content");
+var i;
 
-function toggle() {
-  this.classList.toggle("active")
-}
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-document.getElementById("section2").onclick = toggle
-document.getElementById("section3").onclick = toggle
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+  })
 
 
-
+  }
