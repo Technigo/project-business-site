95eae98f8874dae0492346373a733b0d7c018c4c
Lisen Lundgren <lisen@Lisens-MacBook-Pro.local>
2022-01-27 21:37:32 +0100

    blue level requirements

--- code/script.js
+++ code/script.js
@@ -1,4 +1,4 @@
-// A function that adds and remove the class "active" on the section you click on.
+/*// A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 function toggle() {
@@ -7,3 +7,24 @@ function toggle() {
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+
+*/
+var acc = document.getElementsByClassName("question");
+var i; 
+
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
