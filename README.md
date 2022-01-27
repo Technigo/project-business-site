232019fd588e99495783b773f7d15125aaa55059
TereseClaesson <terese.claesson@gmail.com>
2022-01-27 12:50:23 +0100

    The HTML stucture and CSS and JS

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Business Site
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This is an assigment that requeries that 
 
 ## The problem
 
--- code/script.js
+++ code/script.js
@@ -1,9 +1,18 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
-
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+
+var acc = document.getElementsByClassName ("accordion");
+var i; 
+
+for (i = 0 < acc.length; i++) {
+  acc[i].addEventListener ("click",function() {
+    this.classList.toggle ("active");
+    
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none" ;
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
