8ce3339ed38e2bec2e3678d9e1445864bdbe28c8
Laura Sjolander <laura.sjolander@gmail.com>
2022-01-27 22:08:18 +0100

    added accordion and dog picture

--- code/script.js
+++ code/script.js
@@ -1,17 +1,20 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+//function toggle() {
+// this.classList.toggle("active")
+//}
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+//document.getElementById("section1").onclick = toggle
 
 
 document.querySelectorAll('.accordion-button').forEach(button => {
-    button.addEventListener('click', () => {
-      button.classList.toggle('accordion-button--active');
-    })
+  button.addEventListener('click', () => {
+    button.classList.toggle('accordion-button--active');
+
+    const accordionContent = button.nextElementSibling;
+    accordionContent.classList.toggle("active");
+  })
 });
 
 //const accordionContent = button.nextElementSibling;
