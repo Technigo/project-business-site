c72e14418772c4146f130765663e47dc73b46cf1
Michael Dohmann Chang <michaelchang@Michaels-MBP.lan>
2022-01-27 20:16:42 +0100

    did this javascript even do anything

--- code/script.js
+++ code/script.js
@@ -45,22 +45,25 @@ document.getElementById('form').onsubmit = (event) => {
 
 
 // Check innerheight and apply for landscape mode
-const landscapeMode = window.matchMedia('(max-device-width: 930px) and (orientation: landscape)');
-const portraitMode = window.matchMedia('(max-device-width: 669px) and (orientation: portrait)');
-const headerHeight = document.querySelector('.header');
-let height = window.outerHeight;
+// const landscapeMode = window.matchMedia('(max-device-width: 930px) and (orientation: landscape)');
+// const portraitMode = window.matchMedia('(max-device-width: 669px) and (orientation: portrait)');
+// const headerHeight = document.querySelector('.header');
+// let height = window.outerHeight;
 
-if (landscapeMode.matches) {
-  headerHeight.style.height = height + "px";
-}
+// if (landscapeMode.matches) {
+//   headerHeight.style.height = height + "px";
+// }
 
-landscapeMode.addListener(function(m) {
-  if(m.matches) {
-    headerHeight.style.height = height + "px";
-  } else {
-    headerHeight.style.height = "75vh";
-  }
-});
+// landscapeMode.addListener(function(m) {
+//   if(m.matches) {
+//     headerHeight.style.height = height + "px";
+//   } else {
+//     headerHeight.style.height = "75vh";
+//   }
+// });
+
+
+
 
 // portraitMode.addListener(function(g) {
 //   if(g.matches) {
