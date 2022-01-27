8d1729099744cf170009bac7ba0c2b8422a2d076
Åsa Sieurin <asasieurin@Asas-Air.lan>
2022-01-27 12:31:14 +0100

    thursday lunch

--- code/script.js
+++ code/script.js
@@ -1,18 +1,16 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-
 console.log('HELLO')
 
+// A function that adds and remove the class "active" on the section you click on.
 function toggle() {
   this.classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
+// Selects a "section", and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
 document.getElementById("section2").onclick = toggle
 document.getElementById("section3").onclick = toggle
 document.getElementById("section4").onclick = toggle
 
+//Den här funktionen gör att min accordian inte fungerar? Men vår lärare sa att vi skulle ha den för att "behålla" informationen från formuläret?
 //document.getElementById('form').onsubmit = event => {
-  event.preventDefault()
+  //event.preventDefault()
