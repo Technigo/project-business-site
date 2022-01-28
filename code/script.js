2c56df502422d38bbfc2c38207d946e9608616b9
Emma Högberg <emmahogberg88@gmail.com>
2022-01-28 08:03:16 +0100

    add comments to the code

--- code/script.js
+++ code/script.js
@@ -8,16 +8,20 @@ const delivery = document.getElementById("delivery")
 //Showing message when submitting the form
 form.addEventListener("submit", e => {
   e.preventDefault()
-  let firstName = document.getElementById("firstname").value
+
+  //Save the data from a specific input field
+  const firstName = document.getElementById("firstname").value
   
   message.style.display = "block"
   signup.style.display = "none"
+
+  //grab the first chlid of the message (parent) and change the text content
   message.firstElementChild.textContent = `Hi ${firstName},`
   
   //save data from our form and get the delivery frequency to use in text.
-  let ourFormData = new FormData(e.target)
-  let deliverFrequency = ourFormData.get("delivery-frequency")
-
+  const ourFormData = new FormData(e.target)
+  const deliverFrequency = ourFormData.get("delivery-frequency")
+  
   delivery.innerText = `Thank you for your ${deliverFrequency} order of flowers!`
 }) 
 
