9d67f7015b3553563ae5bc3c07c0a0e8867d930b
mathildakarlsson <86979661+mathildakarlsson@users.noreply.github.com>
2022-01-28 01:00:11 +0100

    styled accordion and changed accordion jS

--- code/script.js
+++ code/script.js
@@ -1,15 +1,81 @@
 
 // A function that adds and remove the class "active" on the section you click on.
 
-function toggle() {
-  this.classList.toggle("active")
-}
+// function toggle() {
+//   this.classList.toggle("active")
+// }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 
-document.getElementById("Question1").onclick = toggle
+// document.getElementById("Question1").onclick = toggle
 
-document.getElementById("Question2").onclick = toggle
+// document.getElementById("Question2").onclick = toggle
+
+// document.getElementById("Question3").onclick = toggle
+
+// document.getElementById("Question4").onclick = toggle
+
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+    
+//   });
+// }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+var acc = document.getElementsByClassName("accordion1");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+var acc = document.getElementsByClassName("accordion3");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
 
 
 
