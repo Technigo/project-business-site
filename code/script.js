d95511c8db7349295279c21bac968603c7768417
Rawisara Saman <rawiisara@gmail.com>
2022-01-28 18:11:13 +0700

    styled radio buttons and checkbox

--- code/script.js
+++ code/script.js
@@ -6,7 +6,8 @@ questions.forEach((question) => {
   })
 })
  
-// Force the video to always autoplay (it doesn't always play despite the autoplay attribut in the video tag, the video size is around 1MB)
+// Force the video to always autoplay just an extra efford to make sure the video will be rendered.
+// (Sometimes the video doesn't always play despite the autoplay attribut in the video tag, the video size is around 1MB, it might also be cache issue)
 function onPlayerReady(event) {
   event.target.playVideo();
 }
