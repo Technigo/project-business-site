8086fdca97e41d97ded72106be7e2010ee5c007e
joannaringqvist <joanna.ringqvist@gmail.com>
2022-01-28 22:41:16 +0100

    Cleaned up the code a bit

--- code/script.js
+++ code/script.js
@@ -1,8 +1,7 @@
-const questions = document.getElementsByClassName('faq--question');
+const questions = document.getElementsByClassName('faq-question');
 
 for (i=0; i<questions.length; i++) {
   questions[i].addEventListener('click', function() {
-    this.nextElementSibling.classList.toggle('faq--answer-active');
-    
+    this.nextElementSibling.classList.toggle('faq-answer-active');
   })
 }
