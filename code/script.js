28a1b901c9ace1d4a4f75aadeff1699d762fbadd
Dorothea-B <barre.dorothea@gmail.com>
2022-01-28 13:23:37 +0100

    boilerplate content

--- code/script.js
+++ code/script.js
@@ -7,3 +7,43 @@ function toggle() {
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+document.getElementById("section2").onclick = toggle
+document.getElementById("section3").onclick = toggle
+document.getElementById("section1").onclick = toggle
+
+
+// document.getElementsByClassName('answer').onclick = toggle ;
+
+
+
+
+// const btn = document.getElementById("button")
+
+// btn.onclick=toggleRedSwitch
+
+// "document.getElementById('id1').style.color = 'red'"
+
+
+// function RedSwitch() {
+
+//   btn.classList.style.color = 'red'
+// }
+
+// document.getElementByName("answer").onclick = toggle
+
+
+// document.getElementById(id).onclick = function(){code}	Adding event handler code to an onclick event
+
+
+// This example returns a list of all <p> elements with class="intro".
+
+// Example
+// const x = document.querySelectorAll("p.intro");
+
+// function validateForm() {
+//   let x = document.forms["myForm"]["fname"].value;
+//   if (x == "") {
+//     alert("Name must be filled out");
+//     return false;
+//   }
+// }
