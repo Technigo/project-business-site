33d8b3893356d7896a274e09bf81f374b2412200
TereseClaesson <terese.claesson@gmail.com>
2022-01-28 15:38:12 +0100

    Fixed my javaScript link and made an accordion

--- code/script.js
+++ code/script.js
@@ -1,18 +1,10 @@
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+const accordion = 
+document.getElementsByClassName ('contentBx');
 
-var acc = document.getElementsByClassName ("accordion");
-var i; 
+for (i = 0; i<accordion.length; i++){
+accordion [i].addEventListener('click',
+  function(){
+    this.classList.toggle('active')
+})
 
-for (i = 0 < acc.length; i++) {
-  acc[i].addEventListener ("click",function() {
-    this.classList.toggle ("active");
-    
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none" ;
-    } else {
-      panel.style.display = "block";
-    }
-  });
 }
