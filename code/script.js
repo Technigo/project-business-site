1d6cc66822e535d3e2030cac809086de45ccbfc5
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-01-29 23:10:36 +0100

    Final version, added interactive components and page reloads

--- code/script.js
+++ code/script.js
@@ -1,14 +1,29 @@
 // A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
 function toggle() {
   this.classList.toggle("active")
 }
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-document.getElementById("section2").onclick = toggle
-document.getElementById("section3").onclick = toggle
+//Improved default code by using for-loop and event listeners
+//Selecting all classes with the question selector, looping through them and adding click event listener to each of them
+let questionSections = document.querySelectorAll('.question');
+for (i = 0; i < questionSections.length; i++){
+  questionSections[i].addEventListener('click', toggle);
+}
 
-//is it possible to loop through these to minimize the code?
-//queryselectorall and for each?
+//Adding an automatic page reload after 5s using setTimeOut, and modifying the DOM for confirmation message after submit event 
+const submitForm = document.querySelector('form');
+const submittedResponse = document.createElement('h1');
+
+  submitForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  submitForm.innerHTML = '';
+  submitForm.style.width = '200px';
+  submitForm.style.height = '500px';
+  submittedResponse.innerHTML = '<span style="display: flex; flex-direction: column; align-items: center;"><p>Thanks for your submit.</p><p>You will be redirected to the landing page in 5s</p></span>';
+  submitForm.appendChild(submittedResponse);
+
+  setTimeout(() => {
+    location.reload();
+  }, 5000)
+});
