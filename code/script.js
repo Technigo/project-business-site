dab5f418a862a64baa3980dd6eec5f4b953fd944
Arianna Allahyar <arianna@Ariannas-MBP.lan>
2022-01-30 17:28:46 +0100

    Message after submitting form added

--- code/script.js
+++ code/script.js
@@ -1,3 +1,13 @@
+// Load message after signing up
+const form = document.getElementById("form")
+const message = document.getElementById("submit-form")
+
+form.onsubmit = event => {
+  event.preventDefault();
+  message.style.display = "block";
+  form.style.display = "none";
+}
+
 // Converts key value pairs to array of elements with the class name 'question'
 const headers = Object.values(document.getElementsByClassName('question'));
 
