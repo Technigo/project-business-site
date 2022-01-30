da9664f15565b5bd6779827af4fd4fb6daa8ed98
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-01-30 20:17:50 +0100

    added grid to deskrop, cleand up in gtml,css and js

--- code/script.js
+++ code/script.js
@@ -1,7 +1,3 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-
 const sectionOne =
 document.getElementById("section1")
 
@@ -35,23 +31,3 @@ sectionFour.onclick = () => {
 
 
 
-
-
-
-
-// const button = 
-// document.getElementById("button").onclick = toggleAccordion
-
-// button.onclick = toggleAccordion
-
-// function toggleAccordion() {
-//   this.classlist.toggleAccordion ("active")
-// }
-
-
-// function toggle() {
-//   this.classList.toggle("active")
-//}
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-// document.getElementById("section1").onclick = toggle 
