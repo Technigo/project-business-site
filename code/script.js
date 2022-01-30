b96113f93b919b5b68cbe24b759863cc5fb972bc
Amanda Tange <amandatange@hotmail.com>
2022-01-30 13:56:50 +0100

    cleaned up the form submit message by adding more code to increase readability

--- code/script.js
+++ code/script.js
@@ -4,12 +4,22 @@
 document.getElementById('form').onsubmit = event => {
   event.preventDefault()
 
-  const inputValue = document.getElementById('first-name').value
+  // Getting the first name with capital letter with perhaps some 'unnecessary' code to make it a little easier to read
+  const firstName = document.getElementById('first-name').value
+  const firstLetterToUpperCase = firstName[0].toUpperCase()
+  const restOfName = firstName.slice(1)
+  const name = firstLetterToUpperCase + restOfName
 
-  const thankYouMsg = `Thank you ${inputValue[0].toUpperCase()}${inputValue.slice(1)}! We will contact you shortly to discuss your ${form["private-or-business"].value} options.`
-  document.getElementById('form').innerText = thankYouMsg;
+  //getting the choice of insurance type from the form
+  const insuranceType = form["private-or-business"].value
+
+  const thankYouMsg = `Thank you ${name}! We will contact you shortly to discuss your ${insuranceType} options.`
+
+  document.getElementById('form').innerHTML = thankYouMsg;
 }
 
+
+//function to make the accordion open and close by toggling the active class, stolen almost entirely from youtube
 document.querySelectorAll(".question").forEach(question => {
   question.addEventListener("click", () => {
     const accordionContent = question.nextElementSibling;
