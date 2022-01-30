fa496a47d4ea1678f3e46c4c3dc85ce7e459a6aa
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-01-30 12:42:14 +0100

    added some content and fixed adult mode

--- code/script.js
+++ code/script.js
@@ -1,5 +1,5 @@
 // accordion
-const accordion = document.getElementsByClassName("accordion-question");
+const accordion = document.getElementsByClassName('accordion-question');
 
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
@@ -7,7 +7,7 @@ for (let i = 0; i < accordion.length; i++) {
     if (answer.style.maxHeight) { // means that if `answer` has a height (is open), put it at `null` (close it)
       answer.style.maxHeight = null;
     } else { // otherwise (if `answer` has no height), put the other answers' height at `null` (close them) if there are any
-      const active = document.querySelectorAll(".accordion-question.active");
+      const active = document.querySelectorAll('.accordion-question.active');
       for (let j = 0; j < active.length; j++) {
         active[j].classList.remove('active');
         active[j].nextElementSibling.style.maxHeight = null;
@@ -59,9 +59,9 @@ adult.addEventListener('click', function () {
 document.addEventListener('DOMContentLoaded', displayChild);
 
 function displayChild() {
+  document.getElementById('subtitle').innerText = 'vegetables';
   document.getElementById('name').innerText = 'Your child\'s name';
   document.getElementById('instruction').innerText = 'your child';
-  document.getElementById('subtitle').innerText = 'vegetables';
   document.getElementById('selectChild').style.display = 'block';
   document.getElementById('selectAdult').style.display = 'none';
   document.getElementById('check').innerText = 'vegetables that your child like';
@@ -73,37 +73,44 @@ function displayChild() {
   document.getElementById('check6').innerText = 'Cruciferous';
 
   document.getElementById('buttonSubmit').innerText = 'Please register my child!';
+  document.getElementById('questionBusiness').innerText = 'grocery store';
+  document.getElementById('answerWho').innerText = 'your child';
+  document.getElementById('answerType').innerText = 'vegetables';
+  document.getElementById('questionType').innerText = 'vegetables';
+  document.getElementById('relatedType').innerText = 'vegetable';
+
 
   document.getElementById('buttonAdult').innerText = 'Interested in our 18+ services?';
-  const greenMedium = document.querySelectorAll('.signup-item .btn, footer .footer1, footer .footer1 .btn2, #myBtn');
+  
+  const greenMedium = document.querySelectorAll('.signup-item .btn, .accordion-answer, footer .footer1, footer .footer1 .btn2, #myBtn');
   for (let i = 0; i < greenMedium.length; i++) {
-    greenMedium[i].style.backgroundColor = "#87A93A";
+    greenMedium[i].style.backgroundColor = '#87A93A';
   }
   const greenDark = document.querySelectorAll('html');
   for (let i = 0; i < greenDark.length; i++) {
-    greenDark[i].style.color = "#3d4c1a";
+    greenDark[i].style.color = '#3d4c1a';
   }
 
   const buttonHover = document.querySelectorAll('#buttonSubmit, #buttonAdult, #myBtn');
 
   for (let i = 0; i < buttonHover.length; i++) {
     buttonHover[i].addEventListener('mouseover', function (eventB) {
-      eventB.target.style.backgroundColor = "#3d4c1a";
+      eventB.target.style.backgroundColor = '#3d4c1a';
     }, false);
   }
 
   for (let i = 0; i < buttonHover.length; i++) {
     buttonHover[i].addEventListener('mouseout', function (eventB) {
-      eventB.target.style.backgroundColor = "#87A93A";
+      eventB.target.style.backgroundColor = '#87A93A';
     }, false);
   }
 
 }
 
 function displayAdult() {
+  document.getElementById('subtitle').innerText = 'fruits';
   document.getElementById('name').innerText = 'Your name';
   document.getElementById('instruction').innerText = 'you';
-  document.getElementById('subtitle').innerText = 'fruits';
   document.getElementById('selectChild').style.display = 'none';
   document.getElementById('selectAdult').style.display = 'block';
   document.getElementById('check').innerText = 'fruits that interest you';
@@ -115,28 +122,34 @@ function displayAdult() {
   document.getElementById('check6').innerText = 'Papaya';
 
   document.getElementById('buttonSubmit').innerText = 'Please register me!';
+  document.getElementById('questionBusiness').innerText = 'sex shop';
+  document.getElementById('answerWho').innerText = 'yourself alone';
+  document.getElementById('answerType').innerText = 'fruits';
+  document.getElementById('questionType').innerText = 'fruits';
+  document.getElementById('relatedType').innerText = 'fruit';
+
 
   document.getElementById('buttonAdult').innerText = 'Back to our child services';
-  const redMedium = document.querySelectorAll('.signup-item .btn, footer .footer1, footer .footer1 .btn2, #myBtn');
+  const redMedium = document.querySelectorAll('.signup-item .btn, .accordion-answer, footer .footer1, footer .footer1 .btn2, #myBtn');
   for (let i = 0; i < redMedium.length; i++) {
-    redMedium[i].style.backgroundColor = "#EA5E5A";
+    redMedium[i].style.backgroundColor = '#EA5E5A';
   }
   const darkRed = document.querySelectorAll('html');
   for (let i = 0; i < darkRed.length; i++) {
-    darkRed[i].style.color = "#71120f";
+    darkRed[i].style.color = '#71120f';
   }
 
   const buttonHover = document.querySelectorAll('#buttonSubmit, #buttonAdult, #myBtn');
 
   for (let i = 0; i < buttonHover.length; i++) {
     buttonHover[i].addEventListener('mouseover', function (eventB) {
-      eventB.target.style.backgroundColor = "#71120f";
+      eventB.target.style.backgroundColor = '#71120f';
     }, false);
   }
 
   for (let i = 0; i < buttonHover.length; i++) {
     buttonHover[i].addEventListener('mouseout', function (eventB) {
-      eventB.target.style.backgroundColor = "#EA5E5A";
+      eventB.target.style.backgroundColor = '#EA5E5A';
     }, false);
   }
 }
