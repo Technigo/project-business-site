45caf60561c35e085ccdbf034c8d323831569873
LovisaBrorson <lovisabrorson@gmail.com>
2022-01-30 08:50:56 +0100

    tryed to solve a warning in the js

--- code/script.js
+++ code/script.js
@@ -1,13 +1,11 @@
 
 const buttons = document.querySelectorAll('.accordion');
 
-
 buttons .forEach (button => {
   button.addEventListener('click', ()=> {
     
   const accordionContent = button.nextElementSibling;
   button.classList.toggle('accordion--active');
   accordionContent.classList.toggle('show-answer');
-    
-  })
-  })
+})
+})
