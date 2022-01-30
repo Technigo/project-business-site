b08871f3fe61483c2d69d55dd498722424be1e62
Katarina Elgerus <katarina.elgerus@liv.se>
2022-01-30 11:04:27 +0100

    cleaned up code

--- code/script.js
+++ code/script.js
@@ -7,6 +7,8 @@ document.getElementById("section1").onclick = toggle
 
 document.getElementById("section2").onclick = toggle
 
+document.getElementById("section3").onclick = toggle
+
 function toggle() {
   this.classList.toggle("active")
 }
