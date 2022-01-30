58902deac95536b1b1834d7f36e2b76b2fa58cb1
JensBergqvist <jensabb@hotmail.com>
2022-01-30 12:07:41 +0100

    project week 3

--- code/script.js
+++ code/script.js
@@ -6,4 +6,11 @@ function toggle() {
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
+
 document.getElementById("section1").onclick = toggle
+
+document.getElementById("section2").onclick = toggle
+
+document.getElementById("section3").onclick = toggle
+
+console.log('Why are you checking here?')
