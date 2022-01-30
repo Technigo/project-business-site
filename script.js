fecdee5a2605f785faa8cb025b816c59c7c7554b
JaEngd <95588205+JaEngd@users.noreply.github.com>
2022-01-30 22:05:10 +0100

    changed folder structure (again)

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// POPUP Login and Register form
+  document.getElementById("button").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "flex";
+})
+
+  document.querySelector(".close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+
+})
+
+let x = document.getElementById('login');
+let y = document.getElementById('register');
+let z = document.getElementById('btn');
+
+function register () {
+  x.style.left = '-400px';
+  y.style.left = '50px';
+  z.style.left = '110px';
+}
+
+function login () { 
+  x.style.left = '50px';
+  y.style.left = '450px';
+  z.style.left = '0px';
+}
+
+// Accordion slide down 
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+  faq.addEventListener("click", () => {
+  faq.classList.toggle("active");
+  })
+})
+
