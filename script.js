7ffc9da14f6c286c54539e05263685530fd502c8
JaEngd <95588205+JaEngd@users.noreply.github.com>
2022-01-31 20:25:54 +0100

    Added comments and controls to the video tag

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// POPUP Login and Register form
+// POPUP Login and Register form functions
   document.getElementById("button").addEventListener("click", function(){
   document.querySelector(".popup").style.display = "flex";
 })
@@ -9,24 +9,25 @@
 
 })
 
+// Toggle button fuctions
+
 let x = document.getElementById('login');
 let y = document.getElementById('register');
 let z = document.getElementById('btn');
 
-function register () {
-  x.style.left = '-400px';
-  y.style.left = '50px';
-  z.style.left = '110px';
+function register () { // Toggle button slide function for the register form
+  x.style.left = '-400px'; // login form slide outside the box while login form is showed
+  y.style.left = '50px'; // login form shows up win box
+  z.style.left = '110px'; // Toggle button slide to register side
 }
 
-function login () { 
+function login () {  // Toggle button slide function for the login form
   x.style.left = '50px';
   y.style.left = '450px';
   z.style.left = '0px';
 }
 
 // Accordion slide down 
-
 const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach(faq => {
