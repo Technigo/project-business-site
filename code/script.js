16c4b5d388d6a8e587776039285cc83196810a08
mathildakarlsson <86979661+mathildakarlsson@users.noreply.github.com>
2022-01-31 03:03:07 +0100

    images and navbar now responsive on desktop and tablet

--- code/script.js
+++ code/script.js
@@ -1,37 +1,4 @@
-
-// A function that adds and remove the class "active" on the section you click on.
-
-// function toggle() {
-//   this.classList.toggle("active")
-// }
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-
-// document.getElementById("Question1").onclick = toggle
-
-// document.getElementById("Question2").onclick = toggle
-
-// document.getElementById("Question3").onclick = toggle
-
-// document.getElementById("Question4").onclick = toggle
-
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-    
-//   });
-// }
-
+// open accordion 2 and 4
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -47,6 +14,7 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+// open accordion 1
 var acc = document.getElementsByClassName("accordion1");
 var i;
 
@@ -62,6 +30,7 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+// open accordion 3
 var acc = document.getElementsByClassName("accordion3");
 var i;
 
@@ -78,9 +47,7 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
-
-// sign up form javascript below
-
+// sign up form javascript
 document.getElementById('form').onsubmit = (event) => {
   event.preventDefault()
 
@@ -90,8 +57,7 @@ document.getElementById('form').onsubmit = (event) => {
 }
 
 
-// open and close mobile nav below //
-
+// open and close mobile nav
 const mobileBtn = document.getElementById('hamburger')
             nav = document.querySelector('nav')
             mobileBtnExit = document.getElementById('close-menu')
