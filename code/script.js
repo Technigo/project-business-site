75af38671a67124aed7cd1b516efae42c918fb89
Julia Ostedt <julia.ostedt@gmail.com>
2022-08-16 17:00:22 +0200

    added some css and js

--- code/script.js
+++ code/script.js
@@ -2,8 +2,13 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 function toggle() {
-  this.classList.toggle("active")
+  this.classList.toggle("active");
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+const questionOne = document.querySelector("#question1");
+questionOne.addEventListener("click", () => {
+  questionOne.classList.toggle("active");
+});
+
+// document.getElementById("question1").onclick = toggle;
