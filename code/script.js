3848b21320f4deae0361bdc975eb09df150193d5
Kristin <kristin@iMac.local>
2022-08-17 12:04:23 +0200

    Fix flexboxes in mobile view

--- code/script.js
+++ code/script.js
@@ -1,18 +1,18 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle()) {
-  this.classList.toggle("active")
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
-
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById(".panel").onclick = toggle
-
-
-
-
-
-
-
-
-sectionBtn.addEventListener("click", () => {}
