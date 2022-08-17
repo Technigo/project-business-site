3a487fe05532f2787fbd236fd5ed3482bfef63a1
Fredrik Holm <karlfredrikholm@gmail.com>
2022-08-17 15:35:49 +0200

    done deal

--- code/script.js
+++ code/script.js
@@ -2,20 +2,9 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 
-const accordionBtn1 = document.querySelector("#section1");
-const accordionBtn2 = document.getElementById(section2);
+const accordionBtn1 = document.querySelector('#section1');
+const answerTextBox = document.querySelector(".answer");
 
-const answerBox = document.querySelector(".answer");
-
-accordionBtn1.addEventListener("click", () => {
-  answerBox.classList.toggle("active");
+accordionBtn1.addEventListener('click', () => {
+  answerTextBox.classList.toggle("active");
 });
-
-
-/*
-function toggle() {
-  this.classList.toggle("active")
-}
-*/
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
