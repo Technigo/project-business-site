9082fd33a13a8c82b15c0638492632c7eb9608df
jessnmo <jessicamo92@gmail.com>
2022-08-17 17:10:16 +0200

    lion_project_biz_site_final

--- code/script.js
+++ code/script.js
@@ -1,9 +1,17 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+const questionOne = document.querySelector ("#question1");
+const answerOne = document.querySelector (".answer1");
+questionOne.addEventListener("click", function(){
+  answerOne.classList.toggle("answer1")
+});
+
+const questionTwo = document.querySelector ("#question2");
+const answerTwo = document.querySelector (".answer2");
+questionTwo.addEventListener("click", function(){
+  answerTwo.classList.toggle("answer2")
+});
+
+
