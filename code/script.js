41ef2504cbd44f920f78444686779cbb69a8e9e4
SofieRydmark <you@example.com>
2022-08-17 16:01:39 +0200

    final save

--- code/script.js
+++ code/script.js
@@ -19,18 +19,7 @@
 
 
 const accordionBtn = document.querySelector(".accordion");
-// sectionBtn.addEventListener("click", function() {
-//     window.alert("Hello Technigo");
-// });
 
-//  1 approach
-// const panel = document.querySelector(".panel");
-// accordionBtn.addEventListener("click", () => {
-//     // window.alert("Hello World");
-//     panel.classList.toggle("active");
-// });
-
-//  2nd approach
 const panelBtn1 = document.getElementById("mainPanelBtn");
 const panelBtn2 = document.querySelector("#secondaryPanelBtn"); 
 const panelBtn3 = document.getElementById("testPanelBtn");
@@ -39,18 +28,14 @@ const panel1 = document.querySelector("#panel1");
 const panel2 = document.querySelector("#panel2");
 const panel3 = document.querySelector("#panel3");
 
-console.log("hej")
 
 panelBtn1.addEventListener("click", () => {
-      // window.alert("Hello World");
       panel1.classList.toggle("active");
   });
 panelBtn2.addEventListener("click", () => {
-    // window.alert("Hello World");
     panel2.classList.toggle("active");
 });
 panelBtn3.addEventListener("click", () => {
-    // window.alert("Hello World");
     panel3.classList.toggle("active");
 });
 //  ()=>{}  == function(){} 
