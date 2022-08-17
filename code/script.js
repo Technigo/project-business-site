aaa244656159ffa0ba28c0f8fb260640e80ece7d
Kim Svensson <svensson.kim88@gmail.com>
2022-08-17 16:57:42 +0200

    Finnished

--- code/script.js
+++ code/script.js
@@ -1,9 +1,28 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
+
+// section1.onclick = toggle
+
+
+// function toggle() {
+//   this.classList.toggle("active")
+// }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+// document.getElementById("section1").onclick = toggle
+
+
+// SO WHAT THIS DOES: When you click on a question, this script activates a 'toggleAnswer' function to display the answer to that question. The 'toggleAnswer function activates a CSS class called '.open', which makes the answer appear.
+
+section1.onclick = toggleAnswer
+
+function toggleAnswer() {
+    this.classList.toggle('open')
+  }
+
+section2.onclick = toggleAnswer
+
+function toggleAnswer() {
+    this.classList.toggle('open')
+}
