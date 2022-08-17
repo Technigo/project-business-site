547d93d42eb22c10413c73826b391266bfc8192b
Fredrik Holm <karlfredrikholm@gmail.com>
2022-08-17 18:40:09 +0200

    done deal

--- code/script.js
+++ code/script.js
@@ -3,8 +3,29 @@
 // console.log(this) to see how it works
 
 const accordionBtn1 = document.querySelector('#section1');
-const answerTextBox = document.querySelector(".answer");
+const answerTextBox1 = document.querySelector(".answer1");
+
+const accordionBtn2 = document.querySelector('#section2');
+const answerTextBox2 = document.querySelector(".answer2");
+
+const accordionBtn3 = document.querySelector('#section3');
+const answerTextBox3 = document.querySelector(".answer3");
+
+const accordionBtn4 = document.querySelector('#section4');
+const answerTextBox4 = document.querySelector(".answer4");
 
 accordionBtn1.addEventListener('click', () => {
-  answerTextBox.classList.toggle("active");
+  answerTextBox1.classList.toggle("active");
+});
+
+accordionBtn2.addEventListener('click', () => {
+  answerTextBox2.classList.toggle("active");
+});
+
+accordionBtn3.addEventListener('click', () => {
+  answerTextBox3.classList.toggle("active");
+});
+
+accordionBtn4.addEventListener('click', () => {
+  answerTextBox4.classList.toggle("active");
 });
