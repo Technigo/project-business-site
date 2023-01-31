a496078254e4163a762183712d87bee680568aee
Frid Matilda <matilda.x.frid@scania.com>
2023-01-31 16:50:19 +0100

    a lot of work

--- code/script.js
+++ code/script.js
@@ -1,8 +1,22 @@
 // Selecting an HTML element
 const elementToToggle = document.querySelector("#section1");
 // A function that adds and remove the class "active" 
-const toggleHTMLElementClass = () => {
-  elementToToggle.classList.toggle("active");
-};
+// const toggleHTMLElementClass = () => {
+//   elementToToggle.classList.toggle("active");
+// };
 // Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
+//function 1
 elementToToggle.onclick = toggleHTMLElementClass;
+function toggleHTMLElementClass() {elementToToggle.classList.toggle("active")};
+
+//function 2
+const elementToToggle2 = document.querySelector("#section2");
+elementToToggle2.onclick = toggleHTMLElementClass2;
+function toggleHTMLElementClass2() {elementToToggle2.classList.toggle("active")};
+
+
+//Alert function
+const ordered = document.querySelector("#button");
+ordered.onclick = orderedturtle;
+function orderedturtle() {alert("Thank you for your order!")};
+
