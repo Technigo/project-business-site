cdca663a334e4fcab0aac3b3338a4892689e23f9
Johanna Leonsson <johanna@leonsson.se>
2023-01-31 17:16:32 +0100

    added some flexbox and styling and some javascript for accordion function also added more elements in html

--- code/script.js
+++ code/script.js
@@ -1,8 +1,32 @@
-// Selecting an HTML element
-const elementToToggle = document.querySelector("#section1");
+// Selecting an HTML element 
 // A function that adds and remove the class "active" 
+const elementToToggle = document.querySelector("#quest1");
+const elementToToggle2 = document.querySelector("#quest2"); 
+const elementToToggle3 = document.querySelector("#quest3");
+const elementToToggle4 = document.querySelector("#quest4");
+const elementToToggle5 = document.querySelector("#quest5");
+
+
 const toggleHTMLElementClass = () => {
   elementToToggle.classList.toggle("active");
 };
+const toggleHTMLElementClass2 = () => {
+  elementToToggle2.classList.toggle("active");
+};
+const toggleHTMLElementClass3 = () => {
+  elementToToggle3.classList.toggle("active");
+};
+const toggleHTMLElementClass4 = () => {
+  elementToToggle4.classList.toggle("active");
+};
+const toggleHTMLElementClass5 = () => {
+  elementToToggle5.classList.toggle("active");
+};
+
+
 // Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
 elementToToggle.onclick = toggleHTMLElementClass;
+elementToToggle2.onclick = toggleHTMLElementClass2;
+elementToToggle3.onclick = toggleHTMLElementClass3;
+elementToToggle4.onclick = toggleHTMLElementClass4;
+elementToToggle5.onclick = toggleHTMLElementClass5;
