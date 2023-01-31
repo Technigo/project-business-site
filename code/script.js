a2544d6ca4855b781346784cb77abff76e09d269
Maja Zimnoch <majamargareta@Majas-MacBook-Air.local>
2023-01-31 16:59:43 +0100

    started with a layout

--- code/script.js
+++ code/script.js
@@ -1,8 +1,17 @@
 // Selecting an HTML element
-const elementToToggle = document.querySelector("#section1");
+const elementToToggleSection1 = document.querySelector("#section1");
+const elementToToggleSection2 = document.querySelector("#section2");
+
 // A function that adds and remove the class "active" 
-const toggleHTMLElementClass = () => {
-  elementToToggle.classList.toggle("active");
+const toggleHTMLElementClass1 = () => {
+  elementToToggleSection1.classList.toggle("active");
+};
+
+const toggleHTMLElementClass2 = () => {
+  elementToToggleSection2.classList.toggle("active");
 };
 // Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
-elementToToggle.onclick = toggleHTMLElementClass;
+elementToToggleSection1.onclick = toggleHTMLElementClass1;
+
+elementToToggleSection2.onclick = toggleHTMLElementClass2;
+
