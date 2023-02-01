441095066c8083d73e9a7080716d6deb62a5fea9
maryzaia <zaia.mary@gmail.com>
2023-02-01 10:55:42 +0100

    Co-authored-by: Leo Thunell <khleoth@users.noreply.github.com> Co-authored-by: Eva Sjöholm <EvaSjoholm@users.noreply.github.com> Co-authored-by: NinaW <NinaWald@users.noreply.github.com> Co-authored-by: Joanna Philips <Joanna-Philips@users.noreply.github.com>

--- code/script.js
+++ code/script.js
@@ -6,3 +6,4 @@ const toggleHTMLElementClass = () => {
 };
 // Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
 elementToToggle.onclick = toggleHTMLElementClass;
+
