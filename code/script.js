609e2a6c810ca7ac838c41d3f563a4a6e221b627
maryzaia <zaia.mary@gmail.com>
2023-02-01 12:59:10 +0100

    wrote some more code

--- code/script.js
+++ code/script.js
@@ -1,9 +1,36 @@
 // Selecting an HTML element
-const elementToToggle = document.querySelector(".question-hidden");
+
+const elementToToggle1 = document.querySelector("#question-hidden1");
+const elementToToggle2 = document.querySelector("#question-hidden2");
+const elementToToggle3 = document.querySelector("#question-hidden3");
+const elementToToggle4 = document.querySelector("#question-hidden4");
+
+
+
 // A function that adds and remove the class "active" 
-const toggleHTMLElementClass = () => {
-  elementToToggle.classList.toggle("active");
+const toggleHTMLElementClass1 = () => {
+    elementToToggle1.classList.toggle("active");
+
+};
+
+const toggleHTMLElementClass2 = () => {
+  elementToToggle2.classList.toggle("active");
+
+};
+
+const toggleHTMLElementClass3 = () => {
+  elementToToggle3.classList.toggle("active");
+
+};
+
+const toggleHTMLElementClass4 = () => {
+  elementToToggle4.classList.toggle("active");
+
 };
 // Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
-elementToToggle.onclick = toggleHTMLElementClass;
+elementToToggle1.onclick = toggleHTMLElementClass1;
+elementToToggle2.onclick = toggleHTMLElementClass2;
+elementToToggle3.onclick = toggleHTMLElementClass3;
+elementToToggle4.onclick = toggleHTMLElementClass4;
+
 
