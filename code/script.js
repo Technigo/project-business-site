d0288907db9eb4350df679c5e3de82a3cc000082
Andreas Axelsson <pererikandreas@gmail.com>
2023-02-01 14:27:52 +0100

    html, css, javascript

--- code/script.js
+++ code/script.js
@@ -1,8 +1,52 @@
+/*
 // Selecting an HTML element
-const elementToToggle = document.querySelector("#section1");
+const elementToToggle = document.querySelector("#pizza");
 // A function that adds and remove the class "active" 
 const toggleHTMLElementClass = () => {
   elementToToggle.classList.toggle("active");
 };
 // Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
-elementToToggle.onclick = toggleHTMLElementClass;
+elementToToggle.onclick = toggleHTMLElementClass;*/
+
+
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
