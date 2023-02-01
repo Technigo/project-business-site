b19f04e6c9768001d8d24775cbc06a3582865bef
Amanda Sjöström <amanda.sl747@gmail.com>
2023-02-01 13:22:05 +0100

    JS Changes

--- code/script.js
+++ code/script.js
@@ -1,8 +1,16 @@
-// Selecting an HTML element
-const elementToToggle = document.getElementById("section1");
-// A function that adds and remove the class "active"
-const toggleHTMLElementClass = () => {
-  elementToToggle.classList.toggle("active");
-};
-// Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
-elementToToggle.onclick = toggleHTMLElementClass;
+// // Selecting an HTML element
+// const elementToToggle = document.getElementById("section1");
+// // A function that adds and remove the class "active"
+// const toggleHTMLElementClass = () => {
+//   elementToToggle.classList.toggle("active");
+// };
+// // Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
+// elementToToggle.onclick = toggleHTMLElementClass;
+
+const elementsToToggle = document.getElementsByClassName("question");
+
+for (i = 0; i < elementsToToggle.length; i++) {
+  elementsToToggle[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
