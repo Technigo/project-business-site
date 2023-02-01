f1ebbd68aee6c6d5fde4fb38470a586126114a48
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-02-01 12:05:29 +0100

    added styling and javascript

--- code/script.js
+++ code/script.js
@@ -1,8 +1,16 @@
 // Selecting an HTML element
-const elementToToggle = document.querySelector("#section1");
-// A function that adds and remove the class "active" 
-const toggleHTMLElementClass = () => {
-  elementToToggle.classList.toggle("active");
+const sectionOneElement = document.querySelector("#section1");
+
+// A function that adds and remove the class "active"
+const toggleSectionOneElementClass = () => {
+  sectionOneElement.classList.toggle("active");
 };
 // Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
-elementToToggle.onclick = toggleHTMLElementClass;
+sectionOneElement.onclick = toggleSectionOneElementClass;
+
+const sectionTwoElement = document.querySelector("#section2");
+
+const toggleSectionTwoElementClass = () => {
+  sectionTwoElement.classList.toggle("active");
+};
+sectionTwoElement.onclick = toggleSectionTwoElementClass;
