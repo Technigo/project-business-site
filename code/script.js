e5edd484cdb305ff383169d02821e0bf51720a43
Matilda Brunemalm <>
2023-08-08 14:21:16 +0200

    Change starter code

--- code/script.js
+++
@@ -1,8 +0,0 @@
-// Selecting an HTML element
-const elementToToggle = document.querySelector("#section1");
-// A function that adds and remove the class "active" 
-const toggleHTMLElementClass = () => {
-  elementToToggle.classList.toggle("active");
-};
-// Adding the toggle function to our selected element, so everytime it's clicked the class will be toggled
-elementToToggle.onclick = toggleHTMLElementClass;
